ad94d0f1d5517d0d3282407c82b73e6f631aa18f
ktorz <matthias.benkort@gmail.com>
2015-12-01 19:42:39 +0100

    Add License headers

--- lorawan/gateway/decode_utils.go
+++ lorawan/gateway/decode_utils.go
@@ -1,3 +1,6 @@
+// Copyright © 2015 Matthias Benkort <matthias.benkort@gmail.com>
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
 package protocol
 
 import (
