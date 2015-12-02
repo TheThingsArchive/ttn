e2fc9b1a0b193a85e1ee247194c7c8d4720eb00e
ktorz <matthias.benkort@gmail.com>
2015-12-02 17:49:32 +0100

    Change license headers + add AUTHORS list

--- lorawan/gateway/encode.go
+++ lorawan/gateway/encode.go
@@ -1,3 +1,6 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
 package protocol
 
 // Marshal transform a packet to a sequence of bytes.
--- lorawan/gateway/protocol.go
+++ lorawan/gateway/protocol.go
@@ -1,4 +1,4 @@
-// Copyright © 2015 Matthias Benkort <matthias.benkort@gmail.com>
+// Copyright © 2015 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
 // Package gateway/protocol provides useful methods and types to handle communications with a gateway.
