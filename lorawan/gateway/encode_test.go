436ae66025564fa9379a46d75456d8c6a0f9f48f
ktorz <matthias.benkort@gmail.com>
2015-12-02 17:34:41 +0100

    Add test about Marshal() for PUSH_DATA packets

--- lorawan/gateway/encode_test.go
+++ lorawan/gateway/encode_test.go
@@ -5,6 +5,7 @@ package protocol
 
 import (
 	"bytes"
+	"io/ioutil"
 	"reflect"
 	"testing"
 	"time"
@@ -13,134 +14,458 @@ import (
 // -----------------------------------------------------------------
 // ------------------------- Marshal (packet *Packet) ([]byte, error)
 // -----------------------------------------------------------------
-
 // ---------- PUSH_DATA
+func checkMarshalPUSH_DATA(packet *Packet, payload []byte) error {
+	raw, err := Marshal(packet)
+
+	if err != nil {
+		return err
+	}
+
+	if len(raw) < 12 {
+		return errors.New(fmt.Printf("Invalid raw sequence length: %d", len(raw)))
+	}
+
+	if raw[0] != packet.Version {
+		return errors.New(fmt.Printf("Invalid raw version: %x", raw[0]))
+	}
+
+	if !bytes.Equal(raw[1:3], packet.Token) {
+		return errors.New(fmt.Printf("Invalid raw token: %x", raw[1:3]))
+	}
+
+	if raw[3] != packet.Identifier {
+		return errors.New(fmt.Printf("Invalid raw identifier: %x", raw[3]))
+	}
+
+	if !bytes.Equal(raw[4:12], packet.GatewayId) {
+		return errors.New(fmt.Printf("Invalid raw gatewayId: % x", raw[4:12]))
+	}
+
+	if packet.Payload != nil && !bytes.Equal(raw[12:], payload) {
+		return errors.New(fmt.Printf("Invalid raw payload: % x", raw[12:]))
+	}
+
+	return err
+}
+
+// Marshal a basic push_data packet with Stat payload
+func TestMarshalPUSH_DATA1(t *testing.T) {
+	time, err := time.Parse(time.RFC3339, "2014-01-12T08:59:28Z")
+
+	// {"stat":{"ackr":100,"alti":145,"dwnb":2,"lati":46.24,"long":3.2523,"rxfw":2,"rxnb":2,"rxok":2,"time":"2014-01-12 08:59:28 GMT","txnb":2}}
+	payload, err := ioutil.ReadFile("./test_data/marshal_stat")
+
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+		Payload: &Payload{
+			Stat: &Stat{
+				Ackr: 100.0,
+				Alti: 145,
+				Long: 3.25230,
+				Rxok: 2,
+				Rxfw: 2,
+				Rxnb: 2,
+				Lati: 46.24,
+				Dwnb: 2,
+				Txnb: 2,
+				Time: time,
+			},
+		},
+	}
+
+	if err = checkMarshalPUSH_DATA(packet, payload); err != nil {
+		t.Errorf("Failed to marshal packet: %v", err)
+	}
+}
+
+// Mashal a push_data packet with RXPK payload
+func TestMarshalPUSH_DATA2(t *testing.T) {
+	time1, err := time.Parse(time.RFC3339Nano, "2013-03-31T16:21:17.528002Z")
+	time2, err := time.Parse(time.RFC3339Nano, "2013-03-31T16:21:17.530974Z")
+
+	//{"rxpk":[{"chan":2,"codr":"4/6","data":"-DS4CGaDCdG+48eJNM3Vai-zDpsR71Pn9CPA9uCON84","datr":"SF7BW125","freq":866.349812,"lsnr":5.1,"modu":"LORA","rssi":-35,"size":32,"stat":1,"time":"2013-03-31T16:21:17.528002Z","tmst":3512348611},{"chan":9,"data":"VEVTVF9QQUNLRVRfMTIzNA==","datr":50000,"freq":869.1,"modu":"FSK","rfch":1,"rssi":-75,"size":16,"stat":1,"time":"2013-03-31T16:21:17.530974Z","tmst":3512348514}]}
+	payload, err := ioutil.ReadFile("./test_data/marshal_rxpk")
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+		Payload: &Payload{
+			RXPK: &[]RXPK{
+				RXPK{
+					Time: time1,
+					Tmst: 3512348611,
+					Chan: 2,
+					Rfch: 0,
+					Freq: 866.349812,
+					Stat: 1,
+					Modu: "LORA",
+					Datr: "SF7BW125",
+					Codr: "4/6",
+					Rssi: -35,
+					Lsnr: 5.1,
+					Size: 32,
+					Data: "-DS4CGaDCdG+48eJNM3Vai-zDpsR71Pn9CPA9uCON84",
+				},
+				RXPK{
+					Chan: 9,
+					Data: "VEVTVF9QQUNLRVRfMTIzNA==",
+					Datr: "50000",
+					Freq: 869.1,
+					Modu: "FSK",
+					Rfch: 1,
+					Rssi: -75,
+					Size: 16,
+					Stat: 1,
+					Time: time2,
+					Tmst: 3512348514,
+				},
+			},
+		},
+	}
+
+	if err = checkMarshalPUSH_DATA(packet, payload); err != nil {
+		t.Errorf("Failed to marshal packet: %v", err)
+	}
+}
+
+// Mashal a push_data packet with RXPK payload and Stat
+func TestMarshalPUSH_DATA2(t *testing.T) {
+	time1, err := time.Parse(time.RFC3339, "2014-01-12T08:59:28Z")
+	time2, err := time.Parse(time.RFC3339Nano, "2013-03-31T16:21:17.528002Z")
+	time3, err := time.Parse(time.RFC3339Nano, "2013-03-31T16:21:17.530974Z")
+
+	//{"rxpk":[{"chan":2,"codr":"4/6","data":"-DS4CGaDCdG+48eJNM3Vai-zDpsR71Pn9CPA9uCON84","datr":"SF7BW125","freq":866.349812,"lsnr":5.1,"modu":"LORA","rssi":-35,"size":32,"stat":1,"time":"2013-03-31T16:21:17.528002Z","tmst":3512348611},{"chan":9,"data":"VEVTVF9QQUNLRVRfMTIzNA==","datr":50000,"freq":869.1,"modu":"FSK","rfch":1,"rssi":-75,"size":16,"stat":1,"time":"2013-03-31T16:21:17.530974Z","tmst":3512348514}],"stat":{"ackr":100,"alti":145,"dwnb":2,"lati":46.24,"long":3.2523,"rxfw":2,"rxnb":2,"rxok":2,"time":"2014-01-12 08:59:28 GMT","txnb":2}}
+	payload, err := ioutil.ReadFile("./test_data/marshal_rxpk_stat")
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+		Payload: &Payload{
+			Stat: &Stat{
+				Ackr: 100.0,
+				Alti: 145,
+				Long: 3.25230,
+				Rxok: 2,
+				Rxfw: 2,
+				Rxnb: 2,
+				Lati: 46.24,
+				Dwnb: 2,
+				Txnb: 2,
+				Time: time1,
+			},
+			RXPK: &[]RXPK{
+				RXPK{
+					Time: time2,
+					Tmst: 3512348611,
+					Chan: 2,
+					Rfch: 0,
+					Freq: 866.349812,
+					Stat: 1,
+					Modu: "LORA",
+					Datr: "SF7BW125",
+					Codr: "4/6",
+					Rssi: -35,
+					Lsnr: 5.1,
+					Size: 32,
+					Data: "-DS4CGaDCdG+48eJNM3Vai-zDpsR71Pn9CPA9uCON84",
+				},
+				RXPK{
+					Chan: 9,
+					Data: "VEVTVF9QQUNLRVRfMTIzNA==",
+					Datr: "50000",
+					Freq: 869.1,
+					Modu: "FSK",
+					Rfch: 1,
+					Rssi: -75,
+					Size: 16,
+					Stat: 1,
+					Time: time3,
+					Tmst: 3512348514,
+				},
+			},
+		},
+	}
+
+	if err = checkMarshalPUSH_DATA(packet, payload); err != nil {
+		t.Errorf("Failed to marshal packet: %v", err)
+	}
+}
+
+// Marshal with an invalid GatewayId (too short)
+func TestMarshalPUSH_DATA3(t *testing.T) {
+	time, err := time.Parse(time.RFC3339, "2014-01-12T08:59:28Z")
+
+	// {"stat":{"ackr":100,"alti":145,"dwnb":2,"lati":46.24,"long":3.2523,"rxfw":2,"rxnb":2,"rxok":2,"time":"2014-01-12 08:59:28 GMT","txnb":2}}
+	payload, err := ioutil.ReadFile("./test_data/marshal_stat")
+
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0}, // Invalid
+		Payload: &Payload{
+			Stat: &Stat{
+				Ackr: 100.0,
+				Alti: 145,
+				Long: 3.25230,
+				Rxok: 2,
+				Rxfw: 2,
+				Rxnb: 2,
+				Lati: 46.24,
+				Dwnb: 2,
+				Txnb: 2,
+				Time: time,
+			},
+		},
+	}
+
+	if err = checkMarshalPUSH_DATA(packet, payload); err == nil {
+		t.Errorf("Successfully mashalled a invalid packet")
+	}
+}
+
+// Marshal with an invalid GatewayId (too long)
+func TestMarshalPUSH_DATA4(t *testing.T) {
+	time, err := time.Parse(time.RFC3339, "2014-01-12T08:59:28Z")
+
+	// {"stat":{"ackr":100,"alti":145,"dwnb":2,"lati":46.24,"long":3.2523,"rxfw":2,"rxnb":2,"rxok":2,"time":"2014-01-12 08:59:28 GMT","txnb":2}}
+	payload, err := ioutil.ReadFile("./test_data/marshal_stat")
+
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}, // Invalid
+		Payload: &Payload{
+			Stat: &Stat{
+				Ackr: 100.0,
+				Alti: 145,
+				Long: 3.25230,
+				Rxok: 2,
+				Rxfw: 2,
+				Rxnb: 2,
+				Lati: 46.24,
+				Dwnb: 2,
+				Txnb: 2,
+				Time: time,
+			},
+		},
+	}
+
+	if err = checkMarshalPUSH_DATA(packet, payload); err == nil {
+		t.Errorf("Successfully mashalled a invalid packet")
+	}
+}
+
+// Marshal with an invalid TokenId (too short)
+func TestMarshalPUSH_DATA5(t *testing.T) {
+	time, err := time.Parse(time.RFC3339, "2014-01-12T08:59:28Z")
+
+	// {"stat":{"ackr":100,"alti":145,"dwnb":2,"lati":46.24,"long":3.2523,"rxfw":2,"rxnb":2,"rxok":2,"time":"2014-01-12 08:59:28 GMT","txnb":2}}
+	payload, err := ioutil.ReadFile("./test_data/marshal_stat")
+
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}, // Invalid
+		Payload: &Payload{
+			Stat: &Stat{
+				Ackr: 100.0,
+				Alti: 145,
+				Long: 3.25230,
+				Rxok: 2,
+				Rxfw: 2,
+				Rxnb: 2,
+				Lati: 46.24,
+				Dwnb: 2,
+				Txnb: 2,
+				Time: time,
+			},
+		},
+	}
+
+	if err = checkMarshalPUSH_DATA(packet, payload); err == nil {
+		t.Errorf("Successfully mashalled a invalid packet")
+	}
+}
+
+// Marshal with an invalid TokenId (too long)
+func TestMarshalPUSH_DATA6(t *testing.T) {
+	time, err := time.Parse(time.RFC3339, "2014-01-12T08:59:28Z")
+
+	// {"stat":{"ackr":100,"alti":145,"dwnb":2,"lati":46.24,"long":3.2523,"rxfw":2,"rxnb":2,"rxok":2,"time":"2014-01-12 08:59:28 GMT","txnb":2}}
+	payload, err := ioutil.ReadFile("./test_data/marshal_stat")
+
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14, 0x28},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}, // Invalid
+		Payload: &Payload{
+			Stat: &Stat{
+				Ackr: 100.0,
+				Alti: 145,
+				Long: 3.25230,
+				Rxok: 2,
+				Rxfw: 2,
+				Rxnb: 2,
+				Lati: 46.24,
+				Dwnb: 2,
+				Txnb: 2,
+				Time: time,
+			},
+		},
+	}
+
+	if err = checkMarshalPUSH_DATA(packet, payload); err == nil {
+		t.Errorf("Successfully mashalled a invalid packet")
+	}
+}
 
 // ---------- PUSH_ACK
 func checkMarshalPUSH_ACK(packet *Packet) error {
-    raw, err := Marshal(packet)
+	raw, err := Marshal(packet)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    if len(raw) != 4 {
-        return errors.New(fmt.Printf("Invalid raw sequence length: %d", len(raw)))
-    }
+	if len(raw) != 4 {
+		return errors.New(fmt.Printf("Invalid raw sequence length: %d", len(raw)))
+	}
 
-    if raw[0] != packet.Version {
-        return errors.New(fmt.Printf("Invalid raw version: %x", raw[0]))
-    }
+	if raw[0] != packet.Version {
+		return errors.New(fmt.Printf("Invalid raw version: %x", raw[0]))
+	}
 
-    if !bytes.Equal(raw[1:3], packet.Token) {
-        return errors.New(fmt.Printf("Invalid raw token: %x", raw[1:3]))
-    }
+	if !bytes.Equal(raw[1:3], packet.Token) {
+		return errors.New(fmt.Printf("Invalid raw token: %x", raw[1:3]))
+	}
 
-    if raw[3] != packet.Identifier {
-        return errors.New(fmt.Printf("Invalid raw identifier: %x", raw[3]))
-    }
+	if raw[3] != packet.Identifier {
+		return errors.New(fmt.Printf("Invalid raw identifier: %x", raw[3]))
+	}
 
-    return err
+	return err
 }
 
 // Marshal a basic push_ack packet
-func TestMarshalPUSH_ACK1(*t testing.T) {
-    packet := &Packet{
-        Version: VERSION,
-        Token: []byte{0xAA, 0x14},
-        Identifier: PUSH_ACK,
-        GatewayId: nil,
-        Payload: nil,
-    }
-    if err := checkMarshalPUSH_ACK(packet); err != nil {
-        t.Errorf("Failed to marshal packet: %v", err)
-    }
+func TestMarshalPUSH_ACK1(t *testing.T) {
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  nil,
+		Payload:    nil,
+	}
+	if err := checkMarshalPUSH_ACK(packet); err != nil {
+		t.Errorf("Failed to marshal packet: %v", err)
+	}
 }
 
 // Marshal a push_ack packet with extra useless gatewayId
-func TestMarshalPUSH_ACK2(*t testing.T) {
-    packet := &Packet{
-        Version: VERSION,
-        Token: []byte{0xAA, 0x14},
-        Identifier: PUSH_ACK,
-        GatewayId: []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
-        Payload: nil,
-    }
-    if err := checkMarshalPUSH_ACK(packet); err != nil {
-        t.Errorf("Failed to marshal packet: %v", err)
-    }
+func TestMarshalPUSH_ACK2(t *testing.T) {
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+		Payload:    nil,
+	}
+	if err := checkMarshalPUSH_ACK(packet); err != nil {
+		t.Errorf("Failed to marshal packet: %v", err)
+	}
 }
 
 // Marshal a push_ack packet with extra useless Payload
-func TestMarshalPUSH_ACK3(*t testing.T) {
-    payload := &Payload{
-        Stat: &Stat{
-            Rxfw: 14,
-            Rxnb: 14,
-            Rxok: 14,
-        },
-    }
-    packet := &Packet{
-        Version: VERSION,
-        Token: []byte{0xAA, 0x14},
-        Identifier: PUSH_ACK,
-        GatewayId: nil,
-        Payload: payload,
-    }
-    if err := checkMarshalPUSH_ACK(packet); err != nil {
-        t.Errorf("Failed to marshal packet: %v", err)
-    }
+func TestMarshalPUSH_ACK3(t *testing.T) {
+	payload := &Payload{
+		Stat: &Stat{
+			Rxfw: 14,
+			Rxnb: 14,
+			Rxok: 14,
+		},
+	}
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  nil,
+		Payload:    payload,
+	}
+	if err := checkMarshalPUSH_ACK(packet); err != nil {
+		t.Errorf("Failed to marshal packet: %v", err)
+	}
 }
 
 // Marshal a push_ack with extra useless gatewayId and payload
-func TestMarshalPUSH_ACK4(*t testing.T) {
-    payload := &Payload{
-        Stat: &Stat{
-            Rxfw: 14,
-            Rxnb: 14,
-            Rxok: 14,
-        },
-    }
-    packet := &Packet{
-        Version: VERSION,
-        Token: []byte{0xAA, 0x14},
-        Identifier: PUSH_ACK,
-        GatewayId: []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
-        Payload: payload,
-    }
-    if err := checkMarshalPUSH_ACK(packet); err != nil {
-        t.Errorf("Failed to marshal packet: %v", err)
-    }
+func TestMarshalPUSH_ACK4(t *testing.T) {
+	payload := &Payload{
+		Stat: &Stat{
+			Rxfw: 14,
+			Rxnb: 14,
+			Rxok: 14,
+		},
+	}
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA, 0x14},
+		Identifier: PUSH_ACK,
+		GatewayId:  []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+		Payload:    payload,
+	}
+	if err := checkMarshalPUSH_ACK(packet); err != nil {
+		t.Errorf("Failed to marshal packet: %v", err)
+	}
 }
 
 // Marshal a push_ack with an invalid token (too short)
-func TestMarshalPUSH_ACK5(*t testing.T) {
-    packet := &Packet{
-        Version: VERSION,
-        Token: []byte{0xAA},
-        Identifier: PUSH_ACK,
-        GatewayId: nil,
-        Payload: nil,
-    }
-    _, err := Marshal(packet)
-    if err == nil {
-        t.Errorf("Successfully marshalled an invalid packet")
-    }
+func TestMarshalPUSH_ACK5(t *testing.T) {
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0xAA},
+		Identifier: PUSH_ACK,
+		GatewayId:  nil,
+		Payload:    nil,
+	}
+	_, err := Marshal(packet)
+	if err == nil {
+		t.Errorf("Successfully marshalled an invalid packet")
+	}
 }
 
 // Marshal a push_ack with an invalid token (too long)
-func TestMarshalPUSH_ACK6(*t testing.T) {
-    packet := &Packet{
-        Version: VERSION,
-        Token: []byte{0x9A, 0x7A, 0x7E},
-        Identifier: PUSH_ACK,
-        GatewayId: nil,
-        Payload: nil,
-    }
-    _, err := Marshal(packet)
-    if err == nil {
-        t.Errorf("Successfully marshalled an invalid packet")
-    }
+func TestMarshalPUSH_ACK6(t *testing.T) {
+	packet := &Packet{
+		Version:    VERSION,
+		Token:      []byte{0x9A, 0x7A, 0x7E},
+		Identifier: PUSH_ACK,
+		GatewayId:  nil,
+		Payload:    nil,
+	}
+	_, err := Marshal(packet)
+	if err == nil {
+		t.Errorf("Successfully marshalled an invalid packet")
+	}
 }
 
 // ---------- PULL_DATA
@@ -148,4 +473,3 @@ func TestMarshalPUSH_ACK6(*t testing.T) {
 // ---------- PULL_ACK
 
 // ---------- PULL_RESP
-
