9891caf4e344283aaee32c9210624b59081a5819
ktorz <matthias.benkort@gmail.com>
2015-12-04 10:49:21 +0100

    Change package name in lorawan/gateway documentation

--- lorawan/gateway/gateway.go
+++ lorawan/gateway/gateway.go
@@ -1,7 +1,7 @@
 // Copyright © 2015 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
-// Package gateway/protocol provides useful methods and types to handle communications with a gateway.
+// Package gateway provides useful methods and types to handle communications with a gateway.
 //
 // This package relies on the SemTech Protocol 1.2 accessible on github: https://github.com/TheThingsNetwork/packet_forwarder/blob/master/PROTOCOL.TXT
 package gateway
