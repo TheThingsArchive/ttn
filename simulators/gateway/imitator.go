10cc995080bd6692e93b31358cd9c46c89335e4c
ktorz <matthias.benkort@gmail.com>
2015-12-14 19:31:21 +0100

    [simulators.gateway] Implement first part of imitator (send random PUSH_DATA). Still no data in them

--- simulators/gateway/imitator.go
+++ simulators/gateway/imitator.go
@@ -3,12 +3,38 @@
 
 package gateway
 
-import ()
+import (
+	"errors"
+	"github.com/thethingsnetwork/core/lorawan/semtech"
+	"time"
+)
 
 type Imitator interface {
 	Mimic() error
 }
 
 func (g *Gateway) Mimic() error {
+	if g.cmd == nil || g.quit == nil {
+		return errors.New("Cannot mimic on a stopped gateway")
+	}
+
+	go func(g *Gateway) {
+		ticker := time.Tick(time.Millisecond * 800)
+		for {
+			select {
+			case <-ticker:
+				g.Forward(semtech.Packet{
+					Version:    semtech.VERSION,
+					Identifier: semtech.PUSH_DATA,
+					GatewayId:  g.Id,
+					Token:      genToken(),
+				})
+			case ack := <-g.quit:
+				ack <- nil
+				return
+			}
+		}
+	}(g)
+
 	return nil
 }
