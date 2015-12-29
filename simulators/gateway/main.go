d030642580f18de3839e90aaf846139aa2b69012
ktorz <matthias.benkort@gmail.com>
2015-12-29 11:19:30 +0100

    [simulators.gateway] Add main.go to hold package doc

---
+++ simulators/gateway/main.go
@@ -0,0 +1,8 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+// package gateway offers a dummy representation of a gateway.
+//
+// The package can be used to create a dummy gateway.
+// Its former use is to provide a handy simulator for further testing of the whole network chain.
+package gateway
