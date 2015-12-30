584b9fedfe85db6b00475196335efef4b829df49
ktorz <matthias.benkort@gmail.com>
2015-12-30 23:38:42 +0100

    [router] Handle PUSH_DATA packets with router

--- components/router/router.go
+++ components/router/router.go
@@ -16,38 +16,82 @@ const (
 )
 
 type Router struct {
-	PortUDP  uint
-	PortHTTP uint
-	Logger   log.Logger
+	PortUDP       uint
+	PortHTTP      uint
+	Logger        log.Logger
+	addressKeeper addressKeeper
 }
 
 func New(portUDP, portHTTP uint) (*Router, error) {
+	localDB, err := NewLocalDB(EXPIRY_DELAY)
+
+	if err != nil {
+		return nil, error
+	}
+
 	return &Router{
-		PortUDP:  portUDP,
-		PortHTTP: portHTTP,
-		Logger:   log.VoidLogger{},
+		PortUDP:       portUDP,
+		PortHTTP:      portHTTP,
+		addressKeeper: localDB,
+		Logger:        log.VoidLogger{},
 	}, nil
 }
 
 // HandleUplink implements the core.Router interface
-func (r *Router) HandleUplink(upAdapter core.GatewayRouterAdapter, packet semtech.Packet, gateway core.GatewayAddress) {
+func (r *Router) HandleUplink(upAdapter core.GatewayRouterAdapter, downAdapter core.RouterBrokerAdapter, packet semtech.Packet, gateway core.GatewayAddress) {
 	switch packet.Identifier {
 	case semtech.PULL_DATA:
-		r.log("PULL_DATA received, sending ack")
+		r.log("receives PULL_DATA, sending ack")
 		upAdapter.Ack(r, semtech.Packet{
 			Version:    semtech.VERSION,
 			Identifier: semtech.PULL_ACK,
 			Token:      packet.Token,
 		}, gateway)
 	case semtech.PUSH_DATA:
-		r.log("TODO PUSH_DATA")
-		/* PUSH_DATA
-		 *
-		 * Send PUSH_ACK
-		 * Stores the gateway connection id for later response
-		 * Lookup for an existing broker associated to the device address
-		 * Forward data to that broker
-		 */
+		// 1. Send an ack
+		r.log("receives PUSH_DATA, sending ack")
+		upAdapter.Ack(r, semtech.Packet{
+			Version:    semtech.VERSION,
+			Identifier: semtech.PUSH_ACK,
+			Token:      packet.Token,
+		}, gateway)
+
+		// 2. Determine payloads related to different end-devices present in the packet
+		// NOTE So far, Stats are ignored.
+		if packet.Payload == nil || len(packet.Payload.RXPK) == 0 {
+			r.log("Ignores inconsistent PUSH_DATA packet")
+			return
+		}
+
+		payloads = make(map[semtech.DeviceAddress]semtech.Payload)
+		for _, rxpk := range packet.Payload.RXPK {
+			devAddr := rxpk.DevAddr()
+			if devAddr == nil {
+				r.log("Unable to determine end-device address for rxpk: %+v", rxpk)
+				continue
+			}
+
+			if _, ok := payloads[*devAddr]; !ok {
+				payloads[*devAddr] = semtech.Payload{
+					RXPK: make([]semtech.RXPK, 0),
+				}
+			}
+
+			payloads[*devAddr].RXPK = append(payloads[*devAddr].RXPK, rxpk)
+		}
+
+		// 3. Broadcast or Forward payloads depending wether or not the brokers are known
+		for payload, devAddr := range payloads {
+			brokers, err := r.addressKeeper.lookup(devAddr)
+			if err != nil {
+				r.log("Forward payload to known brokers %+v", payload)
+				downAdapter.Forward(router, payload, brokers...)
+				continue
+			}
+
+			r.log("Broadcast payload to all brokers %+v", payload)
+			downAdapter.Broadcast(router, payload)
+		}
 	default:
 		r.log("Unexpected packet receive from uplink %+v", packet)
 
