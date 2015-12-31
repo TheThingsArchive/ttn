83732f24ded31bd453c4787c466e6fda35d2b5e8
ktorz <matthias.benkort@gmail.com>
2015-12-31 14:55:18 +0100

    [router] Implements basic mock upadapter

--- testing/adapters/gtw_rtr_moke/adapter.go
+++ testing/adapters/gtw_rtr_moke/adapter.go
@@ -1,25 +1,51 @@
 // Copyright © 2015 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
+// package gtw_rtr_moke offers a gateway <-> router moke adapter that can be used to test a router
+// implementation.
 package gtw_rtr_moke
 
 import (
+	"fmt"
 	"github.com/thethingsnetwork/core"
 	"github.com/thethingsnetwork/core/lorawan/semtech"
 )
 
-type Adapter struct{}
-
-// New constructs a new Gateway-Router-Moke adapter
-func New(router core.Router, port uint) (*Adapter, error) {
-	return nil, nil
+type Adapter struct {
+	FailAck     bool
+	FailConnect bool
+	connected   bool
+	acks        map[core.GatewayAddress][]semtech.Packet
 }
 
-// Ack implements the core.GatewayRouterAdapter interface
-func (a *Adapter) Connect(router core.Router, port uint) error {
+// New constructs a new Gateway-Router-Moke adapter
+func New() (*Adapter, error) {
+	return &Adapter{
+		FailAck:     false,
+		FailConnect: false,
+		connected:   false,
+		acks:        make(map[core.GatewayAddress][]semtech.Packet),
+	}, nil
+}
+
+// Listen implements the core.Adapter interface
+func (a *Adapter) Listen(router core.Router, options interface{}) error {
+	if a.FailConnect {
+		return fmt.Errorf("Unable to establish connection")
+	}
+	a.connected = true
 	return nil
 }
 
 // Ack implements the core.GatewayRouterAdapter interface
 func (a *Adapter) Ack(router core.Router, packet semtech.Packet, gateway core.GatewayAddress) {
+	if a.FailAck {
+		router.HandleError(core.ErrAck(fmt.Errorf("Unable to ack the given packet")))
+		return
+	}
+	a.acks[gateway] = append(a.acks[gateway], packet)
+}
+
+func (a *Adapter) GetAcks(gateway core.GatewayAddress) []semtech.Packet {
+	return a.acks[gateway]
 }
