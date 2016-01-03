679a6e0a88cedafad38580f2cae0a15f5d8ffbac
ktorz <matthias.benkort@gmail.com>
2016-01-03 14:46:47 +0100

    [router] Make mock adapter simplier. Remove non necessary internal states and methods

--- testing/mock_adapters/gtw_rtr_mock/adapter.go
+++ testing/mock_adapters/gtw_rtr_mock/adapter.go
@@ -6,7 +6,6 @@
 package gtw_rtr_mock
 
 import (
-	"fmt"
 	"github.com/thethingsnetwork/core"
 	"github.com/thethingsnetwork/core/lorawan/semtech"
 )
@@ -14,8 +13,6 @@ import (
 type Adapter struct {
 	FailAck    bool                                     // If true, each call to Ack will fail with a core.ErrAck
 	FailListen bool                                     // If true, each call to Listen will return an error
-	connected  bool                                     // Indicate wether or not the Listen method has been called
-	router     core.Router                              // The router to which the adapter is connected
 	Acks       map[core.GatewayAddress][]semtech.Packet // Stores all packet send through Ack()
 }
 
@@ -31,29 +28,16 @@ func New() Adapter {
 // Listen implements the core.Adapter interface
 func (a *Adapter) Listen(router core.Router, options interface{}) error {
 	if a.FailListen {
-		return fmt.Errorf("Unable to establish connection")
+		return core.ErrBadOptions
 	}
-	a.router = router
 	return nil
 }
 
 // Ack implements the core.GatewayRouterAdapter interface
 func (a *Adapter) Ack(router core.Router, packet semtech.Packet, gateway core.GatewayAddress) error {
-	if a.router == nil {
-		return core.ErrNotInitialized
-	}
 	if a.FailAck {
 		return core.ErrInvalidPacket
 	}
 	a.Acks[gateway] = append(a.Acks[gateway], packet)
-	return nil
-}
-
-// Simulate sends the given fake packet to the router as it was received by the adapter
-func (a *Adapter) Simulate(packet semtech.Packet, gateway core.GatewayAddress) error {
-	if a.router == nil {
-		return core.ErrNotInitialized
-	}
-	a.router.HandleUplink(packet, gateway)
 	return nil
 }
