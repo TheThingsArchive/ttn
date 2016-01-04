718f336190d751198a3d7a45daee39fe3ccf3b37
ktorz <matthias.benkort@gmail.com>
2016-01-04 19:13:17 +0100

    [simulators.gateway] Add fake accessor for Network Session Key. Ideally, should be stored in ENV

--- core.go
+++ core.go
@@ -65,3 +65,7 @@ type RouterBrokerAdapter interface {
 	// ave been queried before and are known as dedicated brokers for the related end-device.
 	Forward(router Router, payload Payload, broAddrs ...BrokerAddress) error
 }
+
+func GetNwSKey() [16]byte {
+	return [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+}
