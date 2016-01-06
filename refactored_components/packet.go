c890a3cd03aac100a74a2b1e54e3d4f91e00919f
ktorz <matthias.benkort@gmail.com>
2016-01-06 15:50:50 +0100

    [broker] Start methods related to the packet structure

---
+++ refactored_components/packet.go
@@ -0,0 +1,17 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package components
+
+import (
+	"github.com/thethingsnetwork/core"
+	//"github.com/thethingsnetwork/core/lorawan"
+	"fmt"
+	"github.com/thethingsnetwork/core/semtech"
+)
+
+var ErrImpossibleConversion = fmt.Errorf("The given packet can't be converted")
+
+func ConvertSemtechPacket(p semtech.Packet) (core.Packet, error) {
+	return core.Packet{}, nil
+}
