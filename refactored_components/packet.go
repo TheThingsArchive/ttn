ccc6b6ed36c8d0e8f8c1499bbfae1425613f3da7
ktorz <matthias.benkort@gmail.com>
2016-01-06 19:36:10 +0100

    [broker] Implement convertRXPK for packet

--- refactored_components/packet.go
+++ refactored_components/packet.go
@@ -4,14 +4,50 @@
 package components
 
 import (
-	"github.com/thethingsnetwork/core"
-	//"github.com/thethingsnetwork/core/lorawan"
+	"encoding/base64"
 	"fmt"
+	"github.com/thethingsnetwork/core"
+	"github.com/thethingsnetwork/core/lorawan"
 	"github.com/thethingsnetwork/core/semtech"
+	"reflect"
 )
 
 var ErrImpossibleConversion = fmt.Errorf("The given packet can't be converted")
 
 func ConvertRXPK(p semtech.RXPK) (core.Packet, error) {
-	return core.Packet{}, nil
+	packet := core.Packet{}
+	if p.Data == nil {
+		return packet, ErrImpossibleConversion
+	}
+
+	encoded := *p.Data
+	switch len(encoded) % 4 {
+	case 2:
+		encoded += "=="
+	case 3:
+		encoded += "="
+	}
+
+	raw, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		return packet, err
+	}
+
+	payload := lorawan.NewPHYPayload(true)
+	if err = payload.UnmarshalBinary(raw); err != nil {
+		return packet, err
+	}
+
+	metadata := Metadata{}
+	rxpkValue := reflect.ValueOf(p)
+	rxpkStruct := rxpkValue.Type()
+	metas := reflect.ValueOf(&metadata).Elem()
+	for i := 0; i < rxpkStruct.NumField(); i += 1 {
+		field := rxpkStruct.Field(i).Name
+		if metas.FieldByName(field).CanSet() {
+			metas.FieldByName(field).Set(rxpkValue.Field(i))
+		}
+	}
+
+	return core.Packet{Metadata: &metadata, Payload: payload}, nil
 }
