70b63df689a0b07e91e2406b81b6b3b6833ce6b9
ktorz <matthias.benkort@gmail.com>
2016-01-07 18:23:48 +0100

    [broker] Refactor adapter tests. Make them more simple and readable

--- adapters/brk_hdl_http/adapter_test.go
+++ adapters/brk_hdl_http/adapter_test.go
@@ -26,147 +26,124 @@ func TestNewAdapter(t *testing.T) {
 	}
 
 	for _, test := range tests {
+		Desc(t, "Create new adapter bound to %d", test.Port)
 		_, err := NewAdapter(test.Port)
 		checkErrors(t, test.WantError, err)
 	}
 }
 
-type nextRegistrationTest struct {
-	AppId      string
-	AppUrl     string
-	DevAddr    string
-	NwsKey     string
-	WantResult nextRegistrationResult
-}
-
-type nextRegistrationResult struct {
-	Config  *core.Registration
-	AckNack core.AckNacker
-	Error   error
-}
-
 func TestNextRegistration(t *testing.T) {
-	tests := []nextRegistrationTest{
+	tests := []struct {
+		AppId      string
+		AppUrl     string
+		DevAddr    string
+		NwsKey     string
+		WantResult *core.Registration
+		WantError  error
+	}{
 		// Valid device address
 		{
 			AppId:   "appid",
 			AppUrl:  "myhandler.com:3000",
-			NwsKey:  "00112233445566778899aabbccddeeff",
+			NwsKey:  "000102030405060708090a0b0c0d0e0f",
 			DevAddr: "14aab0a4",
-			WantResult: nextRegistrationResult{
-				Config: &core.Registration{
-					DevAddr: lorawan.DevAddr([4]byte{14, 0xaa, 0xb0, 0xa4}),
-					Handler: core.Recipient{Id: "appid", Address: "myhandler.com:3000"},
-					NwsKey:  lorawan.AES128Key([16]byte{00, 11, 22, 33, 44, 55, 66, 77, 88, 99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}),
-				},
-				Error: nil,
+			WantResult: &core.Registration{
+				DevAddr: lorawan.DevAddr([4]byte{0x14, 0xaa, 0xb0, 0xa4}),
+				Handler: core.Recipient{Id: "appid", Address: "myhandler.com:3000"},
+				NwsKey:  lorawan.AES128Key([16]byte{0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf}),
 			},
+			WantError: nil,
 		},
 		// Invalid device address
 		{
-			AppId:   "appid",
-			AppUrl:  "myhandler.com:3000",
-			NwsKey:  "00112233445566778899aabbccddeeff",
-			DevAddr: "INVALID",
-			WantResult: nextRegistrationResult{
-				Config: nil,
-				Error:  nil,
-			},
+			AppId:      "appid",
+			AppUrl:     "myhandler.com:3000",
+			NwsKey:     "000102030405060708090a0b0c0d0e0f",
+			DevAddr:    "INVALID",
+			WantResult: nil,
+			WantError:  nil,
 		},
 		// Invalid nwskey address
 		{
-			AppId:   "appid",
-			AppUrl:  "myhandler.com:3000",
-			NwsKey:  "00112233445566778899af",
-			DevAddr: "14aaab0a4",
-			WantResult: nextRegistrationResult{
-				Config: nil,
-				Error:  nil,
-			},
+			AppId:      "appid",
+			AppUrl:     "myhandler.com:3000",
+			NwsKey:     "00112233445566778899af",
+			DevAddr:    "14aab0a4",
+			WantResult: nil,
+			WantError:  nil,
 		},
 	}
 
-	adapter, err := NewAdapter(3001)
-	adapter.logger = log.TestLogger{Tag: "BRK_HDL_ADAPTER", T: t}
-	client := &client{
-		adapter: "0.0.0.0:3001",
-		c:       http.Client{},
-		logger:  log.TestLogger{Tag: "http client", T: t},
-	}
+	adapter, err := NewAdapter(3001, log.TestLogger{Tag: "BRK_HDL_ADAPTER", T: t})
+	client := &client{adapter: "0.0.0.0:3001"}
 	<-time.After(time.Millisecond * 200)
 	if err != nil {
 		panic(err)
 	}
 
 	for _, test := range tests {
-		client.send(test.AppId, test.AppUrl, test.DevAddr, test.NwsKey)
-		res := make(chan nextRegistrationResult)
+		// Describe
+		Desc(t, "Trying to register %s -> %s, %s, %s", test.DevAddr, test.AppId, test.AppUrl, test.NwsKey)
+
+		// Build
+		gotErr := make(chan error)
+		gotConf := make(chan core.Registration)
+		go client.send(test.AppId, test.AppUrl, test.DevAddr, test.NwsKey)
+
+		// Operate
 		go func() {
-			config, an, err := adapter.NextRegistration()
-			res <- nextRegistrationResult{&config, an, err}
+			config, _, err := adapter.NextRegistration()
+			gotErr <- err
+			gotConf <- config
 		}()
 
+		// Check
 		select {
-		case result := <-res:
-			checkRegistrationResult(t, test.WantResult, result)
+		case err := <-gotErr:
+			checkErrors(t, test.WantError, err)
 		case <-time.After(time.Millisecond * 250):
-			checkRegistrationResult(t, test.WantResult, nextRegistrationResult{})
+			checkErrors(t, test.WantError, nil)
+		}
+
+		select {
+		case conf := <-gotConf:
+			checkRegistrationResult(t, test.WantResult, &conf)
+		case <-time.After(time.Millisecond * 250):
+			checkRegistrationResult(t, test.WantResult, nil)
 		}
 	}
 }
 
-func checkErrors(t *testing.T, want error, got error) bool {
+func checkErrors(t *testing.T, want error, got error) {
 	if want == got {
 		Ok(t, "Check errors")
-		return true
+		return
 	}
 	Ko(t, "Expected error to be {%v} but got {%v}", want, got)
-	return false
 }
 
-func checkRegistrationResult(t *testing.T, want nextRegistrationResult, got nextRegistrationResult) bool {
-	if !checkErrors(t, want.Error, got.Error) {
-		return false
-	}
-
-	if want.Config == nil {
-		if got.Error == nil || got.AckNack != nil {
-			Ko(t, "Was expecting no result but got %v", got.Config)
-			return false
-		}
-		Ok(t, "Check registration result")
-		return true
-	}
-
-	if !reflect.DeepEqual(*want.Config, *got.Config) {
-		Ko(t, "Received configuration doesn't match expectations\nWant: %v\nGot:  %v", *want.Config, *got.Config)
-		return false
-	}
-
-	if want.AckNack == nil {
-		Ko(t, "Received configuration with a nil AckNacker")
-		return false
+func checkRegistrationResult(t *testing.T, want, got *core.Registration) {
+	if !reflect.DeepEqual(want, got) {
+		Ko(t, "Received configuration doesn't match expectations")
+		return
 	}
 
 	Ok(t, "Check registration result")
-	return true
 }
 
 type client struct {
-	c       http.Client
-	logger  log.Logger
+	http.Client
 	adapter string
 }
 
-func (c *client) send(appId, appUrl, devAddr, nwsKey string) {
-	c.logger.Log("send request to %s", c.adapter)
+func (c *client) send(appId, appUrl, devAddr, nwsKey string) http.Response {
 	buf := new(bytes.Buffer)
 	if _, err := buf.WriteString(fmt.Sprintf(`{"app_id":"%s","app_url":"%s","nws_key":"%s"}`, appId, appUrl, nwsKey)); err != nil {
 		panic(err)
 	}
-	resp, err := c.c.Post(fmt.Sprintf("http://%s/end-device/%s", c.adapter, devAddr), "application/json", buf)
+	resp, err := c.Post(fmt.Sprintf("http://%s/end-device/%s", c.adapter, devAddr), "application/json", buf)
 	if err != nil {
 		panic(err)
 	}
-	c.logger.Log("response code: %d", resp.StatusCode)
+	return *resp
 }
