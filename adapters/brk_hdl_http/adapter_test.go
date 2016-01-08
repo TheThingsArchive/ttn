92b6f2f20e28ef80ffe7e7d722470cbc47949c9e
ktorz <matthias.benkort@gmail.com>
2016-01-08 14:02:47 +0100

    [broker] Write test for upadater Send() method

--- adapters/brk_hdl_http/adapter_test.go
+++ adapters/brk_hdl_http/adapter_test.go
@@ -8,7 +8,9 @@ import (
 	"fmt"
 	"github.com/thethingsnetwork/core"
 	"github.com/thethingsnetwork/core/lorawan"
+	components "github.com/thethingsnetwork/core/refactored_components"
 	"github.com/thethingsnetwork/core/utils/log"
+	"github.com/thethingsnetwork/core/utils/pointer"
 	. "github.com/thethingsnetwork/core/utils/testing"
 	"net/http"
 	"reflect"
@@ -118,6 +120,41 @@ func TestNextRegistration(t *testing.T) {
 
 // Send(p core.Packet, r ...core.Recipient) error
 func TestSend(t *testing.T) {
+	tests := []struct {
+		Packet      core.Packet
+		WantPayload string
+		WantError   error
+	}{
+		{
+			core.Packet{
+				Payload:  genPHYPayload("myData"),
+				Metadata: &components.Metadata{Rssi: pointer.Int(-20), Modu: pointer.String("LORA")},
+			},
+			`{"metadata":{"rssi":-20,"modu":"LORA"},"payload":"myData"}`,
+			nil,
+		},
+		{
+			core.Packet{
+				Payload:  lorawan.PHYPayload{},
+				Metadata: &components.Metadata{},
+			},
+			"",
+			ErrInvalidPacket,
+		},
+	}
+
+	s := genMockServer(3100)
+	adapter, err := NewAdapter(3101, log.TestLogger{Tag: "BRK_HDL_ADAPTER", T: t})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, test := range tests {
+		Desc(t, "Sending packet: %v", test.Packet)
+		err := adapter.Send(test.Packet, s.Recipient)
+		checkErrors(t, test.WantError, err)
+		checkSend(t, test.WantPayload, s)
+	}
 }
 
 func checkErrors(t *testing.T, want error, got error) {
@@ -137,11 +174,31 @@ func checkRegistrationResult(t *testing.T, want, got *core.Registration) {
 	Ok(t, "Check registration result")
 }
 
+func checkSend(t *testing.T, want string, s MockServer) {
+	select {
+	case got := <-s.Payloads:
+		if want != got {
+			Ko(t, "Expected payload %s to be sent but got %s", want, got)
+			return
+		}
+	case <-time.After(time.Millisecond * 100):
+		if want != "" {
+			Ko(t, "Expected payload %s to be sent but got nothing", want)
+			return
+		}
+	}
+	Ok(t, "Check send result")
+}
+
+// Operate utilities
+
+// Wrapper around the http client
 type client struct {
 	http.Client
 	adapter string
 }
 
+// send is a convinient helper to send HTTP from a handler to the adapter
 func (c *client) send(appId, appUrl, devAddr, nwsKey string) http.Response {
 	buf := new(bytes.Buffer)
 	if _, err := buf.WriteString(fmt.Sprintf(`{"app_id":"%s","app_url":"%s","nws_key":"%s"}`, appId, appUrl, nwsKey)); err != nil {
@@ -157,4 +214,80 @@ func (c *client) send(appId, appUrl, devAddr, nwsKey string) http.Response {
 		panic(err)
 	}
 	return *resp
+}
+
+// Build utilities
+
+type MockServer struct {
+	Recipient core.Recipient
+	Payloads  chan string
+}
+
+func genMockServer(port uint) MockServer {
+	addr := fmt.Sprintf("0.0.0.0:%s", port)
+	payloads := make(chan string)
+
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		body := make([]byte, 256)
+		n, err := req.Body.Read(body)
+		if err != nil {
+			panic(err)
+		}
+		payloads <- string(body[:n])
+	})
+
+	go func() {
+		server := http.Server{
+			Handler: serveMux,
+			Addr:    addr,
+		}
+		server.ListenAndServe()
+	}()
+
+	<-time.After(time.Millisecond * 50)
+
+	return MockServer{
+		Recipient: core.Recipient{
+			Address: addr,
+			Id:      "Mock server",
+		},
+		Payloads: payloads,
+	}
+}
+
+// Generate a Physical payload representing an uplink message
+func genPHYPayload(msg string) lorawan.PHYPayload {
+	nwkSKey := [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	appSKey := [16]byte{16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+
+	macPayload := lorawan.NewMACPayload(true)
+	macPayload.FHDR = lorawan.FHDR{
+		DevAddr: lorawan.DevAddr([4]byte{1, 2, 3, 4}),
+		FCtrl: lorawan.FCtrl{
+			ADR:       false,
+			ADRACKReq: false,
+			ACK:       false,
+		},
+		FCnt: 0,
+	}
+	macPayload.FPort = 10
+	macPayload.FRMPayload = []lorawan.Payload{&lorawan.DataPayload{Bytes: []byte(msg)}}
+
+	if err := macPayload.EncryptFRMPayload(appSKey); err != nil {
+		panic(err)
+	}
+
+	payload := lorawan.NewPHYPayload(true)
+	payload.MHDR = lorawan.MHDR{
+		MType: lorawan.ConfirmedDataUp,
+		Major: lorawan.LoRaWANR1,
+	}
+	payload.MACPayload = macPayload
+
+	if err := payload.SetMIC(nwkSKey); err != nil {
+		panic(err)
+	}
+
+	return payload
 }
