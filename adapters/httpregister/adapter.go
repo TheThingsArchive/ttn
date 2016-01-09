b6582ac306dd771f6dce8e8f501e30d283ce905a
ktorz <matthias.benkort@gmail.com>
2016-01-09 18:06:56 +0100

    [broker] rename package in httpregister

--- adapters/httpregister/adapter.go
+++ adapters/httpregister/adapter.go
@@ -1,7 +1,7 @@
 // Copyright © 2015 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
-package http
+package httpregister
 
 import (
 	"bytes"
@@ -11,6 +11,7 @@ import (
 	"github.com/thethingsnetwork/core"
 	"github.com/thethingsnetwork/core/utils/log"
 	"net/http"
+	"sync"
 )
 
 var ErrInvalidPort = fmt.Errorf("The given port is invalid")
@@ -47,28 +48,50 @@ func NewAdapter(port uint, parser Parser, loggers ...log.Logger) (*Adapter, erro
 
 // Send implements the core.Adapter interface
 func (a *Adapter) Send(p core.Packet, r ...core.Recipient) error {
-	metadata, err := json.Marshal(p.Metadata)
+	// Generate payload from core packet
+	m, err := json.Marshal(p.Metadata)
 	if err != nil {
 		return ErrInvalidPacket
 	}
-
-	payload, err := p.Payload.MarshalBinary()
+	pl, err := p.Payload.MarshalBinary()
 	if err != nil {
 		return ErrInvalidPacket
 	}
-	base64Payload := base64.StdEncoding.EncodeToString(payload)
+	payload := fmt.Sprintf(`{"payload":"%s","metadata":%s}`, base64.StdEncoding.EncodeToString(pl), m)
 
-	var errors []error
+	// Prepare ground for parrallel http request
+	nb := len(r)
+	cherr := make(chan error, nb)
+	wg := sync.WaitGroup{}
+	wg.Add(nb)
+
+	// Run each request
 	for _, recipient := range r {
-		buf := new(bytes.Buffer)
-		buf.Write([]byte(fmt.Sprintf(`{"payload":"%s","metadata":%s}`, base64Payload, metadata)))
-		a.log("Post to %v", recipient)
-		resp, err := http.Post(fmt.Sprintf("http://%s", recipient.Address.(string)), "application/json", buf)
-		if err != nil || resp.StatusCode != http.StatusOK {
-			errors = append(errors, err)
-		}
+		go func(recipient core.Recipient) {
+			defer wg.Done()
+			a.log("Post to %v", recipient)
+			buf := new(bytes.Buffer)
+			buf.Write([]byte(payload))
+			resp, err := http.Post(fmt.Sprintf("http://%s", recipient.Address.(string)), "application/json", buf)
+			if err != nil {
+				// Non-blocking, buffered
+				cherr <- err
+				return
+			}
+			if resp.StatusCode != http.StatusOK {
+				// Non-blocking, buffered
+				cherr <- fmt.Errorf("Unexpected response from server: %s (%d)", resp.Status, resp.StatusCode)
+				return
+			}
+		}(recipient)
 	}
 
+	// Wait for each request to be done, and return
+	wg.Wait()
+	var errors []error
+	for i := 0; i < len(cherr); i += 1 {
+		errors = append(errors, <-cherr)
+	}
 	if errors != nil {
 		return fmt.Errorf("Errors: %v", errors)
 	}
@@ -77,6 +100,7 @@ func (a *Adapter) Send(p core.Packet, r ...core.Recipient) error {
 
 // Next implements the core.Adapter interface
 func (a *Adapter) Next() (core.Packet, core.AckNacker, error) {
+	// NOTE not implemented
 	return core.Packet{}, nil, nil
 }
 
