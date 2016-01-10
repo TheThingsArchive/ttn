325e3aba2837c170043daa2a3f0fa2548f8d06d2
ktorz <matthias.benkort@gmail.com>
2016-01-10 23:06:10 +0100

    [broker] Add blank failing router tests as reminder

---
+++ components/router_test.go
@@ -0,0 +1,21 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package components
+
+import (
+	. "github.com/thethingsnetwork/core/utils/testing"
+	"testing"
+)
+
+func TestNewRouter(t *testing.T) {
+	Ko(t, "TODO")
+}
+
+func TestSendRouter(t *testing.T) {
+	Ko(t, "TODO")
+}
+
+func TestHandleUpRouter(t *testing.T) {
+	Ko(t, "TODO")
+}
