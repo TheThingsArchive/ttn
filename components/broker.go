ee732225672f035f3fb70207207937358812f144
ktorz <matthias.benkort@gmail.com>
2016-01-11 16:31:59 +0100

    [broker] Add New and log method to broker

--- components/broker.go
+++ components/broker.go
@@ -6,13 +6,27 @@ package components
 import (
 	"fmt"
 	"github.com/thethingsnetwork/core"
+	"github.com/thethingsnetwork/core/utils/log"
 )
 
-type Broker struct{}
-
-func NewBroker() (*Broker, error) {
-	return nil, nil
+type Broker struct {
+	loggers []log.Logger
+	db      addressKeeper
 }
+
+func NewBroker(loggers ...log.Logger) (*Broker, error) {
+	localDB, err := NewLocalDB(EXPIRY_DELAY)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Broker{
+		loggers: loggers,
+		db:      localDB,
+	}, nil
+}
+
 func (b *Broker) HandleUp(p core.Packet, an core.AckNacker, a core.Adapter) error {
 	return nil
 }
@@ -23,4 +37,10 @@ func (b *Broker) HandleDown(p core.Packet, an core.AckNacker, a core.Adapter) er
 
 func (b *Broker) Register(r core.Registration) error {
 	return nil
+}
+
+func (b *Broker) log(format string, i ...interface{}) {
+	for _, logger := range b.loggers {
+		logger.Log(format, i...)
+	}
 }
