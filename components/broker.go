71d96ce7dc2218f93d4de03f98518bdc9a579898
ktorz <matthias.benkort@gmail.com>
2016-01-11 16:23:33 +0100

    [broker] Prepare broker backbone

--- components/broker.go
+++ components/broker.go
@@ -4,74 +4,23 @@
 package components
 
 import (
+	"fmt"
 	"github.com/thethingsnetwork/core"
 )
 
 type Broker struct{}
 
-/* Scenario
-broker := components.NewBroker()
-upAdapter := adapters.brk_hdl_http
-downAdapter := adapters.brk_rtr_http
-
-downAdapter.Start("3000")
-upAdapter.Start("8080")
-
-// Handle registration coming from Handler
-go func() {
-	for {
-		handler, devAddr, nwskey, err := upAdapter.NextRegistration()
-		if err != nil {
-			// Do some error handling
-		}
-		err = broker.Register(handler, devAddr, nwskey)
-		if err != nil {
-			// Do some error handling
-		}
-	}
-}()
-
-// Handle response packet coming from Handler
-go func() {
-	for {
-		packet, an, error := upAdapter.Next()
-		if err != nil {
-			// Do some error handling
-		}
-		err = broker.Handle(packet, an)
-		if err != nil {
-			// Do some error handling
-		}
-	}
-}
-
-// Handle packet from router
-go func() {
-	for {
-		packet, an, error := downAdapter.Next()
-		if err != nil {
-			// Do some error handling
-		}
-		err = broker.Handle(packet, an)
-		if err != nil {
-			// Do some error handling
-		}
-	}
-}
-*/
-
-func NewBroker() {
-
-}
-
-func (b *Broker) NextUp() (*core.Packet, error) {
+func NewBroker() (*Broker, error) {
 	return nil, nil
 }
-
-func (b *Broker) NextDown() (*core.Packet, error) {
-	return nil, nil
+func (b *Broker) HandleUp(p core.Packet, an core.AckNacker, a core.Adapter) error {
+	return nil
 }
 
-func (b *Broker) Handle(p core.Packet, an core.AckNacker) error {
+func (b *Broker) HandleDown(p core.Packet, an core.AckNacker, a core.Adapter) error {
+	return fmt.Errorf("Not Implemented")
+}
+
+func (b *Broker) Register(r core.Registration) error {
 	return nil
 }
--- components/broker_test.go
+++ components/broker_test.go
@@ -4,35 +4,18 @@
 package components
 
 import (
-	"github.com/thethingsnetwork/core"
 	. "github.com/thethingsnetwork/core/utils/testing"
 	"testing"
 )
 
-type FakeAckNacker struct {
-	ackGot  core.Packet
-	nackGot core.Packet
+func TestNewBroker(t *testing.T) {
+	Ko(t, "TODO")
 }
 
-func (f *FakeAckNacker) Ack(p core.Packet) error {
-	f.ackGot = p
-	return nil
+func TestHandleUpBroker(t *testing.T) {
+	Ko(t, "TODO")
 }
 
-func (f *FakeAckNacker) Nack(p core.Packet) error {
-	f.nackGot = p
-	return nil
-}
-
-// The broker can handle an uplink packet
-func TestBrokerUplink(t *testing.T) {
-	// p = validFullMetaPacket()
-	// p = validPartialMetaPacket()
-	// p = packetWithoutPayload
-	// fake AckNacker ->
-	// broker.HandleUplink(p, an)
-	//
-	// checkAckNacker(p.Packet, An.got)
-
-	Ok(t, "Pending")
+func TestRegisterBroker(t *testing.T) {
+	Ko(t, "TODO")
 }
