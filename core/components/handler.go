3d526e1352906d82c148739b2ca39c7ecda86450
ktorz <matthias.benkort@gmail.com>
2016-01-26 18:10:26 +0100

    [handler] Add debug loglines in handler

--- core/components/handler.go
+++ core/components/handler.go
@@ -79,14 +79,17 @@ func (h *Handler) Register(reg core.Registration, an core.AckNacker) error {
 }
 
 func (h *Handler) HandleUp(p core.Packet, an core.AckNacker, upAdapter core.Adapter) error {
+	h.ctx.Debug("Handling new uplink packet")
 	partition, err := h.db.partition([]core.Packet{p})
 	if err != nil {
+		h.ctx.WithError(err).Debug("Unable to find entry")
 		an.Nack()
 		return err
 	}
 
 	fcnt, err := p.Fcnt()
 	if err != nil {
+		h.ctx.WithError(err).Debug("Unable to retrieve fcnt")
 		an.Nack()
 		return err
 	}
@@ -97,6 +100,7 @@ func (h *Handler) HandleUp(p core.Packet, an core.AckNacker, upAdapter core.Adap
 	buf.Write(partition[0].id[:]) // Partition is necessarily of length 1, associated to 1 packet, the same we gave
 	binary.Write(buf, binary.BigEndian, fcnt)
 	copy(id[:], buf.Bytes())
+	h.ctx.WithField("bundleId", id).Debug("Defining new bundle")
 	h.set <- uplinkBundle{
 		id:      id,
 		packet:  p,
@@ -108,12 +112,15 @@ func (h *Handler) HandleUp(p core.Packet, an core.AckNacker, upAdapter core.Adap
 	resp := <-chresp
 	switch resp.(type) {
 	case core.Packet:
+		h.ctx.WithField("bundleId", id).Debug("Received response with packet. Sending ack")
 		an.Ack(resp.(core.Packet))
 		return nil
 	case error:
+		h.ctx.WithField("bundleId", id).WithError(resp.(error)).Debug("Received response. Sending Nack")
 		an.Nack()
 		return resp.(error)
 	default:
+		h.ctx.WithField("bundleId", id).Debug("Received response. Sending ack")
 		an.Ack()
 		return nil
 	}
@@ -124,11 +131,15 @@ func (h *Handler) HandleDown(p core.Packet, an core.AckNacker, downAdapter core.
 }
 
 func (h *Handler) consumeBundles(chbundles <-chan []uplinkBundle) {
+	ctx := h.ctx.WithField("goroutine", "consumer")
+	ctx.Debug("Starting bundle consumer")
 	for bundles := range chbundles {
 		var packet *core.Packet
 		var sendToAdapter func(packet core.Packet) error
+		ctx.WithField("nb", len(bundles)).Debug("Consuming new bundles set")
 		for _, bundle := range bundles {
 			if packet == nil {
+				ctx.WithField("entry", bundle.entry).Debug("Preparing ground for given entry")
 				*packet = core.Packet{
 					Payload: bundle.packet.Payload,
 					Metadata: core.Metadata{
@@ -137,6 +148,7 @@ func (h *Handler) consumeBundles(chbundles <-chan []uplinkBundle) {
 				}
 				// The handler assumes payloads encrypted with AppSKey only !
 				if err := packet.Payload.DecryptMACPayload(bundle.entry.AppSKey); err != nil {
+					ctx.WithError(err).Debug("Unable to decrypt MAC Payload with given AppSKey")
 					for _, bundle := range bundles {
 						bundle.chresp <- err
 					}
@@ -157,6 +169,7 @@ func (h *Handler) consumeBundles(chbundles <-chan []uplinkBundle) {
 		}
 
 		err := sendToAdapter(*packet)
+		ctx.WithField("error", err).Debug("Sending to bundle adapter")
 		for _, bundle := range bundles {
 			bundle.chresp <- err
 		}
@@ -166,6 +179,9 @@ func (h *Handler) consumeBundles(chbundles <-chan []uplinkBundle) {
 // manageBuffers gather new incoming bundles that possess the same id
 // It then flushs them once a given delay has passed since the reception of the first bundle.
 func (h *Handler) manageBuffers(bundles chan<- []uplinkBundle, set <-chan uplinkBundle) {
+	ctx := h.ctx.WithField("goroutine", "bufferer")
+	ctx.Debug("Starting uplink packets buffering")
+
 	buffers := make(map[bundleId][]uplinkBundle)
 	alarm := make(chan bundleId)
 
@@ -175,10 +191,12 @@ func (h *Handler) manageBuffers(bundles chan<- []uplinkBundle, set <-chan uplink
 			b := buffers[id]
 			delete(buffers, id)
 			go func(b []uplinkBundle) { bundles <- b }(b)
+			ctx.WithField("bundleId", id).Debug("Alarm done. Consuming collected bundles")
 		case bundle := <-set:
 			b := append(buffers[bundle.id], bundle)
 			if len(b) == 1 {
 				go setAlarm(alarm, bundle.id, time.Millisecond*300)
+				ctx.WithField("bundleId", bundle.id).Debug("Starting buffering. New alarm set")
 			}
 			buffers[bundle.id] = b
 		}
