57305cfacf3fcebd7ec262b4a3d7cf58adec628b
Hylke Visser <htdvisser@gmail.com>
2016-01-31 17:30:04 +0100

    Add decent CLI interaction to Broker

--- integration/broker/main.go
+++ integration/broker/main.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"strconv"
 
@@ -18,9 +17,6 @@ import (
 )
 
 func main() {
-	// Parse options
-	routersPort, handlersPort := parseOptions()
-
 	// Create Logging Context
 	log.SetHandler(text.New(os.Stdout))
 	log.SetLevel(log.DebugLevel)
@@ -28,25 +24,28 @@ func main() {
 		"component": "Broker",
 	})
 
+	// Parse options
+	routersPort, handlersPort := parseOptions()
+
 	// Instantiate all components
 	rtrAdapter, err := http.NewAdapter(uint(routersPort), http.JSONPacketParser{}, ctx.WithField("tag", "Routers Adapter"))
 	if err != nil {
-		panic(err)
+		ctx.WithError(err).Fatal("Could not start Routers Adapter")
 	}
 
 	hdlHTTPAdapter, err := http.NewAdapter(uint(handlersPort), http.JSONPacketParser{}, ctx.WithField("tag", "Handlers Adapter"))
 	if err != nil {
-		panic(err)
+		ctx.WithError(err).Fatal("Could not start Handlers Adapter")
 	}
 
 	hdlAdapter, err := pubsub.NewAdapter(hdlHTTPAdapter, pubsub.HandlerParser{}, ctx.WithField("tag", "Handlers Adapter"))
 	if err != nil {
-		panic(err)
+		ctx.WithError(err).Fatal("Could not start Handlers Adapter")
 	}
 
 	broker, err := components.NewBroker(ctx.WithField("tag", "Broker"))
 	if err != nil {
-		panic(err)
+		ctx.WithError(err).Fatal("Could not start Broker")
 	}
 
 	// Bring the service to life
@@ -56,12 +55,12 @@ func main() {
 		for {
 			packet, an, err := rtrAdapter.Next()
 			if err != nil {
-				fmt.Println(err)
+				ctx.WithError(err).Error("Could not retrieve uplink")
 				continue
 			}
 			go func(packet Packet, an AckNacker) {
 				if err := broker.HandleUp(packet, an, hdlAdapter); err != nil {
-					fmt.Println(err)
+					ctx.WithError(err).Error("Could not process uplink")
 				}
 			}(packet, an)
 		}
@@ -72,12 +71,12 @@ func main() {
 		for {
 			reg, an, err := hdlAdapter.NextRegistration()
 			if err != nil {
-				fmt.Println(err)
+				ctx.WithError(err).Error("Could not retrieve registration")
 				continue
 			}
 			go func(reg Registration, an AckNacker) {
 				if err := broker.Register(reg, an); err != nil {
-					fmt.Println(err)
+					ctx.WithError(err).Error("Could not process registration")
 				}
 			}(reg, an)
 		}
@@ -89,19 +88,30 @@ func main() {
 func parseOptions() (routersPort uint64, handlersPort uint64) {
 	var routersPortFlag string
 	var handlersPortFlag string
-	flag.StringVar(&routersPortFlag, "routers-port", "", "TCP port on which the broker should listen to for incoming uplink packets.")
-	flag.StringVar(&handlersPortFlag, "handlers-port", "", "TCP port on which the broker should listen to for incoming registrations and downlink packet.")
-	flag.Parse()
+
+	flags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	flags.StringVar(&routersPortFlag, "routers-port", "", "TCP port on which the broker should listen to for incoming uplink packets.")
+	flags.StringVar(&handlersPortFlag, "handlers-port", "", "TCP port on which the broker should listen to for incoming registrations and downlink packet.")
+
+	flags.Parse(os.Args[1:])
 
 	var err error
+
+	if routersPortFlag == "" {
+		log.Fatal("No Router listen port supplied using the -routers-port flag")
+	}
 	routersPort, err = strconv.ParseUint(routersPortFlag, 10, 64)
 	if err != nil {
-		panic(err)
+		log.Fatal("Could not parse the value for -routers-port")
 	}
 
+	if handlersPortFlag == "" {
+		log.Fatal("No Handler listen port supplied using the -handlers-port flag")
+	}
 	handlersPort, err = strconv.ParseUint(handlersPortFlag, 10, 64)
 	if err != nil {
-		panic(err)
+		log.Fatal("Could not parse the value for -handlers-port")
 	}
 
 	return
