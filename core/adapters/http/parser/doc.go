274fd7c7b2aca87883fbcc7a72f8366789b3075d
ktorz <matthias.benkort@gmail.com>
2016-02-02 15:44:43 +0100

    [doc] Define package-level doc for http adapters, parsers and whatever

---
+++ core/adapters/http/parser/doc.go
@@ -0,0 +1,6 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+// Package parser defines several Packet and Registration parsers that raise the abstraction level
+// for adapters.
+package parser
