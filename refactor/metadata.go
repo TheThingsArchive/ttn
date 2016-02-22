6efd005cce9616fff24b5e9911e2c552a9b2a9d9
ktorz <matthias.benkort@gmail.com>
2016-02-22 15:00:43 +0100

    [refactor] Move metadata and old packet into RPacket

---
+++ refactor/metadata.go
@@ -0,0 +1,92 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package refactor
+
+import (
+	"encoding/json"
+	"time"
+
+	. "github.com/TheThingsNetwork/ttn/core/errors"
+	"github.com/TheThingsNetwork/ttn/semtech"
+	"github.com/TheThingsNetwork/ttn/utils/errors"
+	"github.com/TheThingsNetwork/ttn/utils/pointer"
+)
+
+// metadata allows us to inherit Metadata in metadataProxy but only by extending the exported
+// attributes of Metadata such that, they are still parsed by the json.Marshaller /
+// json.Unmarshaller though we do not end up with a recursive hellish error.
+type metadata Metadata
+
+// MarshalJSON implements the json.Marshal interface
+func (m Metadata) MarshalJSON() ([]byte, error) {
+	var d *semtech.Datrparser
+	var t *semtech.Timeparser
+
+	// Handle datr which can be either an uint or string depending of the modulation
+	if m.Datr != nil {
+		d = new(semtech.Datrparser)
+		if m.Modu != nil && *m.Modu == "FSK" {
+			*d = semtech.Datrparser{Kind: "uint", Value: *m.Datr}
+		} else {
+			*d = semtech.Datrparser{Kind: "string", Value: *m.Datr}
+		}
+	}
+
+	// Time :'( ... By default, we mashall them as RFC3339Nano and unmarshall them the best we can.
+	if m.Time != nil {
+		t = new(semtech.Timeparser)
+		*t = semtech.Timeparser{Layout: time.RFC3339Nano, Value: m.Time}
+	}
+
+	data, err := json.Marshal(metadataProxy{
+		metadata: metadata(m),
+		Datr:     d,
+		Time:     t,
+	})
+
+	if err != nil {
+		err = errors.New(ErrInvalidStructure, err)
+	}
+
+	return data, err
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (m *Metadata) UnmarshalJSON(raw []byte) error {
+	if m == nil {
+		return errors.New(ErrInvalidStructure, "Cannot unmarshal nil Metadata")
+	}
+
+	proxy := metadataProxy{}
+	if err := json.Unmarshal(raw, &proxy); err != nil {
+		return errors.New(ErrInvalidStructure, err)
+	}
+	*m = Metadata(proxy.metadata)
+	if proxy.Time != nil {
+		m.Time = proxy.Time.Value
+	}
+
+	if proxy.Datr != nil {
+		m.Datr = &proxy.Datr.Value
+	}
+
+	return nil
+}
+
+// String implements the io.Stringer interface
+func (m Metadata) String() string {
+	return pointer.DumpPStruct(m, false)
+}
+
+// type metadataProxy is used to conveniently marshal and unmarshal Metadata structure.
+//
+// Datr field could be either string or uint depending on the Modu field.
+// Time field could be parsed in a lot of different way depending of the time format.
+// This proxy make sure that everything is marshaled and unmarshaled to the right thing and allow
+// the Metadata struct to be user-friendly.
+type metadataProxy struct {
+	metadata
+	Datr *semtech.Datrparser `json:"datr,omitempty"`
+	Time *semtech.Timeparser `json:"time,omitempty"`
+}
---
+++ refactor/rpacket.go
@@ -0,0 +1,159 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package refactor
+
+import (
+	"encoding"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+
+	. "github.com/TheThingsNetwork/ttn/core/errors"
+	"github.com/TheThingsNetwork/ttn/utils/errors"
+	"github.com/brocaar/lorawan"
+)
+
+// type RPacket materializes packets manipulated by the router and corresponding adapter handlers
+type RPacket struct {
+	metadata Metadata
+	payload  lorawan.PHYPayload
+}
+
+func NewRPacket(payload lorawan.PHYPayload, metadata Metadata) RPacket {
+	return RPacket{
+		metadata: metadata,
+		payload:  payload,
+	}
+}
+
+// FCnt implements the core.Packet interface
+func (p RPacket) FCnt() (uint32, error) {
+	if p.payload.MACPayload == nil {
+		return 0, errors.New(ErrInvalidStructure, "MACPayload should not be empty")
+	}
+
+	macpayload, ok := p.payload.MACPayload.(*lorawan.MACPayload)
+	if !ok {
+		return 0, errors.New(ErrInvalidStructure, "Packet does not carry a MACPayload")
+	}
+
+	return macpayload.FHDR.FCnt, nil
+}
+
+// Payload implements the core.Packet interface
+func (p RPacket) Payload() encoding.BinaryMarshaler {
+	return p.payload
+}
+
+// Metadata implements the core.Packet interface
+func (p RPacket) Metadata() []Metadata {
+	return []Metadata{p.metadata}
+}
+
+// AppEUI implements the core.Packet interface
+func (p RPacket) AppEUI() (lorawan.EUI64, error) {
+	return lorawan.EUI64{}, errors.New(ErrNotSupported, "AppEUI not supported for RPacket")
+}
+
+// DevEUI implements the core.Packet interface
+func (p RPacket) DevEUI() (lorawan.EUI64, error) {
+	if p.payload.MACPayload == nil {
+		return lorawan.EUI64{}, errors.New(ErrInvalidStructure, "MACPAyload should not be empty")
+	}
+
+	macpayload, ok := p.payload.MACPayload.(*lorawan.MACPayload)
+	if !ok {
+		return lorawan.EUI64{}, errors.New(ErrInvalidStructure, "Packet does not carry a MACPayload")
+	}
+
+	var devEUI lorawan.EUI64
+	copy(devEUI[4:], macpayload.FHDR.DevAddr[:])
+	return devEUI, nil
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface
+func (p RPacket) MarshalBinary() ([]byte, error) {
+	return json.Marshal(p)
+}
+
+// MarshalBinary implements the encoding.BinaryUnMarshaler interface
+func (p *RPacket) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, p)
+}
+
+// MarshalJSON implements the json.Marshaler interface
+func (p RPacket) MarshalJSON() ([]byte, error) {
+	rawMetadata, err := json.Marshal(p.metadata)
+	if err != nil {
+		return nil, errors.New(ErrInvalidStructure, err)
+	}
+	rawPayload, err := p.payload.MarshalBinary()
+	if err != nil {
+		return nil, errors.New(ErrInvalidStructure, err)
+	}
+	strPayload := base64.StdEncoding.EncodeToString(rawPayload)
+	return []byte(fmt.Sprintf(`{"payload":"%s","metadata":%s}`, strPayload, string(rawMetadata))), nil
+}
+
+// UnmarshalJSON impements the json.Unmarshaler interface
+func (p *RPacket) UnmarshalJSON(raw []byte) error {
+	if p == nil {
+		return errors.New(ErrInvalidStructure, "Cannot unmarshal a nil packet")
+	}
+
+	// The payload is a bit tricky to unmarshal as we do not know if its an uplink or downlink
+	// packet. Thus, we'll assume it's an uplink packet (because that's the case most of the time)
+	// and check whether or not the unmarshalling process was okay.
+	var proxy struct {
+		Payload  string `json:"payload"`
+		Metadata Metadata
+	}
+
+	err := json.Unmarshal(raw, &proxy)
+	if err != nil {
+		return errors.New(ErrInvalidStructure, err)
+	}
+
+	rawPayload, err := base64.StdEncoding.DecodeString(proxy.Payload)
+	if err != nil {
+		return errors.New(ErrInvalidStructure, err)
+	}
+
+	payload := lorawan.NewPHYPayload(true) // true -> uplink
+	if err := payload.UnmarshalBinary(rawPayload); err != nil {
+		return errors.New(ErrInvalidStructure, err)
+	}
+
+	// Now, we check the nature of the decoded payload
+	switch payload.MHDR.MType.String() {
+	case "JoinAccept":
+		fallthrough
+	case "UnconfirmedDataDown":
+		fallthrough
+	case "ConfirmedDataDown":
+		// JoinAccept, UnconfirmedDataDown and ConfirmedDataDown are all downlink messages.
+		// We thus have to unmarshall properly
+		payload = lorawan.NewPHYPayload(false) // false -> downlink
+		if err := payload.UnmarshalBinary(rawPayload); err != nil {
+			return errors.New(ErrInvalidStructure, err)
+		}
+	case "JoinRequest":
+		fallthrough
+	case "UnconfirmedDataUp":
+		fallthrough
+	case "ConfirmedDataUp":
+		// JoinRequest, UnconfirmedDataUp and ConfirmedDataUp are all uplink messages.
+		// There's nothing to do, we've already handled them.
+
+	case "Proprietary":
+		// Proprietary can be either downlink or uplink. Right now, we do not have any message of
+		// that type and thus, we just don't know how to handle them. Let's throw an error.
+		return errors.New(ErrInvalidStructure, "Unsupported MType 'Proprietary'")
+	}
+
+	// Packet = Payload + Metadata
+	p.payload = payload
+	p.metadata = proxy.Metadata
+	return nil
+}
