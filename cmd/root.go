68678240ad1f539836580543bd112a1b5a69054f
Hylke Visser <htdvisser@gmail.com>
2016-02-22 16:18:23 +0100

    [refactor] [cli] Add main and version command

---
+++ cmd/root.go
@@ -0,0 +1,71 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/cli"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var ctx log.Interface
+
+// RootCmd is executed when ttn is executed without a subcommand
+var RootCmd = &cobra.Command{
+	Use:   "ttn",
+	Short: "The Things Network",
+	Long:  `The Things Network's backend server`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		var logLevel = log.InfoLevel
+		if viper.GetBool("debug") {
+			logLevel = log.DebugLevel
+		}
+		ctx = &log.Logger{
+			Level:   logLevel,
+			Handler: cli.New(os.Stdout),
+		}
+	},
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default \"$HOME/.ttn.yaml\")")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".ttn")  // name of config file (without extension)
+	viper.AddConfigPath("$HOME") // adding home directory as first search path
+	viper.SetEnvPrefix("ttn")    // set environment prefix
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv() // read in environment variables that match
+
+	viper.BindEnv("debug")
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
