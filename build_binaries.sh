dbcfc3a7617f81b2e234808e2f89d0be51b9fe75
Hylke Visser <htdvisser@gmail.com>
2016-02-22 17:27:37 +0100

    Don't build for arm
    Hopefully a temporary fix because jacobsa/crypto currently doesn't build on arm

--- build_binaries.sh
+++ build_binaries.sh
@@ -65,7 +65,6 @@ else
   build_release darwin amd64
   build_release linux 386
   build_release linux amd64
-  build_release linux arm
   build_release windows 386
   build_release windows amd64
 fi
