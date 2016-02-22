36c4ab8369b37842bbf7f2ea9c27f157b30edb04
ktorz <matthias.benkort@gmail.com>
2016-02-22 22:23:32 +0100

    [refactor] Add package root doc

---
+++ refactor/adapters/http/doc.go
@@ -0,0 +1,11 @@
+// Copyright © 2015 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+// Package http provides adapter implementations which run on top of http.
+//
+// The different protocols and mechanisms used are defined in the following document:
+// https://github.com/TheThingsNetwork/ttn/blob/develop/documents/protocols/protocols.md
+//
+// The basic http adapter module can be used as a brick to build something bigger by registering
+// specific endpoints.
+package http
