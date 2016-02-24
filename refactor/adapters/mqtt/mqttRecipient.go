35bfcde35666db38ae63215337d5dad5ea158a40
ktorz <matthias.benkort@gmail.com>
2016-02-24 11:16:27 +0100

    [refactor] Implements mqtt recipient

--- refactor/adapters/mqtt/mqttRecipient.go
+++ refactor/adapters/mqtt/mqttRecipient.go
@@ -5,11 +5,57 @@ package mqtt
 
 import (
 	"encoding"
+
+	. "github.com/TheThingsNetwork/ttn/core/errors"
+	"github.com/TheThingsNetwork/ttn/utils/errors"
+	"github.com/TheThingsNetwork/ttn/utils/readwriter"
 )
 
 // MqttRecipient describes recipient manipulated by the mqtt adapter
 type MqttRecipient interface {
 	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
 	TopicUp() string
 	TopicDown() string
+}
+
+// mqttRecipient implements the MqttRecipient interface
+type mqttRecipient struct {
+	up   string
+	down string
+}
+
+// NewRecipient creates a new MQTT recipient from two topics
+func NewRecipient(up string, down string) MqttRecipient {
+	return &mqttRecipient{up: up, down: down}
+}
+
+// TopicUp implements the MqttRecipient interface
+func (r mqttRecipient) TopicUp() string {
+	return r.up
+}
+
+// TopicDown implements the MqttRecipient interface
+func (r mqttRecipient) TopicDown() string {
+	return r.down
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface
+func (r mqttRecipient) MarshalBinary() ([]byte, error) {
+	rw := readwriter.New(nil)
+	rw.Write(r.up)
+	rw.Write(r.down)
+	return rw.Bytes()
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface
+func (r *mqttRecipient) UnmarshalBinary(data []byte) error {
+	if r == nil {
+		return errors.New(ErrInvalidStructure, "Cannot unmarshal nil structure")
+	}
+
+	rw := readwriter.New(data)
+	rw.Read(func(data []byte) { r.up = string(data) })
+	rw.Read(func(data []byte) { r.down = string(data) })
+	return rw.Err()
 }
