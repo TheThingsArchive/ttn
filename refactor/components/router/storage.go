58e21172a3136ba97ca7116b664e2a7395f16f16
ktorz <matthias.benkort@gmail.com>
2016-02-28 11:51:41 +0100

    [refactor] implements router_storage constructor and backbone

--- refactor/components/router/storage.go
+++ refactor/components/router/storage.go
@@ -4,16 +4,50 @@
 package router
 
 import (
+	"time"
+
 	. "github.com/TheThingsNetwork/ttn/refactor"
+	"github.com/TheThingsNetwork/ttn/utils/errors"
+	dbutil "github.com/TheThingsNetwork/ttn/utils/storage"
 	"github.com/brocaar/lorawan"
 )
 
 type Storage interface {
 	Lookup(devEUI lorawan.EUI64) (entry, error)
 	Store(reg Registration) error
-	Close() error
 }
 
 type entry struct {
 	Recipient Recipient
+	until     time.Time
+}
+
+type storage struct {
+	dbutil.Interface
+	Name string
+}
+
+// newStorage creates a new internal storage for the router
+func newStorage(name string, delay time.Duration) (Storage, error) {
+	itf, err := dbutil.New(name)
+	if err != nil {
+		return nil, errors.New(errors.Operational, err)
+	}
+
+	tableName := "brokers"
+	if err := itf.Init(tableName); err != nil {
+		return nil, errors.New(errors.Operational, err)
+	}
+
+	return storage{Interface: itf, Name: tableName}, nil
+}
+
+// Lookup implements the router.Storage interface
+func (s storage) Lookup(devEUI lorawan.EUI64) (entry, error) {
+	return entry{}, nil
+}
+
+// Store implements the router.Storage interface
+func (s storage) Store(reg Registration) error {
+	return nil
 }
