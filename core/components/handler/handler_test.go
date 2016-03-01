f2495c45cd095ec69836bd6a4ca2841588050b75
ktorz <matthias.benkort@gmail.com>
2016-03-01 10:12:14 +0100

    [test-handler] Remove unecessary lines to make travis build

--- core/components/handler/handler_test.go
+++ core/components/handler/handler_test.go
@@ -2,18 +2,3 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
 package handler
-
-import (
-	"testing"
-
-	// . "github.com/TheThingsNetwork/ttn/utils/testing"
-)
-
-func TestRegister(t *testing.T) {
-}
-
-func TestHandleDown(t *testing.T) {
-}
-
-func TestHandleUp(t *testing.T) {
-}
