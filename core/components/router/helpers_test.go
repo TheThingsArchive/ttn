018f0633d017257bad2409b11f373b31d24baad9
ktorz <matthias.benkort@gmail.com>
2016-03-02 16:04:13 +0100

    [test-router] Implement test for HandleUp() method in router

--- core/components/router/helpers_test.go
+++ core/components/router/helpers_test.go
@@ -10,8 +10,55 @@ import (
 	. "github.com/TheThingsNetwork/ttn/core"
 	. "github.com/TheThingsNetwork/ttn/core/mocks"
 	. "github.com/TheThingsNetwork/ttn/utils/testing"
+	"github.com/brocaar/lorawan"
 )
 
+// ----- BUILD utilities
+func newRPacket(rawDevAddr [4]byte, payload string, gatewayId []byte) RPacket {
+	var devAddr lorawan.DevAddr
+	copy(devAddr[:], rawDevAddr[:])
+
+	macPayload := lorawan.NewMACPayload(true)
+	macPayload.FHDR.DevAddr = devAddr
+	macPayload.FRMPayload = []lorawan.Payload{&lorawan.DataPayload{Bytes: []byte(payload)}}
+	macPayload.FPort = 1
+	phyPayload := lorawan.NewPHYPayload(true)
+	phyPayload.MACPayload = macPayload
+	phyPayload.MHDR = lorawan.MHDR{
+		MType: lorawan.UnconfirmedDataUp,
+		Major: lorawan.LoRaWANR1,
+	}
+
+	packet, err := NewRPacket(phyPayload, gatewayId, Metadata{})
+	if err != nil {
+		panic(err)
+	}
+	return packet
+}
+
+func newBPacket(rawDevAddr [4]byte, payload string) BPacket {
+	var devAddr lorawan.DevAddr
+	copy(devAddr[:], rawDevAddr[:])
+
+	macPayload := lorawan.NewMACPayload(true)
+	macPayload.FHDR.DevAddr = devAddr
+	macPayload.FRMPayload = []lorawan.Payload{&lorawan.DataPayload{Bytes: []byte(payload)}}
+	macPayload.FPort = 1
+	phyPayload := lorawan.NewPHYPayload(true)
+	phyPayload.MACPayload = macPayload
+	phyPayload.MHDR = lorawan.MHDR{
+		MType: lorawan.UnconfirmedDataUp,
+		Major: lorawan.LoRaWANR1,
+	}
+
+	packet, err := NewBPacket(phyPayload, Metadata{})
+	if err != nil {
+		panic(err)
+	}
+	return packet
+}
+
+// ----- CHECK utilities
 func CheckEntries(t *testing.T, want entry, got entry) {
 	tmin := want.until.Add(-time.Second)
 	tmax := want.until.Add(time.Second)
