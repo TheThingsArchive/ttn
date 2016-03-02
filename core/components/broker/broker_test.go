1fd97c0d11834b1732078b1d85ef41f5a3c499b2
ktorz <matthias.benkort@gmail.com>
2016-03-02 18:26:58 +0100

    [test-broker] Implement test for HandleUp() method

--- core/components/broker/broker_test.go
+++ core/components/broker/broker_test.go
@@ -12,6 +12,7 @@ import (
 	. "github.com/TheThingsNetwork/ttn/utils/errors/checks"
 	"github.com/TheThingsNetwork/ttn/utils/pointer"
 	. "github.com/TheThingsNetwork/ttn/utils/testing"
+	"github.com/brocaar/lorawan"
 )
 
 func TestRegister(t *testing.T) {
@@ -98,5 +99,243 @@ func TestHandleDown(t *testing.T) {
 }
 
 func TestHandleUp(t *testing.T) {
+	{
+		Desc(t, "Send an unknown packet")
 
+		// Build
+		an := NewMockAckNacker()
+		adapter := NewMockAdapter()
+		store := newMockStorage()
+		store.Failures["Lookup"] = errors.New(errors.Behavioural, "Mock Error: Not Found")
+		data, _ := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Payload",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		).MarshalBinary()
+
+		// Operate
+		broker := New(store, GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		CheckErrors(t, pointer.String(string(errors.Behavioural)), err)
+		CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStore)
+		CheckSent(t, nil, adapter.InSendPacket)
+		CheckRecipients(t, nil, adapter.InSendRecipients)
+	}
+
+	// -------------------
+
+	{
+		Desc(t, "Send an invalid packet")
+
+		// Build
+		an := NewMockAckNacker()
+		adapter := NewMockAdapter()
+		store := newMockStorage()
+
+		// Operate
+		broker := New(store, GetLogger(t, "Broker"))
+		err := broker.HandleUp([]byte{1, 2, 3}, an, adapter)
+
+		// Check
+		CheckErrors(t, pointer.String(string(errors.Structural)), err)
+		CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStore)
+		CheckSent(t, nil, adapter.InSendPacket)
+		CheckRecipients(t, nil, adapter.InSendRecipients)
+	}
+
+	// -------------------
+
+	{
+		Desc(t, "Send packet, get 2 entries, no valid MIC")
+
+		// Build
+		an := NewMockAckNacker()
+		adapter := NewMockAdapter()
+		store := newMockStorage()
+		store.OutLookup = []entry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				DevEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}),
+			},
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 5, 5, 5, 5}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 6, 6, 11, 12, 13, 14, 12, 16}),
+			},
+		}
+		data, _ := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Payload",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		).MarshalBinary()
+
+		// Operate
+		broker := New(store, GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		CheckErrors(t, pointer.String(string(errors.Behavioural)), err)
+		CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStore)
+		CheckSent(t, nil, adapter.InSendPacket)
+		CheckRecipients(t, nil, adapter.InSendRecipients)
+	}
+
+	// -------------------
+
+	{
+		Desc(t, "Send packet, get 2 entries, 1 valid MIC | No downlink")
+
+		// Build
+		an := NewMockAckNacker()
+		recipient := NewMockRecipient()
+		adapter := NewMockAdapter()
+		adapter.OutSend = nil
+		adapter.OutGetRecipient = recipient
+		store := newMockStorage()
+		store.OutLookup = []entry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				DevEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}),
+			},
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 2, 3, 2, 3}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8}),
+			},
+		}
+		bpacket := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Payload",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		)
+		data, _ := bpacket.MarshalBinary()
+		hpacket, _ := NewHPacket(
+			store.OutLookup[1].AppEUI,
+			store.OutLookup[1].DevEUI,
+			bpacket.Payload(),
+			bpacket.Metadata(),
+		)
+
+		// Operate
+		broker := New(store, GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		CheckErrors(t, nil, err)
+		CheckAcks(t, true, an.InAck)
+		CheckRegistrations(t, nil, store.InStore)
+		CheckSent(t, hpacket, adapter.InSendPacket)
+		CheckRecipients(t, []Recipient{recipient}, adapter.InSendRecipients)
+	}
+
+	// -------------------
+
+	{
+		Desc(t, "Send packet, get 2 entries, 1 valid MIC | Fails to get recipient")
+
+		// Build
+		an := NewMockAckNacker()
+		adapter := NewMockAdapter()
+		adapter.OutSend = nil
+		adapter.Failures["GetRecipient"] = errors.New(errors.Structural, "Mock Error: Unable to get recipient")
+		store := newMockStorage()
+		store.OutLookup = []entry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				DevEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}),
+			},
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 2, 3, 2, 3}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8}),
+			},
+		}
+		bpacket := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Payload",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		)
+		data, _ := bpacket.MarshalBinary()
+
+		// Operate
+		broker := New(store, GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		CheckErrors(t, pointer.String(string(errors.Structural)), err)
+		CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStore)
+		CheckSent(t, nil, adapter.InSendPacket)
+		CheckRecipients(t, nil, adapter.InSendRecipients)
+	}
+
+	// -------------------
+
+	{
+		Desc(t, "Send packet, get 2 entries, 1 valid MIC | Fails to send")
+
+		// Build
+		an := NewMockAckNacker()
+		recipient := NewMockRecipient()
+		adapter := NewMockAdapter()
+		adapter.OutGetRecipient = recipient
+		adapter.Failures["Send"] = errors.New(errors.Operational, "Mock Error: Unable to send")
+		store := newMockStorage()
+		store.OutLookup = []entry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				DevEUI:    lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}),
+			},
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 2, 3, 2, 3}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8}),
+			},
+		}
+		bpacket := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Payload",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		)
+		data, _ := bpacket.MarshalBinary()
+		hpacket, _ := NewHPacket(
+			store.OutLookup[1].AppEUI,
+			store.OutLookup[1].DevEUI,
+			bpacket.Payload(),
+			bpacket.Metadata(),
+		)
+
+		// Operate
+		broker := New(store, GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		CheckErrors(t, pointer.String(string(errors.Operational)), err)
+		CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStore)
+		CheckSent(t, hpacket, adapter.InSendPacket)
+		CheckRecipients(t, []Recipient{recipient}, adapter.InSendRecipients)
+	}
 }
--- core/components/broker/helpers_test.go
+++ core/components/broker/helpers_test.go
@@ -8,8 +8,40 @@ import (
 
 	. "github.com/TheThingsNetwork/ttn/core"
 	. "github.com/TheThingsNetwork/ttn/core/mocks"
-	//"github.com/brocaar/lorawan"
+	"github.com/brocaar/lorawan"
 )
+
+// ----- BUILD utilities
+func newBPacket(rawDevAddr [4]byte, payload string, nwkSKey [16]byte, fcnt uint32) BPacket {
+	var devAddr lorawan.DevAddr
+	copy(devAddr[:], rawDevAddr[:])
+
+	macPayload := lorawan.NewMACPayload(true)
+	macPayload.FHDR = lorawan.FHDR{
+		FCnt:    fcnt,
+		DevAddr: devAddr,
+	}
+	macPayload.FRMPayload = []lorawan.Payload{&lorawan.DataPayload{Bytes: []byte(payload)}}
+	macPayload.FPort = 1
+	phyPayload := lorawan.NewPHYPayload(true)
+	phyPayload.MACPayload = macPayload
+	phyPayload.MHDR = lorawan.MHDR{
+		MType: lorawan.UnconfirmedDataUp,
+		Major: lorawan.LoRaWANR1,
+	}
+
+	var key lorawan.AES128Key
+	copy(key[:], nwkSKey[:])
+	if err := phyPayload.SetMIC(key); err != nil {
+		panic(err)
+	}
+
+	packet, err := NewBPacket(phyPayload, Metadata{})
+	if err != nil {
+		panic(err)
+	}
+	return packet
+}
 
 // ----- CHECK utilities
 func CheckEntries(t *testing.T, want []entry, got []entry) {
