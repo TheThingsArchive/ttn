c59a93c86e3467d0bde44b93697a1d29190df16f
ktorz <matthias.benkort@gmail.com>
2016-03-03 18:00:18 +0100

    [dutycycle] Start implementing a duty cycle manager

---
+++ core/components/router/dutyManager.go
@@ -0,0 +1,155 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package router
+
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/TheThingsNetwork/ttn/utils/errors"
+	dbutil "github.com/TheThingsNetwork/ttn/utils/storage"
+)
+
+// DutyManager provides an interface to manipulate and compute gateways duty-cycles.
+type DutyManager interface {
+}
+
+type dutyManager struct {
+	sync.Mutex
+	db           dbutil.Interface
+	CycleLength  time.Duration       // Duration upon which the duty-cycle is evaluated
+	MaxDutyCycle map[subBand]float64 // The percentage max duty cycle accepted for each sub-band
+}
+
+// Available sub-bands
+const (
+	europe1 subBand = iota
+	europe2
+)
+
+type subBand byte
+
+// Available regions for LoRaWAN
+const (
+	Europe region = iota
+	US
+	China
+)
+
+type region byte
+
+// NewDutyManager constructs a new gateway manager from
+func NewDutyManager(cycleLength time.Duration, r region) (DutyManager, error) {
+	var maxDuty map[subBand]float64
+	switch r {
+	case Europe:
+		maxDuty = map[subBand]float64{
+			europe1: 0.01,
+			europe2: 0.1,
+		}
+	default:
+		return nil, errors.New(errors.Implementation, "Region not supported")
+	}
+
+	return &dutyManager{
+		CycleLength:  cycleLength,
+		MaxDutyCycle: maxDuty,
+	}, nil
+}
+
+// GetSubBand returns the subband associated to a given frequency
+func GetSubBand(freq float64) (subBand, error) {
+	if int(freq) == 868 {
+		return europe1, nil
+	}
+	return 0, errors.New(errors.Structural, "Unknown frequency")
+}
+
+// Update update an entry with the corresponding time-on-air
+//
+// Datr represents a LoRaWAN data-rate indicator of the form SFxxBWyyy,
+// where xx C [[7;12]] and yyy C { 125, 250, 500 }
+// Codr represents a LoRaWAN code rate  indicator fo the form 4/x with x C [[5;8]]
+func (m dutyManager) Update(id []byte, freq float64, size uint, datr string, codr string) error {
+	sub, err := GetSubBand(freq)
+	if err != nil {
+		return err
+	}
+	key := fmt.Sprintf("%+x", id)
+
+	// Compute the ime-on-air
+	timeOnAir, err := computeTOA(size, datr, codr)
+	if err != nil {
+		return err
+	}
+
+	// Lookup and update the entry
+	m.Lock()
+	defer m.Unlock()
+	itf, err := m.db.Lookup(key, []byte("entry"), &dutyEntry{})
+	if err != nil {
+		return err
+	}
+	entry := itf.([]dutyEntry)[0]
+
+	// If the previous cycle is done, we create a new one
+	if entry.Until.Before(time.Now()) {
+		entry.Until = time.Now()
+		entry.OnAir[sub] = timeOnAir
+	} else {
+		entry.OnAir[sub] += timeOnAir
+	}
+
+	return m.db.Replace(key, []byte("entry"), []dbutil.Entry{&entry})
+}
+
+func computeTOA(size uint, datr string, codr string) (time.Duration, error) {
+	// Ensure the datr and codr are correct
+	var cr float64
+	switch codr {
+	case "4/5":
+		cr = 4.0 / 5.0
+	case "4/6":
+		cr = 4.0 / 6.0
+	case "4/7":
+		cr = 4.0 / 7.0
+	case "4/8":
+		cr = 4.0 / 8.0
+	default:
+		return 0, errors.New(errors.Structural, "Invalid Codr")
+	}
+
+	re := regexp.MustCompile("^SF(7|8|9|10|11|12)BW(125|250|500)$")
+	matches := re.FindStringSubmatch(datr)
+
+	if len(matches) != 3 {
+		return 0, errors.New(errors.Structural, "Invalid Datr")
+	}
+
+	// Compute bitrate, Page 10: http://www.semtech.com/images/datasheet/an1200.22.pdf
+	sf, _ := strconv.ParseFloat(matches[1], 64)
+	bw, _ := strconv.ParseUint(matches[2], 10, 64)
+	bitrate := sf * cr * float64(bw) / math.Pow(2, sf)
+
+	return time.Duration(float64(size) / bitrate), nil
+}
+
+type dutyEntry struct {
+	Until time.Time
+	OnAir map[subBand]time.Duration
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface
+func (e dutyEntry) MarshalBinary() ([]byte, error) {
+	return nil, nil
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface
+func (e *dutyEntry) UnmarshalBinary(data []byte) error {
+	return nil
+}
