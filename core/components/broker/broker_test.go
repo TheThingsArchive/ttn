bdd0bab7dbd983eefbbfdc540ef16bdda6acc97e
ktorz <matthias.benkort@gmail.com>
2016-03-07 18:04:22 +0100

    [network-controller] Add new test to completely test the broker uplink / downlink

--- core/components/broker/broker_test.go
+++ core/components/broker/broker_test.go
@@ -149,6 +149,8 @@ func TestHandleUp(t *testing.T) {
 		CheckRegistrations(t, nil, store.InStoreApp)
 		mocks.CheckSent(t, nil, adapter.InSendPacket)
 		mocks.CheckRecipients(t, nil, adapter.InSendRecipients)
+		CheckCounters(t, 0, store.InUpdateFCnt)
+		CheckDirections(t, "", store.InUpdateDir)
 	}
 
 	// -------------------
@@ -172,6 +174,8 @@ func TestHandleUp(t *testing.T) {
 		CheckRegistrations(t, nil, store.InStoreApp)
 		mocks.CheckSent(t, nil, adapter.InSendPacket)
 		mocks.CheckRecipients(t, nil, adapter.InSendRecipients)
+		CheckCounters(t, 0, store.InUpdateFCnt)
+		CheckDirections(t, "", store.InUpdateDir)
 	}
 
 	// -------------------
@@ -215,6 +219,8 @@ func TestHandleUp(t *testing.T) {
 		CheckRegistrations(t, nil, store.InStoreApp)
 		mocks.CheckSent(t, nil, adapter.InSendPacket)
 		mocks.CheckRecipients(t, nil, adapter.InSendRecipients)
+		CheckCounters(t, 0, store.InUpdateFCnt)
+		CheckDirections(t, "", store.InUpdateDir)
 	}
 
 	// -------------------
@@ -268,6 +274,8 @@ func TestHandleUp(t *testing.T) {
 		CheckRegistrations(t, nil, store.InStoreApp)
 		mocks.CheckSent(t, hpacket, adapter.InSendPacket)
 		mocks.CheckRecipients(t, []core.Recipient{recipient}, adapter.InSendRecipients)
+		CheckCounters(t, 5, store.InUpdateFCnt)
+		CheckDirections(t, "up", store.InUpdateDir)
 	}
 
 	// -------------------
@@ -314,6 +322,8 @@ func TestHandleUp(t *testing.T) {
 		CheckRegistrations(t, nil, store.InStoreApp)
 		mocks.CheckSent(t, nil, adapter.InSendPacket)
 		mocks.CheckRecipients(t, nil, adapter.InSendRecipients)
+		CheckCounters(t, 5, store.InUpdateFCnt)
+		CheckDirections(t, "up", store.InUpdateDir)
 	}
 
 	// -------------------
@@ -367,5 +377,246 @@ func TestHandleUp(t *testing.T) {
 		CheckRegistrations(t, nil, store.InStoreApp)
 		mocks.CheckSent(t, hpacket, adapter.InSendPacket)
 		mocks.CheckRecipients(t, []core.Recipient{recipient}, adapter.InSendRecipients)
+		CheckCounters(t, 5, store.InUpdateFCnt)
+		CheckDirections(t, "up", store.InUpdateDir)
+	}
+
+	// -------------------
+
+	{
+		testutil.Desc(t, "Send packet, get 1 entry, 1 valid MIC | 1 valid downlink")
+
+		// Build
+		an := mocks.NewMockAckNacker()
+		recipient := mocks.NewMockRecipient()
+		adapter := mocks.NewMockAdapter()
+		resp := newBPacketDown(1)
+		data, _ := resp.MarshalBinary()
+		adapter.OutSend = data
+		adapter.OutGetRecipient = recipient
+		store := newMockController()
+		store.OutLookupDevices = []devEntry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 2, 3, 2, 3}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8}),
+			},
+		}
+		bpacket := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Uplink",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		)
+		data, _ = bpacket.MarshalBinary()
+		hpacket, _ := core.NewHPacket(
+			store.OutLookupDevices[0].AppEUI,
+			store.OutLookupDevices[0].DevEUI,
+			bpacket.Payload(),
+			bpacket.Metadata(),
+		)
+
+		// Operate
+		broker := New(store, testutil.GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		errutil.CheckErrors(t, nil, err)
+		mocks.CheckAcks(t, true, an.InAck)
+		CheckRegistrations(t, nil, store.InStoreDevices)
+		CheckRegistrations(t, nil, store.InStoreApp)
+		mocks.CheckSent(t, hpacket, adapter.InSendPacket)
+		mocks.CheckRecipients(t, []core.Recipient{recipient}, adapter.InSendRecipients)
+		CheckCounters(t, 1, store.InUpdateFCnt)
+		CheckDirections(t, "down", store.InUpdateDir)
+	}
+
+	// -------------------
+
+	{
+		testutil.Desc(t, "Send packet, get 1 entry, 1 valid MIC | 1 invalid downlink")
+
+		// Build
+		an := mocks.NewMockAckNacker()
+		recipient := mocks.NewMockRecipient()
+		adapter := mocks.NewMockAdapter()
+		adapter.OutSend = []byte{1, 2, 3}
+		adapter.OutGetRecipient = recipient
+		store := newMockController()
+		store.OutLookupDevices = []devEntry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 2, 3, 2, 3}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8}),
+			},
+		}
+		bpacket := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Uplink",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		)
+		data, _ := bpacket.MarshalBinary()
+		hpacket, _ := core.NewHPacket(
+			store.OutLookupDevices[0].AppEUI,
+			store.OutLookupDevices[0].DevEUI,
+			bpacket.Payload(),
+			bpacket.Metadata(),
+		)
+
+		// Operate
+		broker := New(store, testutil.GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		errutil.CheckErrors(t, pointer.String(string(errors.Operational)), err)
+		mocks.CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStoreDevices)
+		CheckRegistrations(t, nil, store.InStoreApp)
+		mocks.CheckSent(t, hpacket, adapter.InSendPacket)
+		mocks.CheckRecipients(t, []core.Recipient{recipient}, adapter.InSendRecipients)
+		CheckCounters(t, 5, store.InUpdateFCnt)
+		CheckDirections(t, "up", store.InUpdateDir)
+	}
+
+	// -------------------
+
+	{
+		testutil.Desc(t, "Send packet, get 1 entry, 1 valid MIC | 1 unhandled downlink ")
+
+		// Build
+		an := mocks.NewMockAckNacker()
+		recipient := mocks.NewMockRecipient()
+		adapter := mocks.NewMockAdapter()
+		resp, _ := core.NewAPacket(
+			lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+			lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+			[]byte{1, 2},
+			nil,
+		)
+		data, _ := resp.MarshalBinary()
+		adapter.OutSend = data
+		adapter.OutGetRecipient = recipient
+		store := newMockController()
+		store.OutLookupDevices = []devEntry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 2, 3, 2, 3}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8}),
+			},
+		}
+		bpacket := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Uplink",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		)
+		data, _ = bpacket.MarshalBinary()
+		hpacket, _ := core.NewHPacket(
+			store.OutLookupDevices[0].AppEUI,
+			store.OutLookupDevices[0].DevEUI,
+			bpacket.Payload(),
+			bpacket.Metadata(),
+		)
+
+		// Operate
+		broker := New(store, testutil.GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		errutil.CheckErrors(t, pointer.String(string(errors.Operational)), err)
+		mocks.CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStoreDevices)
+		CheckRegistrations(t, nil, store.InStoreApp)
+		mocks.CheckSent(t, hpacket, adapter.InSendPacket)
+		mocks.CheckRecipients(t, []core.Recipient{recipient}, adapter.InSendRecipients)
+		CheckCounters(t, 5, store.InUpdateFCnt)
+		CheckDirections(t, "up", store.InUpdateDir)
+	}
+
+	// -------------------
+
+	{
+		testutil.Desc(t, "Send packet, get 1 entry, 1 valid MIC | 1 downlink invalid counter")
+
+		// Build
+		an := mocks.NewMockAckNacker()
+		recipient := mocks.NewMockRecipient()
+		adapter := mocks.NewMockAdapter()
+		resp := newBPacketDown(55000)
+		data, _ := resp.MarshalBinary()
+		adapter.OutSend = data
+		adapter.OutGetRecipient = recipient
+		store := newMockController()
+		store.OutLookupDevices = []devEntry{
+			{
+				Recipient: []byte{1, 2, 3},
+				AppEUI:    lorawan.EUI64([8]byte{1, 1, 1, 1, 5, 5, 5, 5}),
+				DevEUI:    lorawan.EUI64([8]byte{4, 4, 4, 4, 2, 3, 2, 3}),
+				NwkSKey:   lorawan.AES128Key([16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8}),
+			},
+		}
+		bpacket := newBPacket(
+			[4]byte{2, 3, 2, 3},
+			"Uplink",
+			[16]byte{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8},
+			5,
+		)
+		data, _ = bpacket.MarshalBinary()
+		hpacket, _ := core.NewHPacket(
+			store.OutLookupDevices[0].AppEUI,
+			store.OutLookupDevices[0].DevEUI,
+			bpacket.Payload(),
+			bpacket.Metadata(),
+		)
+
+		// Operate
+		broker := New(store, testutil.GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		errutil.CheckErrors(t, pointer.String(string(errors.Operational)), err)
+		mocks.CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStoreDevices)
+		CheckRegistrations(t, nil, store.InStoreApp)
+		mocks.CheckSent(t, hpacket, adapter.InSendPacket)
+		mocks.CheckRecipients(t, []core.Recipient{recipient}, adapter.InSendRecipients)
+		CheckCounters(t, 5, store.InUpdateFCnt)
+		CheckDirections(t, "up", store.InUpdateDir)
+	}
+
+	// -------------------
+
+	{
+		testutil.Desc(t, "Send unhandled packet type")
+
+		// Build
+		an := mocks.NewMockAckNacker()
+		adapter := mocks.NewMockAdapter()
+		store := newMockController()
+		apacket, _ := core.NewAPacket(
+			lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+			lorawan.EUI64([8]byte{1, 2, 3, 4, 5, 6, 7, 8}),
+			[]byte{1, 2},
+			nil,
+		)
+		data, _ := apacket.MarshalBinary()
+
+		// Operate
+		broker := New(store, testutil.GetLogger(t, "Broker"))
+		err := broker.HandleUp(data, an, adapter)
+
+		// Check
+		errutil.CheckErrors(t, pointer.String(string(errors.Implementation)), err)
+		mocks.CheckAcks(t, false, an.InAck)
+		CheckRegistrations(t, nil, store.InStoreDevices)
+		CheckRegistrations(t, nil, store.InStoreApp)
+		mocks.CheckSent(t, nil, adapter.InSendPacket)
+		mocks.CheckRecipients(t, nil, adapter.InSendRecipients)
+		CheckCounters(t, 0, store.InUpdateFCnt)
+		CheckDirections(t, "", store.InUpdateDir)
 	}
 }
