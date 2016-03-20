3e2752fae64c2d6fae097a6595e6b26f11bd4494
ktorz <matthias.benkort@gmail.com>
2016-03-20 19:53:17 +0100

    [feature/otaa] Handle join requests in broker

--- core/components/broker/controller.go
+++ core/components/broker/controller.go
@@ -6,6 +6,7 @@ package broker
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"math"
 	"reflect"
 	"sync"
@@ -19,6 +20,8 @@ type NetworkController interface {
 	LookupDevices(devAddr []byte) ([]devEntry, error)
 	WholeCounter(devCnt uint32, entryCnt uint32) (uint32, error)
 	StoreDevice(devAddr []byte, entry devEntry) error
+	ReadActivation(appEUI []byte, devEUI []byte) (appEntry, error)
+	UpdateActivation(entry appEntry) error
 	UpdateFCnt(appEUI []byte, devEUI []byte, devAddr []byte, fcnt uint32) error
 	Close() error
 }
@@ -29,6 +32,13 @@ type devEntry struct {
 	DevEUI  []byte
 	NwkSKey [16]byte
 	FCntUp  uint32
+}
+
+type appEntry struct {
+	Dialer    Dialer
+	AppEUI    []byte
+	DevEUI    []byte
+	DevNonces [][]byte
 }
 
 type controller struct {
@@ -57,6 +67,29 @@ func (s *controller) LookupDevices(devAddr []byte) ([]devEntry, error) {
 		return nil, err
 	}
 	return entries.([]devEntry), nil
+}
+
+// ReadActivation implements the broker.NetworkController interface
+func (s *controller) ReadActivation(appEUI []byte, devEUI []byte) (appEntry, error) {
+	s.RLock()
+	defer s.RUnlock()
+	itf, err := s.db.Lookup(fmt.Sprintf("%x.%x", appEUI, devEUI), []byte{}, &appEntry{})
+	if err != nil {
+		return appEntry{}, err
+	}
+	entries := itf.([]appEntry)
+	if len(entries) != 1 {
+		// NOTE should clean up the entry ?
+		return appEntry{}, errors.New(errors.Structural, "Invalid stored entry")
+	}
+	return entries[0], nil
+}
+
+// UpdateAction implements the broker.NetworkController interface
+func (s *controller) UpdateActivation(entry appEntry) error {
+	s.Lock()
+	defer s.Unlock()
+	return s.db.Replace(fmt.Sprintf("%x.%x", entry.AppEUI, entry.DevEUI), []byte{}, []dbutil.Entry{&entry})
 }
 
 // WholeCounter implements the broker.NetworkController interface
@@ -132,8 +165,40 @@ func (e *devEntry) UnmarshalBinary(data []byte) error {
 	e.DevEUI = make([]byte, 8, 8)
 	binary.Read(buf, binary.BigEndian, &e.DevEUI)
 	binary.Read(buf, binary.BigEndian, &e.NwkSKey) // fixed-length array
-	if err := binary.Read(buf, binary.BigEndian, &e.FCntUp); err != nil {
-		return errors.New(errors.Structural, err)
+	binary.Read(buf, binary.BigEndian, &e.FCntUp)
+	e.Dialer = NewDialer(buf.Next(buf.Len()))
+	return nil
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface
+func (e appEntry) MarshalBinary() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, e.AppEUI)
+	binary.Write(buf, binary.BigEndian, e.DevEUI)
+	binary.Write(buf, binary.BigEndian, uint16(len(e.DevNonces)))
+	for _, n := range e.DevNonces {
+		binary.Write(buf, binary.BigEndian, n)
+	}
+	if len(buf.Bytes()) != 8+2+2*len(e.DevNonces) {
+		return nil, errors.New(errors.Structural, "App entry was invalid. Cannot Marshal")
+	}
+	binary.Write(buf, binary.BigEndian, e.Dialer.MarshalSafely())
+	return buf.Bytes(), nil
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface
+func (e *appEntry) UnmarshalBinary(data []byte) error {
+	buf := bytes.NewBuffer(data)
+	e.AppEUI = make([]byte, 8, 8)
+	binary.Read(buf, binary.BigEndian, &e.AppEUI)
+	e.DevEUI = make([]byte, 8, 8)
+	binary.Read(buf, binary.BigEndian, &e.DevEUI)
+	var n uint16
+	binary.Read(buf, binary.BigEndian, &n)
+	for i := 0; i < int(n); i++ {
+		devNonce := make([]byte, 2, 2)
+		binary.Read(buf, binary.BigEndian, &devNonce)
+		e.DevNonces = append(e.DevNonces, devNonce)
 	}
 	e.Dialer = NewDialer(buf.Next(buf.Len()))
 	return nil
