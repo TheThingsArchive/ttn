06295b471ac2e6ced23b66b3daa2568181d8f7a7
johanstokking <johan@stokking.nl>
2016-03-26 17:58:42 +0100

    Login and store token in file

--- ttnctl/cmd/users.go
+++ ttnctl/cmd/users.go
@@ -4,29 +4,50 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 
+	"github.com/TheThingsNetwork/ttn/ttnctl/util"
 	"github.com/howeyc/gopass"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+type token struct {
+	AccessToken      string `json:"access_token"`
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
+}
+
 // usersCmd represents the users command
 var usersCmd = &cobra.Command{
-	Use:   "users",
-	Short: "Manage users on the account server",
-	Long:  `ttnctl users allows you to manage users`,
+	Use:   "user",
+	Short: "Show the current user",
+	Long:  `ttnctl user shows the current logged on user`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
+		t, err := util.LoadAuth(viper.GetString("ttn-account-server"))
+		if err != nil {
+			ctx.WithError(err).Fatal("Failed to load authentication")
+		}
+
+		if t == nil {
+			ctx.Warn("No login found")
+			return
+		}
+
+		// TODO: Validate token
+
+		ctx.Infof("Logged on as %s", t.Email)
 	},
 }
 
 var usersCreateCmd = &cobra.Command{
 	Use:   "create [e-mail]",
 	Short: "Create a new user",
-	Long:  `ttnctl users create allows you to create a new user`,
+	Long:  `ttnctl user create allows you to create a new user`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 1 {
 			ctx.Fatal("Insufficient arguments")
@@ -39,7 +60,7 @@ var usersCreateCmd = &cobra.Command{
 			ctx.Fatal("Invalid password")
 		}
 
-		uri := fmt.Sprintf("http://%s/register", viper.GetString("ttn-account-server"))
+		uri := fmt.Sprintf("%s/register", viper.GetString("ttn-account-server"))
 		values := url.Values{
 			"email":    {email},
 			"password": {string(password)},
@@ -58,10 +79,70 @@ var usersCreateCmd = &cobra.Command{
 	},
 }
 
+var usersLoginCmd = &cobra.Command{
+	Use:   "login [e-mail]",
+	Short: "Login",
+	Long:  `ttnctl user login allows you to login`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			ctx.Fatal("Insufficient arguments")
+		}
+
+		email := args[0]
+		fmt.Print("Password: ")
+		password, err := gopass.GetPasswd()
+		if err != nil {
+			ctx.Fatal("Invalid password")
+		}
+
+		server := viper.GetString("ttn-account-server")
+		uri := fmt.Sprintf("%s/token", server)
+		values := url.Values{
+			"grant_type": {"password"},
+			"username":   {email},
+			"password":   {string(password)},
+		}
+		req, err := http.NewRequest("POST", uri, strings.NewReader(values.Encode()))
+		if err != nil {
+			ctx.WithError(err).Fatal("Create request failed")
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		req.SetBasicAuth("ttnctl", "")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			ctx.WithError(err).Fatal("Request failed")
+		}
+		defer resp.Body.Close()
+
+		decoder := json.NewDecoder(resp.Body)
+		var t token
+		if err := decoder.Decode(&t); err != nil {
+			ctx.WithError(err).Fatal("Failed to parse response")
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			if t.Error != "" {
+				ctx.Fatalf("Request failed: %s", t.ErrorDescription)
+			} else {
+				ctx.Fatalf("Request failed: %s", resp.Status)
+			}
+		}
+
+		ctx.Infof("Logged in as %s", email)
+
+		if err := util.SaveAuth(server, email, t.AccessToken); err != nil {
+			ctx.WithError(err).Error("Failed to save login")
+		}
+	},
+}
+
 func init() {
 	RootCmd.AddCommand(usersCmd)
 	usersCmd.AddCommand(usersCreateCmd)
+	usersCmd.AddCommand(usersLoginCmd)
 
-	usersCmd.PersistentFlags().String("ttn-account-server", "account.thethings.network", "The Things Network OAuth 2.0 account server")
+	usersCmd.PersistentFlags().String("ttn-account-server", "https://account.thethings.network", "The Things Network OAuth 2.0 account server")
 	viper.BindPFlag("ttn-account-server", usersCmd.PersistentFlags().Lookup("ttn-account-server"))
 }
