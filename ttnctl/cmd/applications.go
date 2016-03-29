77e13c0ff7589161553adddc065df769230158b4
johanstokking <johan@stokking.nl>
2016-03-29 02:16:37 +0200

    [ttnctl-applications] Show applications

--- ttnctl/cmd/applications.go
+++ ttnctl/cmd/applications.go
@@ -4,12 +4,21 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/TheThingsNetwork/ttn/ttnctl/util"
+	"github.com/gosuri/uitable"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+type app struct {
+	EUI   string `json:"eui"`
+	Name  string `json:"name"`
+	Owner string `json:"owner"`
+}
 
 // applicationsCmd represents the applications command
 var applicationsCmd = &cobra.Command{
@@ -18,10 +27,36 @@ var applicationsCmd = &cobra.Command{
 	Long:  `ttnctl applications retrieves the applications of the logged on user.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		server := viper.GetString("ttn-account-server")
-		_, err := util.NewRequestWithAuth(server, "GET", fmt.Sprintf("%s/applications", server), nil)
+		req, err := util.NewRequestWithAuth(server, "GET", fmt.Sprintf("%s/applications", server), nil)
 		if err != nil {
-			ctx.WithError(err).Fatal("Create request failed")
+			ctx.WithError(err).Fatal("Failed to created authenticated request")
 		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			ctx.WithError(err).Fatal("Failed to get applications")
+		}
+		if resp.StatusCode != http.StatusOK {
+			ctx.Fatalf("Failed to get applications: %s", resp.Status)
+		}
+
+		defer resp.Body.Close()
+		decoder := json.NewDecoder(resp.Body)
+		var apps []*app
+		err = decoder.Decode(&apps)
+		if err != nil {
+			ctx.WithError(err).Fatal("Failed to read applications")
+		}
+
+		ctx.Infof("Found %d application(s)", len(apps))
+		table := uitable.New()
+		table.MaxColWidth = 70
+		table.AddRow("EUI", "Name", "Owner")
+		for _, app := range apps {
+			table.AddRow(app.EUI, app.Name, app.Owner)
+		}
+		fmt.Println(table)
 	},
 }
 
