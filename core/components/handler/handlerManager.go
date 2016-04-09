5a950e762d11ec46630c1f4a60eb5bf6bdfce49a
johanstokking <johan@stokking.nl>
2016-04-09 18:17:27 -0500

    Implemented getter and setter of default device

--- core/components/handler/handlerManager.go
+++ core/components/handler/handlerManager.go
@@ -11,23 +11,24 @@ import (
 
 // ListDevices implements the core.HandlerManagerServer interface
 func (h component) ListDevices(bctx context.Context, req *core.ListDevicesHandlerReq) (*core.ListDevicesHandlerRes, error) {
-	h.Ctx.Debug("Handle ListDevices Request")
+	h.Ctx.Debug("Handle list devices request")
 
 	// 1. Validate the request
 	if len(req.AppEUI) != 8 {
 		err := errors.New(errors.Structural, "Invalid request parameters")
-		h.Ctx.WithError(err).Debug("Unable to handle ListDevices request")
+		h.Ctx.WithError(err).Debug("Unable to handle list devices request")
 		return new(core.ListDevicesHandlerRes), err
 	}
 
-	// 2. Validate token & retrieve devices from db
+	// 2. Validate token and retrieve devices from the storage
+	// TODO: On not found, report gracefully back to caller instead of failing with not found
 	if _, err := h.Broker.ValidateToken(context.Background(), &core.ValidateTokenBrokerReq{AppEUI: req.AppEUI, Token: req.Token}); err != nil {
-		h.Ctx.WithError(err).Debug("Unable to handle ListDevices request")
+		h.Ctx.WithError(err).Debug("Unable to handle list devices request")
 		return new(core.ListDevicesHandlerRes), errors.New(errors.Operational, err)
 	}
 	entries, err := h.DevStorage.readAll(req.AppEUI)
 	if err != nil {
-		h.Ctx.WithError(err).Debug("Unable to handle ListDevices request")
+		h.Ctx.WithError(err).Debug("Unable to handle list devices request")
 		return new(core.ListDevicesHandlerRes), errors.New(errors.Operational, err)
 	}
 
@@ -63,7 +64,7 @@ func (h component) ListDevices(bctx context.Context, req *core.ListDevicesHandle
 
 // UpsertABP implements the core.HandlerManager interface
 func (h component) UpsertABP(bctx context.Context, req *core.UpsertABPHandlerReq) (*core.UpsertABPHandlerRes, error) {
-	h.Ctx.Debug("Handle Upsert ABP Request")
+	h.Ctx.Debug("Handle upsert ABP request")
 
 	// 1. Validate the request
 	if len(req.AppEUI) != 8 || len(req.DevAddr) != 4 || len(req.NwkSKey) != 16 || len(req.AppSKey) != 16 {
@@ -85,8 +86,8 @@ func (h component) UpsertABP(bctx context.Context, req *core.UpsertABPHandlerReq
 		return new(core.UpsertABPHandlerRes), errors.New(errors.Operational, err)
 	}
 
-	// 3. Save the device in the local storage
-	h.Ctx.WithField("AppEUI", req.AppEUI).WithField("DevAddr", req.DevAddr).Debug("Request accepted by broker. Registering Device.")
+	// 3. Save the device in the storage
+	h.Ctx.WithField("AppEUI", req.AppEUI).WithField("DevAddr", req.DevAddr).Debug("Request accepted by broker. Registering device")
 	entry := devEntry{
 		AppEUI:   req.AppEUI,
 		DevEUI:   append([]byte{0, 0, 0, 0}, req.DevAddr...),
@@ -107,7 +108,7 @@ func (h component) UpsertABP(bctx context.Context, req *core.UpsertABPHandlerReq
 
 // UpsertOTAA implements the core.HandlerManager interface
 func (h component) UpsertOTAA(bctx context.Context, req *core.UpsertOTAAHandlerReq) (*core.UpsertOTAAHandlerRes, error) {
-	h.Ctx.Debug("Handle Upsert OTAA Request")
+	h.Ctx.Debug("Handle upsert OTAA request")
 
 	// 1. Validate the request
 	if len(req.AppEUI) != 8 || len(req.DevEUI) != 8 || len(req.AppKey) != 16 {
@@ -127,8 +128,8 @@ func (h component) UpsertOTAA(bctx context.Context, req *core.UpsertOTAAHandlerR
 		return new(core.UpsertOTAAHandlerRes), errors.New(errors.Operational, err)
 	}
 
-	// 3. Save the device in the local storage
-	h.Ctx.WithField("AppEUI", req.AppEUI).WithField("DevEUI", req.DevEUI).Debug("Request accepted by broker. Registering Device.")
+	// 3. Save the device in the storage
+	h.Ctx.WithField("AppEUI", req.AppEUI).WithField("DevEUI", req.DevEUI).Debug("Request accepted by broker. Registering device")
 	var appKey [16]byte
 	copy(appKey[:], req.AppKey)
 	err = h.DevStorage.upsert(devEntry{
@@ -144,15 +145,15 @@ func (h component) UpsertOTAA(bctx context.Context, req *core.UpsertOTAAHandlerR
 	return new(core.UpsertOTAAHandlerRes), nil
 }
 
-// SetDefaultAppKey implements the core.HandlerManager interface
-func (h component) SetDefaultAppKey(bctx context.Context, req *core.SetDefaultAppKeyReq) (*core.SetDefaultAppKeyRes, error) {
-	h.Ctx.Debug("Handle Set Default AppKey Request")
+// GetDefaultDevice implements the core.HandlerManager Interface
+func (h component) GetDefaultDevice(bctx context.Context, req *core.GetDefaultDeviceReq) (*core.GetDefaultDeviceRes, error) {
+	h.Ctx.Debug("Handle get default device request")
 
 	// 1. Validate the request
-	if len(req.AppEUI) != 8 || len(req.AppKey) != 16 {
+	if len(req.AppEUI) != 8 {
 		err := errors.New(errors.Structural, "Invalid request parameters")
-		h.Ctx.WithError(err).Debug("Unable to handle set default AppKey request")
-		return new(core.SetDefaultAppKeyRes), err
+		h.Ctx.WithError(err).Debug("Unable to handle set default device request")
+		return new(core.GetDefaultDeviceRes), err
 	}
 
 	// 2. Validate the token
@@ -162,11 +163,43 @@ func (h component) SetDefaultAppKey(bctx context.Context, req *core.SetDefaultAp
 	})
 	if err != nil {
 		h.Ctx.WithError(err).Debug("Broker rejected token")
-		return new(core.SetDefaultAppKeyRes), errors.New(errors.Operational, err)
+		return new(core.GetDefaultDeviceRes), errors.New(errors.Operational, err)
 	}
 
-	// 3. Set the key in the local storage
-	h.Ctx.WithField("AppEUI", req.AppEUI).Debug("Valid token. Registering default AppKey.")
+	// 3. Get default device entry from storage
+	// TODO: On not found, report gracefully back to caller instead of failing with not found
+	entry, err := h.DevStorage.getDefault(req.AppEUI)
+	if err != nil {
+		h.Ctx.WithError(err).Debug("Error while trying to retrieve default device")
+		return new(core.GetDefaultDeviceRes), err
+	}
+
+	return &core.GetDefaultDeviceRes{AppKey: entry.AppKey[:]}, nil
+}
+
+// SetDefaultDevice implements the core.HandlerManager interface
+func (h component) SetDefaultDevice(bctx context.Context, req *core.SetDefaultDeviceReq) (*core.SetDefaultDeviceRes, error) {
+	h.Ctx.Debug("Handle set default device request")
+
+	// 1. Validate the request
+	if len(req.AppEUI) != 8 || len(req.AppKey) != 16 {
+		err := errors.New(errors.Structural, "Invalid request parameters")
+		h.Ctx.WithError(err).Debug("Unable to handle set default device request")
+		return new(core.SetDefaultDeviceRes), err
+	}
+
+	// 2. Validate the token
+	_, err := h.Broker.ValidateToken(context.Background(), &core.ValidateTokenBrokerReq{
+		Token:  req.Token,
+		AppEUI: req.AppEUI,
+	})
+	if err != nil {
+		h.Ctx.WithError(err).Debug("Broker rejected token")
+		return new(core.SetDefaultDeviceRes), errors.New(errors.Operational, err)
+	}
+
+	// 3. Set the default device in the storage
+	h.Ctx.WithField("AppEUI", req.AppEUI).Debug("Valid token. Registering default device")
 	var appKey [16]byte
 	copy(appKey[:], req.AppKey)
 	err = h.DevStorage.setDefault(devDefaultEntry{
@@ -174,5 +207,5 @@ func (h component) SetDefaultAppKey(bctx context.Context, req *core.SetDefaultAp
 		AppKey: appKey,
 	})
 
-	return new(core.SetDefaultAppKeyRes), nil
+	return new(core.SetDefaultDeviceRes), nil
 }
