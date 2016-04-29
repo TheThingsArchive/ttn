b2d91f119169debf11ce392f90823ec893c27de0
Hylke Visser <htdvisser@gmail.com>
2016-04-29 09:29:17 +0200

    [ttnctl] Warn people when using default keys

--- ttnctl/cmd/device.go
+++ ttnctl/cmd/device.go
@@ -20,6 +20,8 @@ import (
 
 const emptyCell = "-"
 
+var defaultKey = []byte{0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C}
+
 func getHandlerManager() core.AuthHandlerClient {
 	cli, err := handler.NewClient(viper.GetString("ttn-handler"))
 	if err != nil {
@@ -301,6 +303,9 @@ registration on the Handler`,
 			appSKey, err = util.Parse128(args[2])
 			if err != nil {
 				ctx.Fatalf("Invalid AppSKey: %s", err)
+			}
+			if reflect.DeepEqual(nwkSKey, defaultKey) || reflect.DeepEqual(appSKey, defaultKey) {
+				ctx.Warn("You are using default keys, any attacker can read your data or attack your device's connectivity.")
 			}
 		} else {
 			ctx.Info("Generating random NwkSKey and AppSKey...")
