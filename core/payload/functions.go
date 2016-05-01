5923008dfa3d873157f977c9e003c2d6ccbc6f2e
johanstokking <johan@stokking.nl>
2016-05-02 00:46:08 +0200

    Decode, convert and validate payload using JavaScript functions

---
+++ core/payload/functions.go
@@ -0,0 +1,86 @@
+package payload
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/robertkrimen/otto"
+)
+
+// Functions decodes, converts and validates payload using JavaScript functions
+type Functions struct {
+	// Decoder is a JavaScript function that accepts the payload as byte array and
+	// returns an object containing the decoded values
+	Decoder string
+	// Converter is a JavaScript function that accepts the data as decoded by
+	// Decoder and returns an object containing the converted values
+	Converter string
+	// Validator is a JavaScript function that validates the data is converted by
+	// Converter and returns a boolean value indicating the validity of the data
+	Validator string
+}
+
+// Decode decodes the payload using the Decoder function into a map
+func (f *Functions) Decode(payload []byte) (map[string]interface{}, error) {
+	if f.Decoder == "" {
+		return nil, errors.New("Decoder function not set")
+	}
+
+	vm := otto.New()
+	vm.Set("payload", payload)
+	value, err := vm.Run(fmt.Sprintf("(%s)(payload)", f.Decoder))
+	if err != nil {
+		return nil, err
+	}
+
+	if !value.IsObject() {
+		return nil, errors.New("Decoder does not return an object")
+	}
+
+	v, _ := value.Export()
+	return v.(map[string]interface{}), nil
+}
+
+// Convert converts the values in the specified map to a another map using the
+// Converter function. If the Converter function is not set, this function
+// returns the data as-is
+func (f *Functions) Convert(data map[string]interface{}) (map[string]interface{}, error) {
+	if f.Converter == "" {
+		return data, nil
+	}
+
+	vm := otto.New()
+	vm.Set("data", data)
+	value, err := vm.Run(fmt.Sprintf("(%s)(data)", f.Converter))
+	if err != nil {
+		return nil, err
+	}
+
+	if !value.IsObject() {
+		return nil, errors.New("Converter does not return an object")
+	}
+
+	v, _ := value.Export()
+	return v.(map[string]interface{}), nil
+}
+
+// Validate validates the values in the specified map using the Validator
+// function. If the Validator function is not set, this function returns true
+func (f *Functions) Validate(data map[string]interface{}) (bool, error) {
+	if f.Validator == "" {
+		return true, nil
+	}
+
+	vm := otto.New()
+	vm.Set("data", data)
+	value, err := vm.Run(fmt.Sprintf("(%s)(data)", f.Validator))
+	if err != nil {
+		return false, err
+	}
+
+	if !value.IsBoolean() {
+		return false, errors.New("Validator does not return a boolean")
+	}
+
+	return value.ToBoolean()
+}
---
+++ core/payload/functions_test.go
@@ -0,0 +1,61 @@
+package payload
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/assertions"
+)
+
+func TestDecode(t *testing.T) {
+	a := New(t)
+
+	functions := &Functions{
+		Decoder: `function(payload) {
+      return {
+        value: (payload[0] << 8) | payload[1]
+      };
+    }`,
+	}
+	payload := []byte{0x48, 0x65}
+
+	m, err := functions.Decode(payload)
+	a.So(err, ShouldBeNil)
+
+	size, ok := m["value"]
+	a.So(ok, ShouldBeTrue)
+	a.So(size, ShouldEqual, 18533)
+}
+
+func TestConvert(t *testing.T) {
+	a := New(t)
+
+	functions := &Functions{
+		Converter: `function(data) {
+      return {
+        size: data.temperature * 2
+      };
+    }`,
+	}
+
+	data, err := functions.Convert(map[string]interface{}{"temperature": 11})
+	a.So(err, ShouldBeNil)
+	a.So(data["size"], ShouldEqual, 22)
+}
+
+func TestValidate(t *testing.T) {
+	a := New(t)
+
+	functions := &Functions{
+		Validator: `function(data) {
+      return data.temperature < 20;
+    }`,
+	}
+
+	valid, err := functions.Validate(map[string]interface{}{"temperature": 10})
+	a.So(err, ShouldBeNil)
+	a.So(valid, ShouldBeTrue)
+
+	valid, err = functions.Validate(map[string]interface{}{"temperature": 30})
+	a.So(err, ShouldBeNil)
+	a.So(valid, ShouldBeFalse)
+}
