4e3c486330522ff16e786ccfa203ed907b05f150
johanstokking <johan@stokking.nl>
2016-05-07 15:30:16 +0200

    Fixed tests

---
+++ core/collector/collector_test.go
@@ -0,0 +1,46 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package collector
+
+import (
+	"testing"
+	"time"
+
+	"github.com/TheThingsNetwork/ttn/core/types"
+	ttntesting "github.com/TheThingsNetwork/ttn/utils/testing"
+
+	. "github.com/smartystreets/assertions"
+)
+
+type mockStorage struct {
+}
+
+func TestCollection(t *testing.T) {
+	a := New(t)
+
+	appStorage, err := ConnectRedis("localhost:6379", 0)
+	if err != nil {
+		panic(err)
+	}
+	defer appStorage.Close()
+	defer appStorage.Reset()
+
+	eui, _ := types.ParseAppEUI("8000000000000001")
+	err = appStorage.SetKey(eui, "secret")
+	a.So(err, ShouldBeNil)
+
+	collector := NewCollector(ttntesting.GetLogger(t, "Collector"), appStorage, "localhost:1883", &mockStorage{})
+	collectors, err := collector.Start()
+	defer collector.Stop()
+	a.So(err, ShouldBeNil)
+	a.So(collectors, ShouldHaveLength, 1)
+}
+
+func (s *mockStorage) Save(appEUI types.AppEUI, devEUI types.DevEUI, t time.Time, fields map[string]interface{}) error {
+	return nil
+}
+
+func (s *mockStorage) Close() error {
+	return nil
+}
