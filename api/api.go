c11c69d6c07073f4cefcab78809962e21cecb80e
Hylke Visser <htdvisser@gmail.com>
2016-05-30 14:30:33 +0200

    Set timeout on gRPC connect

--- api/api.go
+++ api/api.go
@@ -1,9 +1,14 @@
 package api
 
-import "google.golang.org/grpc"
+import (
+	"time"
+
+	"google.golang.org/grpc"
+)
 
 // DialOptions are the gRPC dial options for discovery calls
 // TODO: disable insecure connections
 var DialOptions = []grpc.DialOption{
 	grpc.WithInsecure(),
+	grpc.WithTimeout(2 * time.Second),
 }
