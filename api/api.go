e7801c3faad602ea89306031d7c2022c371c6f1e
Hylke Visser <htdvisser@gmail.com>
2016-06-07 16:48:08 +0200

    Use KeepAlive in dialers

--- api/api.go
+++ api/api.go
@@ -1,14 +1,29 @@
 package api
 
 import (
+	"net"
 	"time"
 
 	"google.golang.org/grpc"
 )
 
-// DialOptions are the gRPC dial options for discovery calls
+// Backoff indicates how long a client should wait between failed requests
+var Backoff = 1 * time.Second
+
+// KeepAlive indicates the keep-alive time for the Dialer
+var KeepAlive = 10 * time.Second
+
+// DialOptions to use in TTN gRPC
 // TODO: disable insecure connections
 var DialOptions = []grpc.DialOption{
 	grpc.WithInsecure(),
-	grpc.WithTimeout(2 * time.Second),
+	WithKeepAliveDialer(),
+}
+
+// WithKeepAliveDialer creates a dialer with the configured KeepAlive time
+func WithKeepAliveDialer() grpc.DialOption {
+	return grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+		d := net.Dialer{Timeout: timeout, KeepAlive: KeepAlive}
+		return d.Dial("tcp", addr)
+	})
 }
