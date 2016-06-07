d70b5bcdb7dfd55b7365d92183064ea6e7dc41be
Hylke Visser <htdvisser@gmail.com>
2016-06-07 16:45:50 +0200

    Add gRPC interceptors

--- core/component.go
+++ core/component.go
@@ -7,12 +7,14 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 
 	"github.com/TheThingsNetwork/ttn/api"
 	pb_discovery "github.com/TheThingsNetwork/ttn/api/discovery"
 	"github.com/TheThingsNetwork/ttn/utils/tokenkey"
 	"github.com/apex/log"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/mwitkow/go-grpc-middleware"
 	"github.com/spf13/viper"
 )
 
@@ -76,15 +78,16 @@ func (c *Component) Announce() error {
 // UpdateTokenKey updates the OAuth Bearer token key
 func (c *Component) UpdateTokenKey() error {
 	if c.TokenKeyProvider == nil {
-		return errors.New("No token provider configured")
+		return errors.New("ttn: No public key provider configured for token validation")
 	}
 
 	// Set up Auth Server Token Validation
 	tokenKey, err := c.TokenKeyProvider.Get(true)
 	if err != nil {
-		return fmt.Errorf("ttn: Failed to refresh token key: %s", err.Error())
+		c.Ctx.Warnf("ttn: Failed to refresh public key for token validation: %s", err.Error())
+	} else {
+		c.Ctx.Infof("ttn: Got public key for token validation (%v)", tokenKey.Algorithm)
 	}
-	c.Ctx.Infof("ttn: Got token key for algorithm %v", tokenKey.Algorithm)
 
 	return nil
 
@@ -112,6 +115,57 @@ func (c *Component) ValidateToken(token string) (claims map[string]interface{}, 
 		return nil, errors.New("The token is not valid or is expired")
 	}
 	return parsed.Claims, nil
+}
+
+func (c *Component) ServerOptions() []grpc.ServerOption {
+	unary := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		var peerAddr string
+		peer, ok := peer.FromContext(ctx)
+		if ok {
+			peerAddr = peer.Addr.String()
+		}
+		var peerID string
+		meta, ok := metadata.FromContext(ctx)
+		if ok {
+			id, ok := meta["id"]
+			if ok && len(id) > 0 {
+				peerID = id[0]
+			}
+		}
+		c.Ctx.WithFields(log.Fields{
+			"CallerID": peerID,
+			"CallerIP": peerAddr,
+			"Method":   info.FullMethod,
+		}).Debug("Handle Request")
+		return handler(ctx, req)
+	}
+
+	stream := func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		var peerAddr string
+		peer, ok := peer.FromContext(stream.Context())
+		if ok {
+			peerAddr = peer.Addr.String()
+		}
+		var peerID string
+		meta, ok := metadata.FromContext(stream.Context())
+		if ok {
+			id, ok := meta["id"]
+			if ok && len(id) > 0 {
+				peerID = id[0]
+			}
+		}
+		c.Ctx.WithFields(log.Fields{
+			"CallerID": peerID,
+			"CallerIP": peerAddr,
+			"Method":   info.FullMethod,
+		}).Debug("Start Stream")
+		return handler(srv, stream)
+	}
+
+	return []grpc.ServerOption{
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(unary)),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(stream)),
+	}
 }
 
 // GetContext returns a context for outgoing RPC requests
