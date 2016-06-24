20c99dbca3901a984fe68e8be30a3cc359d64804
Hylke Visser <htdvisser@gmail.com>
2016-06-24 12:31:43 +0200

    MIT License ALL THE FILES 💪

--- core/networkserver/server_test.go
+++ core/networkserver/server_test.go
@@ -1,3 +1,6 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
 package networkserver
 
 // TODO: The RPC server is super simple, but we should still test it.
--- core/types/device_type.go
+++ core/types/device_type.go
@@ -1,3 +1,6 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
 package types
 
 import "errors"
