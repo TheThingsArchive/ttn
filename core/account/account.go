53c66c97acd3dab30e25bcd9ff09c3db6ee6e8b2
Romeo Van Snick <romeovs@gmail.com>
2016-07-18 14:04:16 +0200

    start implementing ListApplications, FindApplication and CreateApplication

--- core/account/account.go
+++ core/account/account.go
@@ -4,35 +4,94 @@
 package account
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/TheThingsNetwork/ttn/core/account/util"
 	"github.com/TheThingsNetwork/ttn/core/types"
 	"github.com/apex/log"
 )
 
 // Account is a proxy to an account on the account server
 type Account struct {
+	// server is the server where the account lives
+	server string
+
+	// accessToken is the accessToken that gives this client the
+	// right to act on behalf of the account
 	accessToken string
 }
 
-// New creates a new Account with the default Client
-func New(accessToken string) *Account {
+// New creates a new Account for the given server and accessToken
+func New(server string, accessToken string) *Account {
 	return &Account{
+		server:      server,
 		accessToken: accessToken,
 	}
 }
 
 // ListApplications list all applications
-func (a *Account) ListApplications(ctx log.Interface) (apps []Application, err error) {
-	panic("ListApplications not implemented")
+func (a *Account) ListApplications() (apps []Application, err error) {
+	resp, err := util.GET(a.server, a.accessToken, "/applications")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&apps); err != nil {
+		return nil, err
+	}
+
+	return apps, nil
 }
 
 // GetApplication gets a specific application from the account server
-func (a *Account) FindApplication(ctx log.Interface, appID string) (Application, error) {
-	panic("FindApplication not implemented")
+func (a *Account) FindApplication(appID string) (app Application, err error) {
+	resp, err := util.GET(a.server, a.accessToken, fmt.Printf("/applications/%s", appID))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return app, fmt.Errorf("Application with id '%s' does not exist", appID)
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&apps); err != nil {
+		return app, err
+	}
+
+	return app, nil
+}
+
+type createApplicationReq struct {
+	Name  string         `json:"name"`
+	AppID string         `json:"id"`
+	EUIS  []types.AppEUI `json:"euis"`
 }
 
 // CreateApplication creates a new application on the account server
-func (a *Account) CreateApplication(ctx log.Interface, appID string, name string, EUIs []types.AppEUI) (Application, error) {
-	panic("CreateApplication not implemented")
+func (a *Account) CreateApplication(ctx log.Interface, appID string, name string, EUIs []types.AppEUI) (app Application, err error) {
+	body := createApplicationReq{
+		Name:  name,
+		AppID: appID,
+		EUIs:  EUIs,
+	}
+
+	resp, err := util.POST(a.server, a.accessToken, "/applications", body)
+	if resp.StatusCode != http.StatusCreated {
+		return app, fmt.Errorf("Could not create application: %s", resp.Status)
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&apps); err != nil {
+		return app, err
+	}
+
+	return app, nil
 }
 
 // DeleteApplication deletes an application
