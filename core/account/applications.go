8daca4e7882b0da3a1276f329e8ce23538e3ae18
Romeo Van Snick <romeovs@gmail.com>
2016-07-19 12:16:34 +0200

    use new http funcs

--- core/account/applications.go
+++ core/account/applications.go
@@ -4,54 +4,26 @@
 package account
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
 	"github.com/TheThingsNetwork/ttn/core/account/util"
 	"github.com/TheThingsNetwork/ttn/core/types"
 )
 
 // ListApplications list all applications
 func (a *Account) ListApplications() (apps []Application, err error) {
-	resp, err := util.GET(a.server, a.accessToken, "/applications")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&apps); err != nil {
-		return nil, err
-	}
-
-	return apps, nil
+	err = util.GET(a.server, a.accessToken, "/applications", &apps)
+	return apps, err
 }
 
 // GetApplication gets a specific application from the account server
 func (a *Account) FindApplication(appID string) (app Application, err error) {
-	resp, err := util.GET(a.server, a.accessToken, fmt.Printf("/applications/%s", appID))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusNotFound {
-		return app, fmt.Errorf("Application with id '%s' does not exist", appID)
-	}
-
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&apps); err != nil {
-		return app, err
-	}
-
-	return app, nil
+	err = util.GET(a.server, a.accessToken, "/applications", &app)
+	return app, err
 }
 
 type createApplicationReq struct {
 	Name  string         `json:"name"`
 	AppID string         `json:"id"`
-	EUIS  []types.AppEUI `json:"euis"`
+	EUIs  []types.AppEUI `json:"euis"`
 }
 
 // CreateApplication creates a new application on the account server
@@ -62,17 +34,8 @@ func (a *Account) CreateApplication(appID string, name string, EUIs []types.AppE
 		EUIs:  EUIs,
 	}
 
-	resp, err := util.POST(a.server, a.accessToken, "/applications", body)
-	if resp.StatusCode != http.StatusCreated {
-		return app, fmt.Errorf("Could not create application: %s", resp.Status)
-	}
-
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&apps); err != nil {
-		return app, err
-	}
-
-	return app, nil
+	err = util.POST(a.server, a.accessToken, "/applications", &body, &app)
+	return app, err
 }
 
 // DeleteApplication deletes an application
@@ -81,22 +44,22 @@ func (a *Account) DeleteAppliction(appID string) error {
 }
 
 // Grant adds a collaborator to the application
-func (a *Account) Grant(appID string, username string, rights []Right) error {
+func (a *Account) Grant(appID string, username string, rights []types.Right) error {
 	panic("Grant not implemented")
 }
 
 // Retract removes rights from a collaborator of the application
-func (a *Account) Retract(appID string, username string, rights []Right) error {
+func (a *Account) Retract(appID string, username string, rights []types.Right) error {
 	panic("Retract not implemented")
 }
 
 // AddAccessKey
-func (a *Account) AddAccessKey(appID string, key AccessKey) error {
+func (a *Account) AddAccessKey(appID string, key types.AccessKey) error {
 	panic("AddAccessKey not implemented")
 }
 
 // RemoveAccessKey
-func (a *Account) RemoveAccessKey(appID string, key AccessKey) error {
+func (a *Account) RemoveAccessKey(appID string, key types.AccessKey) error {
 	panic("RemoveAccessKey not implemented")
 }
 
