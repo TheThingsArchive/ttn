dba99fa627a9069d7f698a104bb3612959795a41
Romeo Van Snick <romeovs@gmail.com>
2016-07-20 11:21:00 +0200

    add tests for HTTP PUT

--- core/account/util/http_test.go
+++ core/account/util/http_test.go
@@ -5,6 +5,7 @@ package util
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -63,6 +64,18 @@ func RedirectHandler(a *Assertion, method string) http.HandlerFunc {
 	})
 }
 
+func EchoHandler(a *Assertion, method string) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a.So(r.RequestURI, ShouldEqual, "/foo")
+		a.So(r.Method, ShouldEqual, method)
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		a.So(err, ShouldBeNil)
+		w.Write(body)
+	})
+}
+
 func TestGET(t *testing.T) {
 	a := New(t)
 	server := httptest.NewServer(OKHandler(a, "GET"))
@@ -83,7 +96,7 @@ func TestGETDropResponse(t *testing.T) {
 	a.So(err, ShouldBeNil)
 }
 
-func TestGETWrongResponseType(t *testing.T) {
+func TestGETIllegalResponse(t *testing.T) {
 	a := New(t)
 	server := httptest.NewServer(OKHandler(a, "GET"))
 	defer server.Close()
@@ -93,7 +106,7 @@ func TestGETWrongResponseType(t *testing.T) {
 	a.So(err, ShouldNotBeNil)
 }
 
-func TestGETWrongResponseTypeIgnore(t *testing.T) {
+func TestGETIllegalResponseIgnore(t *testing.T) {
 	a := New(t)
 	server := httptest.NewServer(FooHandler(a, "GET"))
 	defer server.Close()
@@ -111,4 +124,50 @@ func TestGETRedirect(t *testing.T) {
 	var resp OKResp
 	err := GET(server.URL, "ok", "/foo", &resp)
 	a.So(err, ShouldBeNil)
+}
+
+func TestPUT(t *testing.T) {
+	a := New(t)
+	server := httptest.NewServer(EchoHandler(a, "PUT"))
+	defer server.Close()
+
+	var resp FooResp
+	body := FooResp{
+		Foo: "ok",
+	}
+	err := PUT(server.URL, "ok", "/foo", body, &resp)
+	a.So(err, ShouldBeNil)
+	a.So(resp.Foo, ShouldEqual, body.Foo)
+}
+
+func TestPUTIllegalRequest(t *testing.T) {
+	a := New(t)
+	server := httptest.NewServer(EchoHandler(a, "PUT"))
+	defer server.Close()
+
+	var resp FooResp
+	body := FooResp{}
+	err := PUT(server.URL, "ok", "/foo", body, &resp)
+	a.So(err, ShouldNotBeNil)
+}
+
+func TestPUTIllegalResponse(t *testing.T) {
+	a := New(t)
+	server := httptest.NewServer(OKHandler(a, "PUT"))
+	defer server.Close()
+
+	var resp FooResp
+	err := PUT(server.URL, "ok", "/foo", nil, &resp)
+	a.So(err, ShouldNotBeNil)
+}
+
+func TestPUTRedirect(t *testing.T) {
+	a := New(t)
+	server := httptest.NewServer(RedirectHandler(a, "PUT"))
+	defer server.Close()
+
+	var resp FooResp
+	err := PUT(server.URL, "ok", "/foo", nil, &resp)
+	a.So(err, ShouldBeNil)
+	a.So(resp.Foo, ShouldEqual, "ok")
 }
