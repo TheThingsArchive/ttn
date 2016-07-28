0f3f7369d70456ba32d73d049ac02fc29fa2bff0
johanstokking <johan@stokking.nl>
2016-07-28 11:28:44 +0100

    Fixed typo in short name

--- ttnctl/cmd/applications_list.go
+++ ttnctl/cmd/applications_list.go
@@ -14,7 +14,7 @@ import (
 // applicationsListCmd is the entrypoint for handlerctl
 var applicationsListCmd = &cobra.Command{
 	Use:   "list",
-	Short: "list applications",
+	Short: "List applications",
 	Long:  `ttnctl applications list can be used to list applications.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		account := util.GetAccount(ctx)
