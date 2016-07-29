a748a03cb0d8e6f95624c12393be68c904c01775
Hylke Visser <htdvisser@gmail.com>
2016-07-29 14:36:52 +0100

    Fix rights check in Discovery metadata

--- core/discovery/server.go
+++ core/discovery/server.go
@@ -5,6 +5,7 @@ package discovery
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/TheThingsNetwork/ttn/api"
 	pb "github.com/TheThingsNetwork/ttn/api/discovery"
@@ -16,16 +17,57 @@ type discoveryServer struct {
 	discovery Discovery
 }
 
+func (d *discoveryServer) checkMetadataEditRights(ctx context.Context, in *pb.MetadataRequest) error {
+	claims, err := d.discovery.ValidateTTNAuthContext(ctx)
+	if err != nil {
+		return err
+	}
+	switch in.Metadata.Key {
+	case pb.Metadata_PREFIX:
+		if in.ServiceName != "broker" {
+			return errors.New("ttn/discovery: Announcement service type should be \"broker\"")
+		}
+		if claims.Type != in.ServiceName {
+			return fmt.Errorf("ttn/discovery: Token subject %s does not correspond with announcement ID %s", claims.Subject, in.Id)
+		}
+		if claims.Subject != in.Id {
+			return fmt.Errorf("ttn/discovery: Token type %s does not correspond with announcement service type %s", claims.Type, in.ServiceName)
+		}
+		// TODO: Check if this PREFIX can be announced
+	case pb.Metadata_APP_EUI:
+		if in.ServiceName != "handler" {
+			return errors.New("ttn/discovery: Announcement service type should be \"handler\"")
+		}
+		if claims.Type != in.ServiceName {
+			return fmt.Errorf("ttn/discovery: Token subject %s does not correspond with announcement ID %s", claims.Subject, in.Id)
+		}
+		if claims.Subject != in.Id {
+			return fmt.Errorf("ttn/discovery: Token type %s does not correspond with announcement service type %s", claims.Type, in.ServiceName)
+		}
+		// TODO: Check if this APP_EUI can be announced
+		return errors.New("ttn/discovery: Can not announce AppEUIs at this time")
+	case pb.Metadata_APP_ID:
+		if in.ServiceName != "handler" {
+			return errors.New("ttn/discovery: Announcement service type should be \"handler\"")
+		}
+		// When announcing APP_ID, token is user token that contains apps
+		if !claims.CanEditApp(string(in.Metadata.Value)) {
+			return errors.New("ttn/discovery: No access to this application")
+		}
+	}
+	return nil
+}
+
 func (d *discoveryServer) Announce(ctx context.Context, announcement *pb.Announcement) (*api.Ack, error) {
 	claims, err := d.discovery.ValidateTTNAuthContext(ctx)
 	if err != nil {
 		return nil, err
 	}
 	if claims.Subject != announcement.Id {
-		return nil, errors.New("ttn/discovery: Token subject does not correspond with announcement ID")
+		return nil, fmt.Errorf("ttn/discovery: Token subject %s does not correspond with announcement ID %s", claims.Subject, announcement.Id)
 	}
 	if claims.Type != announcement.ServiceName {
-		return nil, errors.New("ttn/discovery: Token type does not correspond with announcement service type")
+		return nil, fmt.Errorf("ttn/discovery: Token type %s does not correspond with announcement service type %s", claims.Type, announcement.ServiceName)
 	}
 	announcementCopy := *announcement
 	announcement.Metadata = []*pb.Metadata{} // This will be taken from existing announcement
@@ -37,26 +79,9 @@ func (d *discoveryServer) Announce(ctx context.Context, announcement *pb.Announc
 }
 
 func (d *discoveryServer) AddMetadata(ctx context.Context, in *pb.MetadataRequest) (*api.Ack, error) {
-	claims, err := d.discovery.ValidateTTNAuthContext(ctx)
+	err := d.checkMetadataEditRights(ctx, in)
 	if err != nil {
 		return nil, err
-	}
-	if claims.Subject != in.Id {
-		return nil, errors.New("ttn/discovery: Token subject does not correspond with announcement ID")
-	}
-	if claims.Type != in.ServiceName {
-		return nil, errors.New("ttn/discovery: Token type does not correspond with announcement service type")
-	}
-	switch in.Metadata.Key {
-	case pb.Metadata_PREFIX:
-		// Allow announcing any PREFIX
-	case pb.Metadata_APP_EUI:
-		// TODO: Check the claims for the announced APP_EUI
-		return nil, errors.New("ttn/discovery: Can not announce AppEUIs at this time")
-	case pb.Metadata_APP_ID:
-		if !claims.CanEditApp(string(in.Metadata.Value)) {
-			return nil, errors.New("ttn/discovery: No access to this application")
-		}
 	}
 	err = d.discovery.AddMetadata(in.ServiceName, in.Id, in.Metadata)
 	if err != nil {
@@ -66,15 +91,9 @@ func (d *discoveryServer) AddMetadata(ctx context.Context, in *pb.MetadataReques
 }
 
 func (d *discoveryServer) DeleteMetadata(ctx context.Context, in *pb.MetadataRequest) (*api.Ack, error) {
-	claims, err := d.discovery.ValidateTTNAuthContext(ctx)
+	err := d.checkMetadataEditRights(ctx, in)
 	if err != nil {
 		return nil, err
-	}
-	if claims.Subject != in.Id {
-		return nil, errors.New("ttn/discovery: Token subject does not correspond with announcement ID")
-	}
-	if claims.Type != in.ServiceName {
-		return nil, errors.New("ttn/discovery: Token type does not correspond with announcement service type")
 	}
 	err = d.discovery.DeleteMetadata(in.ServiceName, in.Id, in.Metadata)
 	if err != nil {
