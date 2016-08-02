1e89cf9ca5faeb82710b716d0e76bfec6aa96b74
Romans Volosatovs <b1101@riseup.net>
2016-08-02 14:20:16 +0200

    ttnctl: `gateway` subcommand

---
+++ ttnctl/cmd/gateway.go
@@ -0,0 +1,19 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package cmd
+
+import "github.com/spf13/cobra"
+
+var gatewayCmd = &cobra.Command{
+	Use:   "gateway",
+	Short: "Manage gateways",
+	Long:  `ttnctl applications can be used to manage gateways.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		RootCmd.PersistentPreRun(cmd, args)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(gatewayCmd)
+}
