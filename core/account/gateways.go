d134828ae429f0ac795dc10be2e49fa37c36de7b
Romeo Van Snick <romeovs@gmail.com>
2016-08-12 10:07:18 +0100

    implement gateway api client

---
+++ core/account/gateways.go
@@ -0,0 +1,94 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package account
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/TheThingsNetwork/ttn/core/types"
+)
+
+// ListApplications list all applications
+func (a *Account) ListGateways() (gateways []Gateway, err error) {
+	err = a.get("/gateways", &gateways)
+	return gateways, err
+}
+
+// FindGateway returns the information about a specific gateay
+func (a *Account) FindGateway(gatewayID string) (gateway Gateway, err error) {
+	err = a.get(fmt.Sprintf("/gateways/%s", gatewayID), &gateway)
+	return gateway, err
+}
+
+// NewGateway is used as a paramater to CreateGateway to allow for optional
+// arguments
+type NewGateway struct {
+	// ID is the ID of the new gateway (required)
+	ID string `json:"id"`
+
+	// Country is the country code of the new gateway (required)
+	Country string `json:"country"`
+
+	// EUI is the EUI of the new gateway
+	EUI string `json:"eui,omitemtpy"`
+
+	// Location is the location of the new gateway
+	Location *Location `json:"location,omitempty"`
+}
+
+// CreateGateway registers a new gateway on the account server
+func (a *Account) CreateGateway(opts *NewGateway) (gateway Gateway, err error) {
+	if opts.ID == "" {
+		return gateway, errors.New("Cannot create gateway: no ID given")
+	}
+
+	if opts.Country == "" {
+		return gateway, errors.New("Cannot create gateway: no Country given")
+	}
+
+	err = a.post("/applications", &opts, &gateway)
+	return gateway, err
+}
+
+// DeleteGateway removes a gateway from the account server
+func (a *Account) DeleteGateway(gatewayID string) error {
+	return a.del(fmt.Sprintf("/gateways/%s", gatewayID))
+}
+
+// Grant grants rights to a collaborator of the gateway
+func (a *Account) GrantGatewayRights(gatewayID string, username string, rights []types.Right) error {
+	req := grantReq{
+		Rights: rights,
+	}
+	return a.put(fmt.Sprintf("/gateways/%s/collaborators/%s", gatewayID, username), req, nil)
+}
+
+// Retract removes rights from a collaborator of the gateway
+func (a *Account) RetractGatewayRights(gatewayID string, username string) error {
+	return a.del(fmt.Sprintf("/gateways/%s/collaborators/%s", gatewayID, username))
+}
+
+type editGatewayReq struct {
+	Owner        string        `json:"owner,omitempty"`
+	PublicRights []types.Right `json:"public_rights,omitempty"`
+}
+
+// TransferOwnership transfers the owenership of the gateway to another user
+func (a *Account) TransferOwnership(gatewayID, username string) error {
+	req := &editGatewayReq{
+		Owner: username,
+	}
+
+	return a.patch(fmt.Sprintf("/gateways/%s", gatewayID), req, nil)
+}
+
+// SetPublicRights changes the publicily visible rights of the gateway
+func (a *Account) SetPublicRights(gatewayID string, rights []types.Right) error {
+	req := &editGatewayReq{
+		PublicRights: rights,
+	}
+
+	return a.patch(fmt.Sprintf("/gateways/%s", gatewayID), req, nil)
+}
--- core/account/types.go
+++ core/account/types.go
@@ -59,3 +59,33 @@ type Component struct {
 func (n *Name) String() string {
 	return n.First + " " + n.Last
 }
+
+// Gateway represents a gateway on the account server
+type Gateway struct {
+	// ID is the id of the gateway
+	ID string `json:"id"`
+
+	// EUI is the eui of the gateway
+	EUI []types.GatewayEUI `json:"eui,omitempty"`
+
+	// Location is the location of the gateway
+	Location *Location `json:"location,omitempty"`
+
+	// Country is the country code where the gateway is located
+	Country string `json:"country"`
+
+	// Activated denotes wether or not the gateway has been activated yet
+	Activated bool `json:"activated"`
+
+	// Owner is the user that owns the gateway
+	Owner string `json:"owner,omitempty"`
+
+	// PublicRights are the rights that are publicly available for the gateway
+	PublicRights []types.Right `json:"public_rights"`
+}
+
+// Location represents a geo location
+type Location struct {
+	Lng float64 `json:"lng"`
+	Lat float64 `json:"lat"`
+}
