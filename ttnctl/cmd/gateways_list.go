29a06ce1303df313b1c9e7aa52f8961d86ead57d
Romeo Van Snick <romeovs@gmail.com>
2016-09-20 17:34:01 +0200

    Add gateway list cmd

---
+++ ttnctl/cmd/gateways_list.go
@@ -0,0 +1,46 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/TheThingsNetwork/ttn/ttnctl/util"
+	"github.com/gosuri/uitable"
+	"github.com/spf13/cobra"
+)
+
+var gatewaysListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List your gateways",
+	Long:  `ttnctl gateways list can be used to list the gateways you have access to`,
+	Run: func(cmd *cobra.Command, args []string) {
+		act := util.GetAccount(ctx)
+		gateways, err := act.ListGateways()
+		if err != nil {
+			ctx.WithError(err).Fatal("Could not list gateways")
+		}
+
+		table := uitable.New()
+		table.MaxColWidth = 70
+		table.AddRow("", "ID", "Activated", "Frequency Plan", "Lat", "Lng")
+		for i, gateway := range gateways {
+			var lat float64
+			var lng float64
+			if gateway.Location != nil {
+				lat = gateway.Location.Latitude
+				lng = gateway.Location.Longitude
+			}
+			table.AddRow(i+1, gateway.ID, gateway.Activated, gateway.FrequencyPlan, lat, lng)
+		}
+
+		fmt.Println()
+		fmt.Println(table)
+		fmt.Println()
+	},
+}
+
+func init() {
+	gatewaysCmd.AddCommand(gatewaysListCmd)
+}
