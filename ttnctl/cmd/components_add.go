f9792b378ebe3229e1329500cf55e8009e6aa640
Romeo Van Snick <romeovs@gmail.com>
2016-10-06 11:55:16 +0200

    Add ttnctl components commands

---
+++ ttnctl/cmd/components_add.go
@@ -0,0 +1,39 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"github.com/TheThingsNetwork/ttn/ttnctl/util"
+	"github.com/spf13/cobra"
+)
+
+var componentsAddCmd = &cobra.Command{
+	Use:   "add [type] [ComponentID]",
+	Short: "Add a new network component",
+	Long:  `ttnctl components add can be used to add a new network component.`,
+	Example: `$ ttnctld components add handler test                                                                                                                                             146 !
+  INFO Added network component                  id=test type=handler
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			cmd.UsageFunc()(cmd)
+			return
+		}
+
+		account := util.GetAccount(ctx)
+
+		err := account.CreateComponent(args[0], args[1])
+		if err != nil {
+			ctx.WithError(err).WithField("type", args[0]).WithField("id", args[1]).Fatal("Could not add component")
+		}
+
+		util.ForceRefreshToken(ctx)
+
+		ctx.WithField("type", args[0]).WithField("id", args[1]).Info("Added network component")
+	},
+}
+
+func init() {
+	componentsCmd.AddCommand(componentsAddCmd)
+}
---
+++ ttnctl/cmd/components_info.go
@@ -0,0 +1,49 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/TheThingsNetwork/ttn/ttnctl/util"
+	"github.com/spf13/cobra"
+)
+
+var componentsInfoCmd = &cobra.Command{
+	Use:   "info [type] [ComponentID]",
+	Short: "Get information about a network component.",
+	Long:  `components info can be used to retreive information about a network component.`,
+	Example: `$ ttnctl components info handler test
+  INFO Found network component
+
+Component ID:   test
+Type:           handler
+Created:        2016-10-06 09:52:28.766 +0000 UTC
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			cmd.UsageFunc()(cmd)
+			return
+		}
+
+		account := util.GetAccount(ctx)
+
+		component, err := account.FindComponent(args[0], args[1])
+		if err != nil {
+			ctx.WithError(err).WithField("type", args[0]).WithField("id", args[1]).Fatal("Could not find component")
+		}
+
+		ctx.Info("Found network component")
+
+		fmt.Println()
+		fmt.Printf("Component ID:   %s\n", component.ID)
+		fmt.Printf("Type:           %s\n", component.Type)
+		fmt.Printf("Created:        %s\n", component.Created)
+		fmt.Println()
+	},
+}
+
+func init() {
+	componentsCmd.AddCommand(componentsInfoCmd)
+}
