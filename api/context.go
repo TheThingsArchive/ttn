345477d18144dba7ad49d88ef6c76918a194a41e
Roman Volosatovs <roman@thethingsnetwork.org>
2016-10-07 00:10:24 +0200

    api.{ID,Token}FromContext: helper functions

---
+++ api/context.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
+)
+
+func TokenFromContext(ctx context.Context) (token string, err error) {
+	var md metadata.MD
+	if md, err = MetadataFromContext(ctx); err != nil {
+		return "", err
+	}
+
+	return TokenFromMetadata(md)
+}
+
+func IDFromContext(ctx context.Context) (token string, err error) {
+	var md metadata.MD
+	if md, err = MetadataFromContext(ctx); err != nil {
+		return "", err
+	}
+
+	return IDFromMetadata(md)
+}
