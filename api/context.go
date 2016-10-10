eea57b3b4e71606807cbac19ee21729f0bc21f9c
Roman Volosatovs <roman@thethingsnetwork.org>
2016-10-10 17:16:20 +0200

    api/context: refactor

--- api/context.go
+++ api/context.go
@@ -1,24 +1,19 @@
 package api
 
-import (
-	context "golang.org/x/net/context" //TODO change to "context", when protoc supports it
-	"google.golang.org/grpc/metadata"
-)
+import context "golang.org/x/net/context"
 
 func TokenFromContext(ctx context.Context) (token string, err error) {
-	var md metadata.MD
-	if md, err = MetadataFromContext(ctx); err != nil {
+	md, err := MetadataFromContext(ctx)
+	if err != nil {
 		return "", err
 	}
-
 	return TokenFromMetadata(md)
 }
 
 func IDFromContext(ctx context.Context) (token string, err error) {
-	var md metadata.MD
-	if md, err = MetadataFromContext(ctx); err != nil {
+	md, err := MetadataFromContext(ctx)
+	if err != nil {
 		return "", err
 	}
-
 	return IDFromMetadata(md)
 }
