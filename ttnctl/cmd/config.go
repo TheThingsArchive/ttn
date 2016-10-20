7285a83872da2f3dfc127745a8214ae3cddc61ba
Romeo Van Snick <romeovs@gmail.com>
2016-10-20 17:08:17 +0200

    Add config command

---
+++ ttnctl/cmd/config.go
@@ -0,0 +1,22 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"github.com/TheThingsNetwork/ttn/ttnctl/util"
+	"github.com/spf13/cobra"
+)
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Print the used configuration",
+	Long:  `ttnctl config prints the configuration that is used`,
+	Run: func(cmd *cobra.Command, args []string) {
+		util.PrintConfig(ctx, false)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(configCmd)
+}
