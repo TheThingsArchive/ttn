54c8ef880e0787ae6ae69773606d37680f52aa7e
Hylke Visser <htdvisser@gmail.com>
2016-11-11 13:38:26 +0100

    Use correct logging in ttnctl

--- ttnctl/cmd/root.go
+++ ttnctl/cmd/root.go
@@ -48,7 +48,7 @@ var RootCmd = &cobra.Command{
 		api.DialOptions = append(api.DialOptions, grpc.WithBlock())
 		api.DialOptions = append(api.DialOptions, grpc.WithTimeout(2*time.Second))
 		grpclog.SetLogger(logging.NewGRPCLogger(ctx))
-		api.SetLogger(ctx)
+		api.SetLogger(api.Apex(ctx))
 	},
 }
 
