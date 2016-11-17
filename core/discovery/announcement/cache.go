aff423a782806e8d560cf09dade531a449de8e19
Hylke Visser <htdvisser@gmail.com>
2016-11-17 16:44:06 +0100

    Fix cache keys for discovery ServiceCache

--- core/discovery/announcement/cache.go
+++ core/discovery/announcement/cache.go
@@ -4,6 +4,8 @@
 package announcement
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/TheThingsNetwork/ttn/core/types"
@@ -32,20 +34,19 @@ var DefaultCacheOptions = CacheOptions{
 	ListCacheExpiration:    10 * time.Second, // We can afford to fetch every 10 seconds
 }
 
-type serviceCacheKey struct {
-	ServiceName string
-	ServiceID   string
+func serviceCacheKey(serviceName, serviceID string) string {
+	return fmt.Sprintf("%s:%s", serviceName, serviceID)
 }
 
 // NewCachedAnnouncementStore returns a cache wrapper around the existing store
 func NewCachedAnnouncementStore(store Store, options CacheOptions) Store {
-	serviceCache := gcache.New(options.ServiceCacheSize).Expiration(options.ServiceCacheExpiration).ARC().
+	serviceCache := gcache.New(options.ServiceCacheSize).Expiration(options.ServiceCacheExpiration).LFU().
 		LoaderFunc(func(k interface{}) (interface{}, error) {
-			key := k.(*serviceCacheKey)
-			return store.Get(key.ServiceName, key.ServiceID)
+			key := strings.Split(k.(string), ":")
+			return store.Get(key[0], key[1])
 		}).Build()
 
-	listCache := gcache.New(options.ListCacheSize).Expiration(options.ListCacheExpiration).ARC().
+	listCache := gcache.New(options.ListCacheSize).Expiration(options.ListCacheExpiration).LFU().
 		LoaderFunc(func(k interface{}) (interface{}, error) {
 			key := k.(string)
 			announcements, err := store.ListService(key)
@@ -54,7 +55,7 @@ func NewCachedAnnouncementStore(store Store, options CacheOptions) Store {
 			}
 			go func(announcements []*Announcement) {
 				for _, announcement := range announcements {
-					serviceCache.Set(&serviceCacheKey{announcement.ServiceName, announcement.ID}, announcement)
+					serviceCache.Set(serviceCacheKey(announcement.ServiceName, announcement.ID), announcement)
 				}
 			}(announcements)
 			return announcements, nil
@@ -81,7 +82,7 @@ func (s *cachedAnnouncementStore) ListService(serviceName string) ([]*Announceme
 }
 
 func (s *cachedAnnouncementStore) Get(serviceName, serviceID string) (*Announcement, error) {
-	a, err := s.serviceCache.Get(&serviceCacheKey{serviceName, serviceID})
+	a, err := s.serviceCache.Get(serviceCacheKey(serviceName, serviceID))
 	if err != nil {
 		return nil, err
 	}
@@ -89,7 +90,7 @@ func (s *cachedAnnouncementStore) Get(serviceName, serviceID string) (*Announcem
 }
 
 func (s *cachedAnnouncementStore) GetMetadata(serviceName, serviceID string) ([]Metadata, error) {
-	a, err := s.serviceCache.Get(&serviceCacheKey{serviceName, serviceID})
+	a, err := s.serviceCache.Get(serviceCacheKey(serviceName, serviceID))
 	if err != nil {
 		return nil, err
 	}
@@ -110,7 +111,7 @@ func (s *cachedAnnouncementStore) Set(new *Announcement) error {
 	if err := s.backingStore.Set(new); err != nil {
 		return err
 	}
-	s.serviceCache.Remove(&serviceCacheKey{new.ServiceName, new.ID})
+	s.serviceCache.Remove(serviceCacheKey(new.ServiceName, new.ID))
 	s.listCache.Remove(&new.ServiceName)
 	return nil
 }
@@ -119,7 +120,7 @@ func (s *cachedAnnouncementStore) AddMetadata(serviceName, serviceID string, met
 	if err := s.backingStore.AddMetadata(serviceName, serviceID, metadata...); err != nil {
 		return err
 	}
-	s.serviceCache.Remove(&serviceCacheKey{serviceName, serviceID})
+	s.serviceCache.Remove(serviceCacheKey(serviceName, serviceID))
 	s.listCache.Remove(&serviceName)
 	return nil
 }
@@ -128,7 +129,7 @@ func (s *cachedAnnouncementStore) RemoveMetadata(serviceName, serviceID string, 
 	if err := s.backingStore.RemoveMetadata(serviceName, serviceID, metadata...); err != nil {
 		return err
 	}
-	s.serviceCache.Remove(&serviceCacheKey{serviceName, serviceID})
+	s.serviceCache.Remove(serviceCacheKey(serviceName, serviceID))
 	s.listCache.Remove(&serviceName)
 	return nil
 }
@@ -137,7 +138,7 @@ func (s *cachedAnnouncementStore) Delete(serviceName, serviceID string) error {
 	if err := s.backingStore.Delete(serviceName, serviceID); err != nil {
 		return err
 	}
-	s.serviceCache.Remove(&serviceCacheKey{serviceName, serviceID})
+	s.serviceCache.Remove(serviceCacheKey(serviceName, serviceID))
 	s.listCache.Remove(&serviceName)
 	return nil
 }
