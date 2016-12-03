ae695393a66fee1eeff26f54d61edc32186c63e6
Hylke Visser <htdvisser@gmail.com>
2016-12-03 09:19:42 +0100

    Move api stats to own package

--- api/stats/stats.go
+++ api/stats/stats.go
@@ -1,37 +1,41 @@
 // Copyright © 2016 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
-package api
+package stats
 
 import (
 	"os"
 	"runtime"
+	"time"
 
+	"github.com/TheThingsNetwork/ttn/api"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/load"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/process"
 )
 
-// GetSystemStats gets statistics about the system
-func GetSystemStats() *SystemStats {
-	status := new(SystemStats)
+var startTime = time.Now()
+
+// GetSystem gets statistics about the system
+func GetSystem() *api.SystemStats {
+	status := new(api.SystemStats)
 	if load, err := load.Avg(); err == nil {
-		status.Load = &SystemStats_Loadstats{
+		status.Load = &api.SystemStats_Loadstats{
 			Load1:  float32(load.Load1),
 			Load5:  float32(load.Load5),
 			Load15: float32(load.Load15),
 		}
 	}
 	if cpu, err := cpu.Times(false); err == nil && len(cpu) == 1 {
-		status.Cpu = &SystemStats_CPUStats{
+		status.Cpu = &api.SystemStats_CPUStats{
 			User:   float32(cpu[0].User),
 			System: float32(cpu[0].System),
 			Idle:   float32(cpu[0].Idle),
 		}
 	}
 	if mem, err := mem.VirtualMemory(); err == nil {
-		status.Memory = &SystemStats_MemoryStats{
+		status.Memory = &api.SystemStats_MemoryStats{
 			Total:     mem.Total,
 			Available: mem.Available,
 			Used:      mem.Used,
@@ -40,19 +44,20 @@ func GetSystemStats() *SystemStats {
 	return status
 }
 
-// GetComponentStats gets statistics about this component
-func GetComponentStats() *ComponentStats {
-	status := new(ComponentStats)
+// GetComponent gets statistics about this component
+func GetComponent() *api.ComponentStats {
+	status := new(api.ComponentStats)
+	status.Uptime = uint64(time.Now().Sub(startTime).Seconds())
 	process, err := process.NewProcess(int32(os.Getpid()))
 	if err == nil {
 		if memory, err := process.MemoryInfo(); err == nil {
-			status.Memory = &ComponentStats_MemoryStats{
+			status.Memory = &api.ComponentStats_MemoryStats{
 				Memory: memory.RSS,
 				Swap:   memory.Swap,
 			}
 		}
 		if cpu, err := process.Times(); err == nil {
-			status.Cpu = &ComponentStats_CPUStats{
+			status.Cpu = &api.ComponentStats_CPUStats{
 				User:   float32(cpu.User),
 				System: float32(cpu.System),
 				Idle:   float32(cpu.Idle),
