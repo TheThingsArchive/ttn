ae014d19f294a2a2c12d9a985c0c66210e256a11
Roman Volosatovs <roman@thethingsnetwork.org>
2016-12-19 09:43:42 +0100

    api.{OffsetFromMetadata, LimitFromMetadata}

--- api/metadata.go
+++ api/metadata.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"strconv"
+
 	"github.com/TheThingsNetwork/ttn/utils/errors"
 	"golang.org/x/net/context" // See https://github.com/grpc/grpc-go/issues/711"
 	"google.golang.org/grpc/metadata"
@@ -8,10 +10,12 @@ import (
 
 // Errors that are returned when an item could not be retrieved
 var (
-	ErrContext = errors.NewErrInternal("Could not get metadata from context")
-	ErrNoToken = errors.NewErrInvalidArgument("Metadata", "token missing")
-	ErrNoKey   = errors.NewErrInvalidArgument("Metadata", "key missing")
-	ErrNoID    = errors.NewErrInvalidArgument("Metadata", "id missing")
+	ErrContext  = errors.NewErrInternal("Could not get metadata from context")
+	ErrNoOffset = errors.NewErrInvalidArgument("Metadata", "offset missing")
+	ErrNoLimit  = errors.NewErrInvalidArgument("Metadata", "limit missing")
+	ErrNoToken  = errors.NewErrInvalidArgument("Metadata", "token missing")
+	ErrNoKey    = errors.NewErrInvalidArgument("Metadata", "key missing")
+	ErrNoID     = errors.NewErrInvalidArgument("Metadata", "id missing")
 )
 
 func MetadataFromContext(ctx context.Context) (metadata.MD, error) {
@@ -44,4 +48,20 @@ func KeyFromMetadata(md metadata.MD) (string, error) {
 		return "", ErrNoKey
 	}
 	return key[0], nil
+}
+
+func OffsetFromMetadata(md metadata.MD) (int, error) {
+	offset, ok := md["offset"]
+	if !ok || len(offset) == 0 {
+		return 0, ErrNoOffset
+	}
+	return strconv.Atoi(offset[0])
+}
+
+func LimitFromMetadata(md metadata.MD) (int, error) {
+	limit, ok := md["limit"]
+	if !ok || len(limit) == 0 {
+		return 0, ErrNoLimit
+	}
+	return strconv.Atoi(limit[0])
 }
