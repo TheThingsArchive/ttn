14de12750bf90dd06674b2505c8bfd7124d825d8
Hylke Visser <htdvisser@gmail.com>
2017-01-12 16:26:54 +0100

    Add GoString to Trace

--- api/trace/utils.go
+++ api/trace/utils.go
@@ -5,6 +5,9 @@ package trace
 
 import (
 	"encoding/base64"
+	"fmt"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/TheThingsNetwork/ttn/utils/random"
@@ -33,4 +36,32 @@ func (m *Trace) WithEvent(event string, metadata map[string]string) *Trace {
 		t.Parents = append(t.Parents, m)
 	}
 	return t
+}
+
+type byTime []*Trace
+
+func (a byTime) Len() int           { return len(a) }
+func (a byTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byTime) Less(i, j int) bool { return a[i].Time < a[j].Time }
+
+// Flatten the trace events and sort by timestamp
+func (m *Trace) Flatten() []*Trace {
+	flattened := []*Trace{m}
+	for _, parent := range m.Parents {
+		flattened = append(flattened, parent.Flatten()...)
+	}
+	sort.Sort(byTime(flattened))
+	return flattened
+}
+
+func (m *Trace) GoString() (out string) {
+	flattened := m.Flatten()
+	for _, trace := range flattened {
+		out += fmt.Sprintf("%d | %s %s | %s", trace.Time, trace.ServiceName, trace.ServiceId, trace.Event)
+		for k, v := range trace.Metadata {
+			out += fmt.Sprintf(" (%s=%s)", k, v)
+		}
+		out += "\n"
+	}
+	return strings.TrimSpace(out)
 }
