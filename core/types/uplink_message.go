7f88cd3e90a707df04bb82125e6a144ed86d299c
Hylke Visser <htdvisser@gmail.com>
2017-01-16 10:58:56 +0100

    Add HardwareSerial to MQTT Uplink
    For LoRaWAN this is the DevEUI
    
    Resolves #406

--- core/types/uplink_message.go
+++ core/types/uplink_message.go
@@ -5,11 +5,12 @@ package types
 
 // UplinkMessage represents an application-layer uplink message
 type UplinkMessage struct {
-	AppID         string                 `json:"app_id,omitempty"`
-	DevID         string                 `json:"dev_id,omitempty"`
-	FPort         uint8                  `json:"port"`
-	FCnt          uint32                 `json:"counter"`
-	PayloadRaw    []byte                 `json:"payload_raw"`
-	PayloadFields map[string]interface{} `json:"payload_fields,omitempty"`
-	Metadata      Metadata               `json:"metadata,omitempty"`
+	AppID          string                 `json:"app_id,omitempty"`
+	DevID          string                 `json:"dev_id,omitempty"`
+	HardwareSerial string                 `json:"hardware_serial,omitempty"`
+	FPort          uint8                  `json:"port"`
+	FCnt           uint32                 `json:"counter"`
+	PayloadRaw     []byte                 `json:"payload_raw"`
+	PayloadFields  map[string]interface{} `json:"payload_fields,omitempty"`
+	Metadata       Metadata               `json:"metadata,omitempty"`
 }
