TheThingsArchive/ttn

commit 04f1fa6702f7dd24231964b2a4e96458e0eb1e41

Capitalize

ttnctl/cmd/gateways_edit.go
@@ -12,14 +12,14 @@ import (
 
 var gatewaysEditCmd = &cobra.Command{
 	Use:   "edit [GatewayID]",
-	Short: "edit a gateway",
+	Short: "Edit a gateway",
 	Long:  `ttnctl gateways edit can be used to edit settings of a gateway`,
 	Example: `$ ttnctl gateways edit test --location 52.37403,4.88968 --frequency-plan EU
   INFO Edited gateway                          Gateway ID=test
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
-			cmd.UsageFunc()(cmd)
+			cmd.Usagefunc()(cmd)
 			return
 		}
 

commit 448195e1cb8cd2d6fc0f2c1ed62f29b64dec717c

Fix typo introduced in 04f1fa67

ttnctl/cmd/gateways_edit.go
@@ -19,7 +19,7 @@ var gatewaysEditCmd = &cobra.Command{
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
-			cmd.Usagefunc()(cmd)
+			cmd.UsageFunc()(cmd)
 			return
 		}
 
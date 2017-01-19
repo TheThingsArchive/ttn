448195e1cb8cd2d6fc0f2c1ed62f29b64dec717c
Hylke Visser <htdvisser@gmail.com>
2017-01-19 10:11:11 +0100

    Fix typo introduced in 04f1fa67

--- ttnctl/cmd/gateways_edit.go
+++ ttnctl/cmd/gateways_edit.go
@@ -19,7 +19,7 @@ var gatewaysEditCmd = &cobra.Command{
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
-			cmd.Usagefunc()(cmd)
+			cmd.UsageFunc()(cmd)
 			return
 		}
 
