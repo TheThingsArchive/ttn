6bd6440822ee1b4bd9bf88da8cf5e1234b899286
Hylke Visser <htdvisser@gmail.com>
2017-01-26 14:29:02 +0100

    Use gRPC interceptors from go-utils

--- core/component/grpc.go
+++ core/component/grpc.go
@@ -4,86 +4,40 @@
 package component
 
 import (
-	"time"
-
+	"github.com/TheThingsNetwork/go-utils/grpc/interceptor"
 	"github.com/TheThingsNetwork/ttn/utils/errors"
 	"github.com/apex/log"
 	"github.com/mwitkow/go-grpc-middleware"
 	"golang.org/x/net/context" // See https://github.com/grpc/grpc-go/issues/711"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/peer"
 )
 
 func (c *Component) ServerOptions() []grpc.ServerOption {
-	unary := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		var peerAddr string
-		peer, ok := peer.FromContext(ctx)
-		if ok {
-			peerAddr = peer.Addr.String()
-		}
-		var peerID string
-		meta, ok := metadata.FromContext(ctx)
-		if ok {
-			id, ok := meta["id"]
-			if ok && len(id) > 0 {
-				peerID = id[0]
-			}
-		}
-		logCtx := c.Ctx.WithFields(log.Fields{
-			"CallerID": peerID,
-			"CallerIP": peerAddr,
-			"Method":   info.FullMethod,
-		})
-		t := time.Now()
+
+	unaryLog := interceptor.Unary(func(req interface{}, info *grpc.UnaryServerInfo) (log.Interface, string) {
+		return c.Ctx, "Request"
+	})
+
+	unaryErr := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		iface, err := handler(ctx, req)
 		err = errors.BuildGRPCError(err)
-		logCtx = logCtx.WithField("Duration", time.Now().Sub(t))
-		if grpc.Code(err) == codes.OK || grpc.Code(err) == codes.Canceled {
-			logCtx.Debug("Handled request")
-		} else {
-			logCtx.WithField("ErrCode", grpc.Code(err)).WithError(err).Debug("Handled request with error")
-		}
 		return iface, err
 	}
 
-	stream := func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		var peerAddr string
-		peer, ok := peer.FromContext(stream.Context())
-		if ok {
-			peerAddr = peer.Addr.String()
-		}
-		var peerID string
-		meta, ok := metadata.FromContext(stream.Context())
-		if ok {
-			id, ok := meta["id"]
-			if ok && len(id) > 0 {
-				peerID = id[0]
-			}
-		}
-		logCtx := c.Ctx.WithFields(log.Fields{
-			"CallerID": peerID,
-			"CallerIP": peerAddr,
-			"Method":   info.FullMethod,
-		})
-		t := time.Now()
-		logCtx.Debug("Start stream")
+	streamLog := interceptor.Stream(func(req interface{}, info *grpc.StreamServerInfo) (log.Interface, string) {
+		return c.Ctx, "Stream"
+	})
+
+	streamErr := func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		err := handler(srv, stream)
 		err = errors.BuildGRPCError(err)
-		logCtx = logCtx.WithField("Duration", time.Now().Sub(t))
-		if grpc.Code(err) == codes.OK || grpc.Code(err) == codes.Canceled {
-			logCtx.Debug("End stream")
-		} else {
-			logCtx.WithField("ErrCode", grpc.Code(err)).WithError(err).Debug("End stream with error")
-		}
 		return err
 	}
 
 	opts := []grpc.ServerOption{
-		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(unary)),
-		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(stream)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(unaryErr, unaryLog)),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(streamErr, streamLog)),
 	}
 
 	if c.tlsConfig != nil {
