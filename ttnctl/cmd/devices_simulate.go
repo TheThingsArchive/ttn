a6379fca335325861f9fc7fe953fa67bba4903c5
Hylke Visser <htdvisser@gmail.com>
2017-01-31 16:20:24 +0100

    Implement uplink simulation in Handler
    Resolves #310

---
+++ ttnctl/cmd/devices_simulate.go
@@ -0,0 +1,61 @@
+// Copyright © 2017 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"github.com/TheThingsNetwork/ttn/api"
+	"github.com/TheThingsNetwork/ttn/core/types"
+	"github.com/TheThingsNetwork/ttn/ttnctl/util"
+	"github.com/spf13/cobra"
+)
+
+var devicesSimulateCmd = &cobra.Command{
+	Use:   "simulate [Device ID] [Payload]",
+	Short: "Simulate uplink for a device",
+	Long:  `ttnctl devices simulate can be used to simulate an uplink message for a device.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(args) < 2 {
+			cmd.UsageFunc()(cmd)
+			return
+		}
+
+		devID := args[0]
+		if !api.ValidID(devID) {
+			ctx.Error("Invalid Device ID")
+			return
+		}
+
+		appID := util.GetAppID(ctx)
+
+		port, err := cmd.Flags().GetUint32("port")
+		if err != nil {
+			ctx.WithError(err).Error("Failed to read port flag")
+			return
+		}
+
+		payload, err := types.ParseHEX(args[1], len(args[1])/2)
+		if err != nil {
+			ctx.WithError(err).Error("Invalid Payload")
+			return
+		}
+
+		conn, manager := util.GetHandlerManager(ctx, appID)
+		defer conn.Close()
+
+		err = manager.SimulateUplink(appID, devID, port, payload)
+		if err != nil {
+			ctx.WithError(err).Error("Simulate failed")
+			return
+		}
+
+		ctx.Info("Simulated uplink sent")
+
+	},
+}
+
+func init() {
+	devicesCmd.AddCommand(devicesSimulateCmd)
+	devicesSimulateCmd.Flags().Uint32("port", 1, "Port number")
+}
