4adc7cc0d7a7967ec40fc01d5fb0a0a314f269a1
Hylke Visser <htdvisser@gmail.com>
2017-01-31 12:55:56 +0100

    Use gogo/protobuf in grpc-gateway

---
+++ core/proxy/jsonpb/copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sed \
+  -e 's:runtime:jsonpb:' \
+  -e 's:golang/protobuf:gogo/protobuf:g' \
+  -e 's:JSONPb:GoGoJSONPb:g' \
+  -e '/import (/a \
+  . "github.com/grpc-ecosystem/grpc-gateway/runtime"' \
+  ../../../vendor/github.com/grpc-ecosystem/grpc-gateway/runtime/marshal_jsonpb.go > marshaling.go
+
+goimports -w marshaling.go
+
+echo -e "// Code modified from github.com/grpc-ecosystem/grpc-gateway/runtime by copy.sh\n\n$(cat marshaling.go)" > marshaling.go
