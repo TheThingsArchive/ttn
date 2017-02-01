a760b911922f489150b8c74efc43a75c80b5af02
Hylke Visser <htdvisser@gmail.com>
2017-02-01 14:31:11 +0100

    Detect uplink retries in Handler [API]

--- core/types/uplink_message.go
+++ core/types/uplink_message.go
@@ -10,6 +10,7 @@ type UplinkMessage struct {
 	HardwareSerial string                 `json:"hardware_serial,omitempty"`
 	FPort          uint8                  `json:"port"`
 	FCnt           uint32                 `json:"counter"`
+	IsRetry        bool                   `json:"is_retry,omitempty"`
 	PayloadRaw     []byte                 `json:"payload_raw"`
 	PayloadFields  map[string]interface{} `json:"payload_fields,omitempty"`
 	Metadata       Metadata               `json:"metadata,omitempty"`
