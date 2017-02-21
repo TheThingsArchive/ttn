37db85970114f2fe402e727b33f7f808cf5e0fe6
Roman Volosatovs <roman@thethingsnetwork.org>
2017-02-21 21:48:51 +0100

    add random generators back

--- utils/random/random.go
+++ utils/random/random.go
@@ -4,8 +4,8 @@
 package random
 
 import (
-	"math/rand"
-	"reflect"
+	"fmt"
+	"math"
 	"time"
 
 	"github.com/TheThingsNetwork/go-utils/pseudorandom"
@@ -29,49 +29,122 @@ var global = New()
 
 // ID returns randomly generated ID
 func (r *TTNRandom) ID() string {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	return r.String(2 + rand.Intn(61))
+	return r.Interface.String(2 + r.Interface.Intn(61))
 }
 
-// ByteArray generates a random byte array of length n
-func (r *TTNRandom) ByteArray(n int) interface{} {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	var b byte
-	v := reflect.New(reflect.ArrayOf(n, reflect.TypeOf(b)))
-	for i, b := range global.Bytes(n) {
-		v.Index(i).Set(reflect.ValueOf(b))
+// Rssi generates RSSI signal between -120 < rssi < 0
+func (r *TTNRandom) Rssi() int32 {
+	// Generate RSSI. Tend towards generating great signal strength.
+	x := float64(r.Interface.Intn(math.MaxInt32)) * float64(2e-9)
+	return int32(-1.6 * math.Exp(x))
+}
+
+var freqs = []float32{
+	// EU
+	868.1,
+	868.3,
+	868.5,
+	868.8,
+	867.1,
+	867.3,
+	867.5,
+	867.7,
+	867.9,
+
+	// US
+	903.9,
+	904.1,
+	904.3,
+	904.5,
+	904.7,
+	904.9,
+	905.1,
+	905.3,
+	904.6,
+}
+
+// Freq generates a frequency between 867.1 and 905.3 Mhz
+func (r *TTNRandom) Freq() float32 {
+	return freqs[r.Interface.Intn(len(freqs))]
+}
+
+// Datr generates Datr for instance: SF4BW125
+func (r *TTNRandom) Datr() string {
+	// Spread Factor from 12 to 7
+	sf := 12 - r.Interface.Intn(7)
+	var bw int
+	if sf == 6 {
+		// DR6 -> SF7@250Khz
+		sf = 7
+		bw = 250
+	} else {
+		bw = 125
 	}
-	return v.Interface()
+	return fmt.Sprintf("SF%dBW%d", sf, bw)
 }
 
-func (r *TTNRandom) Bool() bool {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	return r.rand.Int63()%2 == 0
+// Codr generates Codr for instance: 4/6
+func (r *TTNRandom) Codr() string {
+	d := r.Interface.Intn(4) + 5
+	return fmt.Sprintf("4/%d", d)
 }
 
-func (r *TTNRandom) DevNonce() types.DevNonce {
-	return types.DevNonce(r.ByteArray(2).([2]byte))
+// Lsnr generates LoRa SNR ratio in db. Tend towards generating good ratio with low noise
+func (r *TTNRandom) Lsnr() float32 {
+	x := float64(r.Interface.Intn(math.MaxInt32)) * float64(2e-9)
+	return float32(math.Floor((-0.1*math.Exp(x)+5.5)*10) / 10)
 }
-func (r *TTNRandom) AppNonce() types.AppNonce {
-	return types.AppNonce(r.ByteArray(3).([3]byte))
+
+func (r *TTNRandom) DevNonce() (devNonce types.DevNonce) {
+	r.Interface.FillBytes(devNonce[:])
+	return
 }
-func (r *TTNRandom) NetID() types.NetID {
-	return types.NetID(r.ByteArray(3).([3]byte))
+func (r *TTNRandom) AppNonce() (appNonce types.AppNonce) {
+	r.Interface.FillBytes(appNonce[:])
+	return
 }
-func (r *TTNRandom) DevAddr() types.DevAddr {
-	return types.DevAddr(r.ByteArray(4).([4]byte))
+func (r *TTNRandom) NetID() (netID types.NetID) {
+	r.Interface.FillBytes(netID[:])
+	return
 }
-func (r *TTNRandom) EUI64() types.EUI64 {
-	return types.EUI64(r.ByteArray(8).([8]byte))
+func (r *TTNRandom) DevAddr() (devAddr types.DevAddr) {
+	r.Interface.FillBytes(devAddr[:])
+	return
 }
-func (r *TTNRandom) DevEUI() types.DevEUI {
+func (r *TTNRandom) EUI64() (eui types.EUI64) {
+	r.Interface.FillBytes(eui[:])
+	return
+}
+func (r *TTNRandom) DevEUI() (eui types.DevEUI) {
 	return types.DevEUI(r.EUI64())
 }
-func (r *TTNRandom) AppEUI() types.AppEUI {
+func (r *TTNRandom) AppEUI() (eui types.AppEUI) {
 	return types.AppEUI(r.EUI64())
+}
+
+// Rssi generates RSSI signal between -120 < rssi < 0
+func Rssi() int32 {
+	return global.Rssi()
+}
+
+// Freq generates a frequency between 865.0 and 870.0 Mhz
+func Freq() float32 {
+	return global.Freq()
+}
+
+// Datr generates Datr for instance: SF4BW125
+func Datr() string {
+	return global.Datr()
+}
+
+// Codr generates Codr for instance: 4/6
+func Codr() string {
+	return global.Codr()
+}
+
+// Lsnr generates LoRa SNR ratio in db. Tend towards generating good ratio with low noise
+func Lsnr() float32 {
+	return global.Lsnr()
 }
 
 // Intn returns random int with max n
@@ -87,11 +160,6 @@ func String(n int) string {
 // Bytes generates a random byte slice of length n
 func Bytes(n int) []byte {
 	return global.Bytes(n)
-}
-
-// ByteArray generates a random byte array of length n
-func ByteArray(n int) interface{} {
-	return global.ByteArray(n)
 }
 
 // Bool generates a random boolean
