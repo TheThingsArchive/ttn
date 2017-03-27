d5fabca2829f98599a7aee340c19d30661533941
Hylke Visser <hylke@thethingsnetwork.org>
2017-03-27 15:42:54 +0900

    Write mem pprof to disk in dev builds

--- core/component/pprof.go
+++
@@ -1,8 +0,0 @@
-// +build dev
-
-// Copyright © 2017 The Things Network
-// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
-
-package component
-
-import _ "net/http/pprof"
