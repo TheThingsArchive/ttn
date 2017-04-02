a54e465f0fc69c05b0eb60943d34b3d9d5492a4a
Johan Stokking <johan@stokking.nl>
2017-04-02 21:09:53 +0900

    Test with CayenneLPP

--- core/handler/convert_fields_test.go
+++ core/handler/convert_fields_test.go
@@ -38,51 +38,109 @@ func TestConvertFieldsUpCustom(t *testing.T) {
 	}
 
 	// No functions
-	ttnUp, appUp := buildCustomUplink(appID)
-	err := h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
-	a.So(err, ShouldBeNil)
-	a.So(appUp.PayloadFields, ShouldBeEmpty)
+	{
+		ttnUp, appUp := buildCustomUplink(appID)
+		err := h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appUp.PayloadFields, ShouldBeEmpty)
+	}
 
-	// Normal flow
 	app := &application.Application{
 		AppID:         appID,
 		PayloadFormat: application.PayloadFormatCustom,
 		CustomDecoder: `function Decoder (data) { return { temperature: ((data[0] << 8) | data[1]) / 100 }; }`,
 	}
-	a.So(h.applications.Set(app), ShouldBeNil)
-	defer func() {
-		h.applications.Delete(appID)
-	}()
-	ttnUp, appUp = buildCustomUplink(appID)
-	err = h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
-	a.So(err, ShouldBeNil)
 
-	a.So(appUp.PayloadFields, ShouldResemble, map[string]interface{}{
-		"temperature": 21.6,
-	})
+	// Normal flow
+	{
+		a.So(h.applications.Set(app), ShouldBeNil)
+		defer func() {
+			h.applications.Delete(appID)
+		}()
+		ttnUp, appUp := buildCustomUplink(appID)
+		err := h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appUp.PayloadFields, ShouldResemble, map[string]interface{}{
+			"temperature": 21.6,
+		})
+	}
 
 	// Invalidate data
-	app.StartUpdate()
-	app.CustomValidator = `function Validator (data) { return false; }`
-	h.applications.Set(app)
-	ttnUp, appUp = buildCustomUplink(appID)
-	err = h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
-	a.So(err, ShouldNotBeNil)
-	a.So(appUp.PayloadFields, ShouldBeEmpty)
+	{
+		app.StartUpdate()
+		app.CustomValidator = `function Validator (data) { return false; }`
+		h.applications.Set(app)
+		ttnUp, appUp := buildCustomUplink(appID)
+		err := h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
+		a.So(err, ShouldNotBeNil)
+		a.So(appUp.PayloadFields, ShouldBeEmpty)
+	}
 
 	// Function error
-	app.StartUpdate()
-	app.CustomValidator = `function Validator (data) { throw new Error("expected"); }`
-	h.applications.Set(app)
-	ttnUp, appUp = buildCustomUplink(appID)
-	err = h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
-	a.So(err, ShouldBeNil)
-	a.So(appUp.PayloadFields, ShouldBeEmpty)
+	{
+		app.StartUpdate()
+		app.CustomValidator = `function Validator (data) { throw new Error("expected"); }`
+		h.applications.Set(app)
+		ttnUp, appUp := buildCustomUplink(appID)
+		err := h.ConvertFieldsUp(GetLogger(t, "TestConvertFieldsUpCustom"), ttnUp, appUp, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appUp.PayloadFields, ShouldBeEmpty)
+		a.So(len(h.mqttEvent), ShouldEqual, 1)
+		evt := <-h.mqttEvent
+		_, ok := evt.Data.(types.ErrorEventData)
+		a.So(ok, ShouldBeTrue)
+	}
+}
 
-	a.So(len(h.mqttEvent), ShouldEqual, 1)
-	evt := <-h.mqttEvent
-	_, ok := evt.Data.(types.ErrorEventData)
-	a.So(ok, ShouldBeTrue)
+func buildCayenneLPPUplink(appID string) (*pb_broker.DeduplicatedUplinkMessage, *types.UplinkMessage) {
+	ttnUp := &pb_broker.DeduplicatedUplinkMessage{
+		AppId: appID,
+		DevId: "DevID-1",
+	}
+	appUp := &types.UplinkMessage{
+		FPort:      1,
+		AppID:      appID,
+		DevID:      "DevID-1",
+		PayloadRaw: []byte{10, 115, 41, 239}, // Channel 10, Barometric Pressure of 1073.5
+	}
+	return ttnUp, appUp
+}
+
+func TestConvertFieldsUpCayenneLPP(t *testing.T) {
+	a := New(t)
+	appID := "AppID-1"
+	ctx := GetLogger(t, "TestConvertFieldsUpCayenneLPP")
+
+	h := &handler{
+		applications: application.NewRedisApplicationStore(GetRedisClient(), "handler-test-convert-fields-up"),
+		mqttEvent:    make(chan *types.DeviceEvent, 1),
+	}
+
+	// No application
+	{
+		ttnUp, appUp := buildCayenneLPPUplink(appID)
+		err := h.ConvertFieldsUp(ctx, ttnUp, appUp, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appUp.PayloadFields, ShouldBeEmpty)
+	}
+
+	// Normal flow
+	{
+		app := &application.Application{
+			AppID:         appID,
+			PayloadFormat: application.PayloadFormatCayenneLPP,
+		}
+		a.So(h.applications.Set(app), ShouldBeNil)
+		defer func() {
+			h.applications.Delete(appID)
+		}()
+		ttnUp, appUp := buildCayenneLPPUplink(appID)
+		err = h.ConvertFieldsUp(ctx, ttnUp, appUp, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appUp.PayloadFields, ShouldResemble, map[string]interface{}{
+			"barometric_pressure_10": 1073.5,
+		})
+	}
 }
 
 func buildCustomDownlink() (*pb_broker.DownlinkMessage, *types.DownlinkMessage) {
@@ -110,27 +168,30 @@ func TestConvertFieldsDownCustom(t *testing.T) {
 	}
 
 	// No Encoder
-	ttnDown, appDown := buildCustomDownlink()
-	err := h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustom"), appDown, ttnDown, nil)
-	a.So(err, ShouldBeNil)
-	a.So(appDown.PayloadRaw, ShouldBeEmpty)
+	{
+		ttnDown, appDown := buildCustomDownlink()
+		err := h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustom"), appDown, ttnDown, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appDown.PayloadRaw, ShouldBeEmpty)
+	}
 
 	// Normal flow with Encoder
-	h.applications.Set(&application.Application{
-		AppID:         appID,
-		PayloadFormat: application.PayloadFormatCustom,
-		CustomEncoder: `function Encoder (payload, port){
+	{
+		h.applications.Set(&application.Application{
+			AppID:         appID,
+			PayloadFormat: application.PayloadFormatCustom,
+			CustomEncoder: `function Encoder (payload, port){
   		return [ port, 1, 2, 3, 4, 5, 6, 7 ]
 		}`,
-	})
-	defer func() {
-		h.applications.Delete(appID)
-	}()
-
-	ttnDown, appDown = buildCustomDownlink()
-	err = h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustom"), appDown, ttnDown, nil)
-	a.So(err, ShouldBeNil)
-	a.So(appDown.PayloadRaw, ShouldResemble, []byte{byte(appDown.FPort), 1, 2, 3, 4, 5, 6, 7})
+		})
+		defer func() {
+			h.applications.Delete(appID)
+		}()
+		ttnDown, appDown := buildCustomDownlink()
+		err := h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustom"), appDown, ttnDown, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appDown.PayloadRaw, ShouldResemble, []byte{byte(appDown.FPort), 1, 2, 3, 4, 5, 6, 7})
+	}
 }
 
 func TestConvertFieldsDownCustomNoPort(t *testing.T) {
@@ -142,25 +203,77 @@ func TestConvertFieldsDownCustomNoPort(t *testing.T) {
 	}
 
 	// No Encoder
-	ttnDown, appDown := buildCustomDownlink()
-	err := h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustomNoPort"), appDown, ttnDown, nil)
-	a.So(err, ShouldBeNil)
-	a.So(appDown.PayloadRaw, ShouldBeEmpty)
+	{
+		ttnDown, appDown := buildCustomDownlink()
+		err := h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustomNoPort"), appDown, ttnDown, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appDown.PayloadRaw, ShouldBeEmpty)
+	}
 
 	// Normal flow with Encoder
-	h.applications.Set(&application.Application{
-		AppID:         appID,
-		PayloadFormat: application.PayloadFormatCustom,
-		CustomEncoder: `function Encoder (payload){
+	{
+		h.applications.Set(&application.Application{
+			AppID:         appID,
+			PayloadFormat: application.PayloadFormatCustom,
+			CustomEncoder: `function Encoder (payload){
   		return [ 1, 2, 3, 4, 5, 6, 7 ]
 		}`,
-	})
-	defer func() {
-		h.applications.Delete(appID)
-	}()
+		})
+		defer func() {
+			h.applications.Delete(appID)
+		}()
+		ttnDown, appDown := buildCustomDownlink()
+		err := h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustomNoPort"), appDown, ttnDown, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appDown.PayloadRaw, ShouldResemble, []byte{1, 2, 3, 4, 5, 6, 7})
+	}
+}
 
-	ttnDown, appDown = buildCustomDownlink()
-	err = h.ConvertFieldsDown(GetLogger(t, "TestConvertFieldsDownCustomNoPort"), appDown, ttnDown, nil)
-	a.So(err, ShouldBeNil)
-	a.So(appDown.PayloadRaw, ShouldResemble, []byte{1, 2, 3, 4, 5, 6, 7})
+func buildCayenneLPPDownlink() (*pb_broker.DownlinkMessage, *types.DownlinkMessage) {
+	appEUI := types.AppEUI([8]byte{1, 2, 3, 4, 5, 6, 7, 8})
+	devEUI := types.DevEUI([8]byte{1, 2, 3, 4, 5, 6, 7, 8})
+	ttnDown := &pb_broker.DownlinkMessage{
+		AppEui: &appEUI,
+		DevEui: &devEUI,
+	}
+	appDown := &types.DownlinkMessage{
+		FPort:         1,
+		AppID:         "AppID-1",
+		DevID:         "DevID-1",
+		PayloadFields: map[string]interface{}{"temperature_0": -15.6},
+	}
+	return ttnDown, appDown
+}
+
+func TestConvertFieldsDownCayenneLPP(t *testing.T) {
+	a := New(t)
+	appID := "AppID-1"
+	ctx := GetLogger(t, "TestConvertFieldsDownCustom")
+
+	h := &handler{
+		applications: application.NewRedisApplicationStore(GetRedisClient(), "handler-test-convert-fields-down"),
+	}
+
+	// No Encoder
+	{
+		ttnDown, appDown := buildCayenneLPPDownlink()
+		err := h.ConvertFieldsDown(ctx, appDown, ttnDown, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appDown.PayloadRaw, ShouldBeEmpty)
+	}
+
+	// Normal flow with Encoder
+	{
+		h.applications.Set(&application.Application{
+			AppID:         appID,
+			PayloadFormat: application.PayloadFormatCayenneLPP,
+		})
+		defer func() {
+			h.applications.Delete(appID)
+		}()
+		ttnDown, appDown := buildCayenneLPPDownlink()
+		err := h.ConvertFieldsDown(ctx, appDown, ttnDown, nil)
+		a.So(err, ShouldBeNil)
+		a.So(appDown.PayloadRaw, ShouldResemble, []byte{7, 103, 255, 100})
+	}
 }
