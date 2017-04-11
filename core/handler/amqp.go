bd5f73cde14e34bcd5f421493ccb64d68387e3d0
Hylke Visser <hylke@thethingsnetwork.org>
2017-04-11 11:25:27 +0200

    Make AMQP goroutines consistent with MQTT

--- core/handler/amqp.go
+++ core/handler/amqp.go
@@ -4,6 +4,8 @@
 package handler
 
 import (
+	"sync"
+
 	ttnlog "github.com/TheThingsNetwork/go-utils/log"
 	"github.com/TheThingsNetwork/ttn/amqp"
 	"github.com/TheThingsNetwork/ttn/core/types"
@@ -70,50 +72,53 @@ func (h *handler) HandleAMQP(username, password, host, exchange, downlinkQueue s
 	if err != nil {
 		return err
 	}
-	go h.amqp()
-	return nil
-}
 
-func (h *handler) amqp() error {
 	ctx := h.Ctx.WithField("Protocol", "AMQP")
 	publisher := h.amqpClient.NewPublisher(h.amqpExchange)
-	err := publisher.Open()
+	err = publisher.Open()
 	if err != nil {
 		ctx.WithError(err).Error("Could not open publisher channel")
 		return err
 	}
-	defer publisher.Close()
-	go h.amqpUplink(ctx, publisher)
-	h.amqpEvt(ctx, publisher)
+
+	var pubWait sync.WaitGroup
+	pubWait.Add(2)
+	defer func() {
+		go func() {
+			pubWait.Wait()
+			publisher.Close()
+		}()
+	}()
+
+	go func() {
+		defer pubWait.Done()
+		for up := range h.amqpUp {
+			ctx.WithFields(ttnlog.Fields{
+				"DevID": up.DevID,
+				"AppID": up.AppID,
+			}).Debug("Publish Uplink")
+			err := publisher.PublishUplink(*up)
+			if err != nil {
+				ctx.WithError(err).Warn("Could not publish Uplink")
+			}
+		}
+	}()
+
+	go func() {
+		defer pubWait.Done()
+		for event := range h.amqpEvent {
+			ctx.WithFields(ttnlog.Fields{
+				"DevID": event.DevID,
+				"AppID": event.AppID,
+				"Event": event.Event,
+			}).Debug("Publish Event")
+			if event.DevID == "" {
+				publisher.PublishAppEvent(event.AppID, event.Event, event.Data)
+			} else {
+				publisher.PublishDeviceEvent(event.AppID, event.DevID, event.Event, event.Data)
+			}
+		}
+	}()
+
 	return nil
-}
-
-func (h *handler) amqpUplink(ctx ttnlog.Interface, publisher amqp.Publisher) {
-
-	for up := range h.amqpUp {
-		ctx.WithFields(ttnlog.Fields{
-			"DevID": up.DevID,
-			"AppID": up.AppID,
-		}).Debug("Publish Uplink")
-		err := publisher.PublishUplink(*up)
-		if err != nil {
-			ctx.WithError(err).Warn("Could not publish Uplink")
-		}
-	}
-}
-
-func (h *handler) amqpEvt(ctx ttnlog.Interface, publisher amqp.Publisher) {
-
-	for event := range h.amqpEvent {
-		h.Ctx.WithFields(ttnlog.Fields{
-			"DevID": event.DevID,
-			"AppID": event.AppID,
-			"Event": event.Event,
-		}).Debug("Publish Event on AMQP")
-		if event.DevID == "" {
-			publisher.PublishAppEvent(event.AppID, event.Event, event.Data)
-		} else {
-			publisher.PublishDeviceEvent(event.AppID, event.DevID, event.Event, event.Data)
-		}
-	}
 }
