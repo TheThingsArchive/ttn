fe5f18d863c756e418d915b9175eafac0b3328af
Hylke Visser <hylke@thethingsnetwork.org>
2017-05-18 11:44:06 +0200

    Add unit test for scoring downlink options in Broker

--- core/broker/downlink_test.go
+++ core/broker/downlink_test.go
@@ -4,15 +4,75 @@
 package broker
 
 import (
+	"sort"
 	"testing"
 
 	pb "github.com/TheThingsNetwork/ttn/api/broker"
+	pb_gateway "github.com/TheThingsNetwork/ttn/api/gateway"
 	pb_monitor "github.com/TheThingsNetwork/ttn/api/monitor"
 	"github.com/TheThingsNetwork/ttn/core/component"
 	"github.com/TheThingsNetwork/ttn/core/types"
 	. "github.com/TheThingsNetwork/ttn/utils/testing"
 	. "github.com/smartystreets/assertions"
 )
+
+func TestDownlinkScoring(t *testing.T) {
+	a := New(t)
+	refMD := pb_gateway.RxMetadata{Snr: 10, Rssi: -50}
+	refOption := pb.DownlinkOption{PossibleConflicts: 1, DutyCycle: 0.1, Utilization: 0.1}
+
+	var options ByScore
+	sort := func(md pb_gateway.RxMetadata, option pb.DownlinkOption) {
+		options = ByScore{
+			downlinkOption{uplinkMetadata: &refMD, option: &refOption},
+			downlinkOption{uplinkMetadata: &md, option: &option},
+		}
+		sort.Sort(options)
+	}
+
+	{
+		md, option := refMD, refOption
+		md.Snr += 2
+		sort(md, option)
+		a.So(*options[0].uplinkMetadata, ShouldResemble, md)
+	}
+
+	{
+		md, option := refMD, refOption
+		md.Snr -= 2
+		sort(md, option)
+		a.So(*options[0].uplinkMetadata, ShouldResemble, refMD)
+	}
+
+	{
+		md, option := refMD, refOption
+		md.Rssi += 10
+		sort(md, option)
+		a.So(*options[0].uplinkMetadata, ShouldResemble, md)
+	}
+
+	{
+		md, option := refMD, refOption
+		md.Rssi -= 10
+		sort(md, option)
+		a.So(*options[0].uplinkMetadata, ShouldResemble, refMD)
+	}
+
+	{
+		md, option := refMD, refOption
+		option.Utilization -= 0.05
+		sort(md, option)
+		a.So(*options[0].option, ShouldResemble, option)
+	}
+
+	{
+		md, option := refMD, refOption
+		option.Utilization += 0.05
+		sort(md, option)
+		a.So(*options[0].option, ShouldResemble, refOption)
+	}
+
+}
 
 func TestDownlink(t *testing.T) {
 	a := New(t)
