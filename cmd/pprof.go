0fb1a7c28e2b82ac2e8736326f3715cf86fff15d
Romeo Van Snick <romeovs@gmail.com>
2017-06-26 14:48:57 +0200

    Add pprof endpoint in dev build

--- cmd/pprof.go
+++ cmd/pprof.go
@@ -8,6 +8,7 @@ package cmd
 import (
 	"bytes"
 	"io/ioutil"
+	"net/http"
 	_ "net/http/pprof"
 	"runtime"
 	"runtime/pprof"
@@ -15,10 +16,13 @@ import (
 
 	"github.com/TheThingsNetwork/ttn/api/stats"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func init() {
 	cobra.OnInitialize(initPprof)
+
+	RootCmd.PersistentFlags().String("pprof-address", "", "The address to start listening on for pprof debugging")
 }
 
 func initPprof() {
@@ -37,6 +41,18 @@ func initPprof() {
 			buf.Reset()
 
 			ctx.WithField("ComponentStats", stats.GetComponent()).Debug("Stats")
+		}
+	}()
+
+	go func() {
+		time.Sleep(time.Second)
+		addr := viper.GetString("pprof-address")
+		if addr != "" {
+			ctx.WithField("Address", addr).Debug("Adding pprof server")
+			err := http.ListenAndServe(addr, nil)
+			if err != nil {
+				ctx.WithError(err).Warnf("Could not start pprof server")
+			}
 		}
 	}()
 }
