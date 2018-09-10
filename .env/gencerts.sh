55c1e6100177ee77e2f9d45b7e658629ca81aebd
Hylke Visser <hylke@thethingsnetwork.org>
2018-09-10 11:52:15 +0200

    Update development certificates

--- .env/gencerts.sh
+++ .env/gencerts.sh
@@ -6,3 +6,9 @@ for service in discovery router broker networkserver handler
 do
   ttn $service gen-cert --config "$env/$service/dev.yml" --key-dir "$env/$service" "localhost" "127.0.0.1" "::1"
 done
+
+pushd "$env/mqtt" > /dev/null
+
+go run generate.go
+
+popd > /dev/null
