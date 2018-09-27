7b3020dd0bbf3409e56c03a16f135b3c8a11a56f
Hylke Visser <hylke@thethingsnetwork.org>
2018-09-27 12:57:31 +0200

    Add DNS names for docker-compose to certs

--- .env/gencerts.sh
+++ .env/gencerts.sh
@@ -4,7 +4,7 @@ env=$(dirname $0)
 
 for service in discovery router broker networkserver handler
 do
-  ttn $service gen-cert --config "$env/$service/dev.yml" --key-dir "$env/$service" "localhost" "127.0.0.1" "::1"
+  ttn $service gen-cert --config "$env/$service/dev.yml" --key-dir "$env/$service" "$service" "localhost" "127.0.0.1" "::1"
 done
 
 pushd "$env/mqtt" > /dev/null
