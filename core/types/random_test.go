86414c703ee1f05ec121ae87e11eafdea92b29ad
Hylke Visser <hylke@thethingsindustries.com>
2019-05-16 11:23:57 +0200

    Get dependencies of core/types module

--- core/types/random_test.go
+++ core/types/random_test.go
@@ -1,14 +1,13 @@
 // Copyright © 2017 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
-package types_test
+package types
 
 import (
 	"math/rand"
 	"testing"
 	"time"
 
-	. "github.com/TheThingsNetwork/ttn/core/types"
 	. "github.com/smartystreets/assertions"
 )
 
